e1505120bfd05d954bec0ca0e82220ba41605e5e
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-08 15:17:45 +0200

    Refactored finto request

--- src/controllers/finto.ts
+++ src/controllers/finto.ts
@@ -2,53 +2,41 @@ import { Request, Response } from "express";
 import request from "request";
 import { parseString, processors } from "xml2js";
 
-function getResults(url: string) {
-  return new Promise((resolve, reject) => {
+export const getYsoOntologia = (req: Request, res: Response) => {
+  const options = {
+    url: `${process.env.FINTO_URL}/yso/data`,
+    headers: {
+      "Accept": "application/rdf+xml"
+    }
+  };
+
+  // TODO: Error handling
+  request.get(options, (error, response, body) => {
     const options = {
-      url: `${process.env.FINTO_URL}${url}`,
-      headers: {
-        "Accept": "application/rdf+xml"
-      }
+      tagNameProcessors: [processors.stripPrefix],
+      attrNameProcessors: [processors.stripPrefix],
+      valueProcessors: [processors.stripPrefix],
+      attrValueProcessors: [processors.stripPrefix]
     };
 
-    request.get(options, (error, response, body) => {
+    parseString(body, options, (err, result) => {
       if (error) {
-        reject(error);
-      } else {
-        const options = {
-          tagNameProcessors: [processors.stripPrefix],
-          attrNameProcessors: [processors.stripPrefix],
-          valueProcessors: [processors.stripPrefix],
-          attrValueProcessors: [processors.stripPrefix]
-        };
-
-        parseString(body, options, (error, result) => {
-          if (error) {
-            reject(error);
-          }
-
-          const data = result.RDF.Concept.map((concept: any) => {
-            const labels: any = {};
-
-            concept.prefLabel.forEach((label: any) => {
-              labels[label.$.lang] = label._;
-            });
-
-            return {
-              "concept": labels
-            };
-          });
-
-          resolve(data);
-        });
+        res.status(response.statusCode).send(err);
       }
+
+      const data = result.RDF.Concept.map((concept: any) => {
+        const labels: any = {};
+
+        concept.prefLabel.forEach((label: any) => {
+          labels[label.$.lang] = label._;
+        });
+
+        return {
+          "concept": labels
+        };
+      });
+
+      res.status(response.statusCode).json({data});
     });
   });
-}
-
-export const getYsoOntologia = (req: Request, res: Response) => {
-  getResults("/yso/data")
-    .then(data => {
-      res.status(200).json({data});
-    });
 };
