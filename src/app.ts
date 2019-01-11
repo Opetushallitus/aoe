007d236954589ffee7206b08822b5ba3b41140ef
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-11 09:42:41 +0200

    Now uses express.router with prefix

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import dotenv from "dotenv";
 import * as eperusteetKoodisto from "./controllers/eperusteet-koodisto";
 import * as ePerusteetAmosaa from "./controllers/eperusteet-amosaa";
 import * as finto from "./controllers/finto";
+import router from "./routes";
 
 dotenv.config();
 
@@ -17,9 +18,7 @@ app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Get requests
-app.get("/lukionkurssit", eperusteetKoodisto.getLukionkurssit);
-app.get("/opetussuunnitelmat", ePerusteetAmosaa.getOpetussuunnitelmat);
-app.get("/yso", finto.getYsoOntologia);
+// Prefixed routes
+app.use("/api/v1", router);
 
 export default app;
