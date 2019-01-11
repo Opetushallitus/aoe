722a3c25536933c976cdd9ae62e28b3136d1d619
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-11 13:27:44 +0200

    Added session storage

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import compression from "compression"; // compress requests
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import session from "express-session";
 
 import router from "./routes";
 
@@ -10,10 +11,18 @@ dotenv.config();
 const app = express();
 
 // Configuration
-app.set("port", 3000);
+app.use(session({
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  },
+  saveUninitialized: true,
+  secret: "ygDe7*1d@Z%PJLXE6FBGli5mN!%v^E",
+  // store: new RedisStore(),
+}));
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("port", 3000);
 
 // Prefixed routes
 app.use("/api/v1", router);
