c3eef8bbe9dd1473834313d978ead01d39a29ebc
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-16 15:40:42 +0200

    Added redis functionality

--- src/controllers/eperusteet-amosaa.ts
+++ src/controllers/eperusteet-amosaa.ts
@@ -1,41 +1,54 @@
 import { Request, Response } from "express";
 import rp from "request-promise";
 
-async function getData(endpoint: string, lang: string) {
-  try {
-    const data: object[] = [];
-    const pageSize: number = 100;
-    let page: number = 0;
-    let getResults: boolean = true;
+import { RedisWrapper } from "../utils/redis-wrapper";
 
-    while (getResults) {
-      let body = await rp.get({
-        url: `${process.env.EPERUSTEET_AMOSAA_URL}${endpoint}?sivukoko=${pageSize}&sivu=${page}`,
-        headers: {
-          "Accept": "application/json"
+const client = new RedisWrapper();
+
+async function getData(endpoint: string, lang: string, key: string) {
+  if (await client.exists(key)) {
+    return await client.getFromRedis(key);
+  } else {
+    try {
+      let data: object[] = [];
+      const pageSize: number = 100;
+      let page: number = 0;
+      let getResults: boolean = true;
+
+      while (getResults) {
+        let body = await rp.get({
+          url: `${process.env.EPERUSTEET_AMOSAA_URL}${endpoint}?sivukoko=${pageSize}&sivu=${page}`,
+          headers: {
+            "Accept": "application/json"
+          }
+        });
+
+        body = JSON.parse(body);
+        await data.push.apply(data, body.data);
+        page = body.sivu + 1;
+
+        // we're on the last page, stop looping
+        if (body.data.length < pageSize) {
+          getResults = false;
         }
+      }
+
+      data = data.map((koodi: any) => {
+        return {
+          "arvo": koodi.perusteDiaarinumero,
+          "selite": koodi.nimi[lang],
+        };
       });
 
-      body = JSON.parse(body);
-      await data.push.apply(data, body.data);
-      page = body.sivu + 1;
+      await client.setToRedis(key, data);
 
-      if (body.data.length < pageSize) {
-        getResults = false;
-      }
+      return await client.getFromRedis(key);
+    } catch (error) {
+      console.log(error);
     }
-
-    return data.map((koodi: any) => {
-      return {
-        "arvo": koodi.perusteDiaarinumero,
-        "selite": koodi.nimi[lang],
-      };
-    });
-  } catch (error) {
-    console.log(error);
   }
 }
 
 export const getOpetussuunnitelmat = async (req: Request, res: Response) => {
-  res.status(200).json(await getData("/opetussuunnitelmat", "fi"));
+  res.status(200).json(await getData("/opetussuunnitelmat", "fi", "opetussuunnitelmat"));
 };
