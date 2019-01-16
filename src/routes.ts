2cbff66c4c29857c0ab01c934ec0ff766b039dfd
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-16 16:15:38 +0200

    Fixed spelling

--- src/routes.ts
+++ src/routes.ts
@@ -1,14 +1,14 @@
 import { Router } from "express";
 
 import * as eperusteetKoodisto from "./controllers/eperusteet-koodisto";
-import * as ePerusteetAmosaa from "./controllers/eperusteet-amosaa";
+import * as eperusteetAmosaa from "./controllers/eperusteet-amosaa";
 import * as finto from "./controllers/finto";
 
 const router: Router = Router();
 
 // Development purposes
 router.get("/lukionkurssit", eperusteetKoodisto.getLukionkurssit);
-router.get("/opetussuunnitelmat", ePerusteetAmosaa.getOpetussuunnitelmat);
+router.get("/opetussuunnitelmat", eperusteetAmosaa.getOpetussuunnitelmat);
 router.get("/yso", finto.getYsoOntologia);
 
 // Actual routes
