1f2f013d479c718ae81972ab89ef568eef038bb4
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-16 16:32:03 +0200

    Added redis functionality, not sure if setting to redis works

--- src/controllers/finto.ts
+++ src/controllers/finto.ts
@@ -2,49 +2,65 @@ import { Request, Response } from "express";
 import rp from "request-promise";
 import { parseString, processors } from "xml2js";
 
-async function getData(endpoint: string, lang: string) {
-  try {
-    const options = {
-      url: `${process.env.FINTO_URL}${endpoint}`,
-      headers: {
-        "Accept": "application/rdf+xml"
-      }
-    };
+import { RedisWrapper } from "../utils/redis-wrapper";
 
-    const body = await rp.get(options);
+const client = new RedisWrapper();
 
-    const parseOptions = {
-      tagNameProcessors: [processors.stripPrefix],
-      attrNameProcessors: [processors.stripPrefix],
-      valueProcessors: [processors.stripPrefix],
-      attrValueProcessors: [processors.stripPrefix]
-    };
-
-    return await new Promise((resolve, reject) => {
-      parseString(body, parseOptions, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          const data: object[] = [];
-
-          result.RDF.Concept.map((concept: any) => {
-            const label = concept.prefLabel.find((e: any) => e.$.lang === lang);
-
-            data.push({
-              "arvo": concept.$.about,
-              "selite": label._,
-            });
-          });
-
-          resolve(data);
+async function getData(endpoint: string, lang: string, key: string) {
+  if (await client.exists(key)) {
+    return await client.getFromRedis(key);
+  } else {
+    try {
+      const options = {
+        url: `${process.env.FINTO_URL}${endpoint}`,
+        headers: {
+          "Accept": "application/rdf+xml"
         }
+      };
+
+      const body = await rp.get(options);
+
+      const parseOptions = {
+        tagNameProcessors: [processors.stripPrefix],
+        attrNameProcessors: [processors.stripPrefix],
+        valueProcessors: [processors.stripPrefix],
+        attrValueProcessors: [processors.stripPrefix]
+      };
+
+      const data = await new Promise((resolve, reject) => {
+        parseString(body, parseOptions, (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            const data: object[] = [];
+
+            result.RDF.Concept.map((concept: any) => {
+              const label = concept.prefLabel.find((e: any) => e.$.lang === lang);
+
+              data.push({
+                "arvo": concept.$.about,
+                "selite": label._,
+              });
+            });
+
+            resolve(data);
+          }
+        });
+
+        return data;
       });
-    });
-  } catch (error) {
-    console.log(error);
+
+      console.log(data);
+
+      await client.setToRedis(key, data);
+
+      return client.getFromRedis(key);
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
 export const getYsoOntologia = async (req: Request, res: Response) => {
-  res.status(200).json(await getData("/yso/data", "fi"));
+  res.status(200).json(await getData("/yso/data", "fi", "yso"));
 };
