3d87d32444357473791b40bc3a859d7c0a406802
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-16 12:53:22 +0200

    Now gets all paginated results

--- src/controllers/eperusteet-amosaa.ts
+++ src/controllers/eperusteet-amosaa.ts
@@ -3,17 +3,29 @@ import rp from "request-promise";
 
 async function getData(endpoint: string, lang: string) {
   try {
-    const options = {
-      url: process.env.EPERUSTEET_AMOSAA_URL + endpoint,
-      headers: {
-        "Accept": "application/json"
+    const data: object[] = [];
+    const pageSize: number = 100;
+    let page: number = 0;
+    let getResults: boolean = true;
+
+    while (getResults) {
+      let body = await rp.get({
+        url: `${process.env.EPERUSTEET_AMOSAA_URL}${endpoint}?sivukoko=${pageSize}&sivu=${page}`,
+        headers: {
+          "Accept": "application/json"
+        }
+      });
+
+      body = JSON.parse(body);
+      await data.push.apply(data, body.data);
+      page = body.sivu + 1;
+
+      if (body.data.length < pageSize) {
+        getResults = false;
       }
-    };
+    }
 
-    const body = await rp.get(options);
-    const results = JSON.parse(body);
-
-    return results.data.map((koodi: any) => {
+    return data.map((koodi: any) => {
       return {
         "arvo": koodi.perusteDiaarinumero,
         "selite": koodi.nimi[lang],
