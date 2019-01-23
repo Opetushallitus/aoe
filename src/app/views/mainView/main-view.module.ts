5be94165cee365190e17db1491319c70bb46a647
kallelehtonen <coadiespla@gmail.com>
2019-01-23 15:09:31 +0200

    Replaced dummy content with demo-material-list

--- src/app/views/mainView/main-view.module.ts
+++ src/app/views/mainView/main-view.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
@@ -6,10 +7,13 @@ import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 import { MainViewComponent } from './main-view.component';
 import { MainViewRoutingModule } from './main-view-routing.module';
+import { TruncatePipe } from '../../pipes/truncate.pipe';
 import { OppimateriaaliCardComponent } from '../../components/oppimateriaali-card/oppimateriaali-card.component';
+import { DemoMaterialListComponent } from '../demo-material-list/demo-material-list.component';
 
 @NgModule({
   imports: [
+    CommonModule,
     FormsModule,
     MainViewRoutingModule,
     ChartsModule,
@@ -18,7 +22,9 @@ import { OppimateriaaliCardComponent } from '../../components/oppimateriaali-car
   ],
   declarations: [
     MainViewComponent,
-    OppimateriaaliCardComponent
+    TruncatePipe,
+    OppimateriaaliCardComponent,
+    DemoMaterialListComponent
   ]
 })
 export class MainViewModule { }
