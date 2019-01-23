80b2827972e5a19244bbd73091fedd08a89e4e67
kallelehtonen <coadiespla@gmail.com>
2019-01-23 15:12:53 +0200

    Now uses input binding to receive data

--- src/app/components/oppimateriaali-card/oppimateriaali-card.component.ts
+++ src/app/components/oppimateriaali-card/oppimateriaali-card.component.ts
@@ -1,14 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { DemoMaterial } from '../../models/demo-material';
 
 @Component({
   selector: 'app-oppimateriaali-card',
   templateUrl: './oppimateriaali-card.component.html'
 })
-export class OppimateriaaliCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class OppimateriaaliCardComponent {
+  @Input() demoMaterial: DemoMaterial;
 }
