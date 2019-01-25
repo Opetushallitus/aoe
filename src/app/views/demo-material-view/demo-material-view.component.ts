6be512087c7287dd24d476e2a8db4af946892271
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-25 15:32:32 +0200

    Added goBack functionality

--- src/app/views/demo-material-view/demo-material-view.component.ts
+++ src/app/views/demo-material-view/demo-material-view.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { DEMOMATERIALS } from '../demo-material-list/mock-demo-materials';
 import { DemoMaterial } from '../../models/demo-material';
 
@@ -12,12 +14,19 @@ export class DemoMaterialViewComponent implements OnInit {
   demoMaterials: DemoMaterial[] = DEMOMATERIALS;
   demoMaterial: DemoMaterial;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.demoMaterial = this.demoMaterials.find(m => m.id === +params['id']);
     });
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
