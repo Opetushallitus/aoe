7ec19178f59bb2d5ee7d8900715aafe5ead1e756
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-01-25 12:53:59 +0200

    Added TranslateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   // tslint:disable-next-line
@@ -7,7 +8,13 @@ import { Router, NavigationEnd } from '@angular/router';
   template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements OnInit {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private translate: TranslateService) {
+    translate.addLangs(['fi', 'en', 'sv']);
+    translate.setDefaultLang('fi');
+
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/fi|en|sv/) ? browserLang : 'fi');
+  }
 
   ngOnInit() {
     this.router.events.subscribe((evt) => {
--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,11 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './default-layout.component.html'
 })
 export class DefaultLayoutComponent {
-  constructor() {
-
-  }
+  constructor(private translate: TranslateService) { }
 }
