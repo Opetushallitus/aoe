a0e92e00e5630e7d438605fb9c00980a4bba1bbc
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-02-12 15:42:45 +0200

    Removed resource type icons and added i18n education materials

--- src/app/views/educational-material-view/educational-material-view.component.ts
+++ src/app/views/educational-material-view/educational-material-view.component.ts
@@ -1,42 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-
-import { EDUCATIONALMATERIALS } from '../../mocks/demo/educational-materials-fi.mock';
-import { LEARNINGRESOURCETYPEICONS } from '../../mocks/learning-resource-type-icons.mock';
+import { TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 import { EducationalMaterial } from '../../models/demo/educational-material';
-import { LearningResourceTypeIcon } from '../../models/learning-resource-type-icon';
+import { EDUCATIONALMATERIALS as EducationalMaterialsFI } from '../../mocks/demo/educational-materials-fi.mock';
+import { EDUCATIONALMATERIALS as EducationalMaterialsEN } from '../../mocks/demo/educational-materials-en.mock';
 
 @Component({
   selector: 'app-demo-material-view',
   templateUrl: './educational-material-view.component.html',
 })
-export class EducationalMaterialViewComponent implements OnInit {
-
-  educationalMaterials: EducationalMaterial[] = EDUCATIONALMATERIALS;
-  educationalMaterial: EducationalMaterial;
-  learningResourceTypeIcons: LearningResourceTypeIcon[] = LEARNINGRESOURCETYPEICONS;
+export class EducationalMaterialViewComponent implements OnInit, OnDestroy {
+  private educationalMaterials: EducationalMaterial[];
+  private materials = {
+    'fi': EducationalMaterialsFI,
+    'en': EducationalMaterialsEN,
+  };
+  public educationalMaterial: EducationalMaterial;
+  private subscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
-    private location: Location
+    private location: Location,
+    private translate: TranslateService
   ) { }
 
-  ngOnInit() {
-    this.route.params.subscribe(params => {
+  ngOnInit(): void {
+    // Initialize education materials
+    this.educationalMaterials = this.materials[this.translate.currentLang];
+
+    this.subscription = this.route.params.subscribe(params => {
       this.educationalMaterial = this.educationalMaterials.find(m => m.id === +params['id']);
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 
   goBack(): void {
     this.location.back();
   }
-
-  getLearningResourceTypeIcon(learningResourceType: string): string {
-    const learningResourceTypeIcon = this.learningResourceTypeIcons.find(lrti => lrti.type === learningResourceType);
-
-    return learningResourceTypeIcon.icon;
-  }
-
 }
