6eceb707a572b46f45857e9a6d9770fba54c6490
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-02-12 14:54:54 +0200

    Internationalized education materials list

--- src/app/components/educational-materials-list/educational-materials-list.component.ts
+++ src/app/components/educational-materials-list/educational-materials-list.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Subscription } from 'rxjs';
 
 import { EducationalMaterial } from '../../models/demo/educational-material';
-import { EDUCATIONALMATERIALS } from '../../mocks/demo/educational-materials-fi.mock';
+import { EDUCATIONALMATERIALS as EducationalMaterialsFI } from '../../mocks/demo/educational-materials-fi.mock';
+import { EDUCATIONALMATERIALS as EducationalMaterialsEN } from '../../mocks/demo/educational-materials-en.mock';
 
 @Component({
   selector: 'app-educational-materials-list',
   templateUrl: './educational-materials-list.component.html',
 })
-export class EducationalMaterialsListComponent {
-  educationalMaterials: EducationalMaterial[] = EDUCATIONALMATERIALS;
+export class EducationalMaterialsListComponent implements OnInit, OnDestroy {
+  public educationalMaterials: EducationalMaterial[];
+  private materials = {
+    'fi': EducationalMaterialsFI,
+    'en': EducationalMaterialsEN,
+  };
+  private subscription: Subscription;
+
+  constructor(private translate: TranslateService) { }
+
+  ngOnInit(): void {
+    // Initialize education materials
+    this.educationalMaterials = this.materials[this.translate.currentLang];
+
+    // Subscribe to language changes
+    this.subscription = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.educationalMaterials = this.materials[event.lang];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
