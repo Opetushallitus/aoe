6c6b9985336a1b0b757c091729cf85cbc6e3cd40
Kalle Lehtonen <coadiespla@gmail.com>
2019-02-27 13:38:45 +0200

    Added logic for limiting amount of tags shown on card

--- src/app/components/educational-material-card/educational-material-card.component.ts
+++ src/app/components/educational-material-card/educational-material-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { EducationalMaterial } from '../../models/demo/educational-material';
 import { LearningResourceTypeService } from '../../services/learning-resource-type.service';
@@ -7,8 +7,30 @@ import { LearningResourceTypeService } from '../../services/learning-resource-ty
   selector: 'app-educational-material-card',
   templateUrl: './educational-material-card.component.html',
 })
-export class EducationalMaterialCardComponent {
+export class EducationalMaterialCardComponent implements OnInit {
   @Input() educationalMaterial: EducationalMaterial;
+  public keywords: object[];
+  public educationalLevels: object[];
 
   constructor(public lrtSvc: LearningResourceTypeService) { }
+
+  ngOnInit(): void {
+    this.keywords = this.getValuesWithinLimits(this.educationalMaterial.keywords);
+    this.educationalLevels = this.getValuesWithinLimits(this.educationalMaterial.educationalLevel);
+  }
+
+  private getValuesWithinLimits(input: object[]): object[] {
+    const charLimit = 60;
+    let usedChars = 0;
+    const values = [];
+
+    input.forEach(row => {
+      if (usedChars + row['value'].length <= charLimit) {
+        values.push(row);
+        usedChars += row['value'].length;
+      }
+    });
+
+    return values;
+  }
 }
