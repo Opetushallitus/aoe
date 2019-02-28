d0c599a272e9784ef240a656fe093c82be2d78c1
kallelehtonen <coadiespla@gmail.com>
2019-02-28 10:31:25 +0200

    Subscribed to langChange events

--- src/app/views/educational-material-view/educational-material-view.component.ts
+++ src/app/views/educational-material-view/educational-material-view.component.ts
@@ -8,6 +8,7 @@ import { LearningResourceTypeService } from '../../services/learning-resource-ty
 import { EducationalMaterial } from '../../models/demo/educational-material';
 import { EDUCATIONALMATERIALS } from '../../mocks/demo/educational-materials.mock';
 import { Material } from '../../models/demo/material';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-demo-material-view',
@@ -16,25 +17,39 @@ import { Material } from '../../models/demo/material';
 export class EducationalMaterialViewComponent implements OnInit, OnDestroy {
   private educationalMaterials: EducationalMaterial[] = EDUCATIONALMATERIALS;
   public educationalMaterial: EducationalMaterial;
-  private subscription: Subscription;
+  private routeSubscription: Subscription;
+  private langChangeSubscription: Subscription;
   public previewMaterial: Material;
+  private specialId: number;
 
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    public lrtSvc: LearningResourceTypeService
+    public lrtSvc: LearningResourceTypeService,
+    private translate: TranslateService
   ) { }
 
   ngOnInit(): void {
-    this.subscription = this.route.params.subscribe(params => {
-      this.educationalMaterial = this.educationalMaterials.find(m => m.id === +params['id']);
+    this.routeSubscription = this.route.params.subscribe(params => {
+      this.specialId = +params['specialId'];
+
+      this.educationalMaterial = this.educationalMaterials.find(m =>
+        m.specialId === this.specialId && m.inLanguage.id.toLocaleLowerCase() === this.translate.currentLang);
 
       this.previewMaterial = this.educationalMaterial.materials[0];
+    });
+
+    this.langChangeSubscription = this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.educationalMaterial = this.educationalMaterials.find(m =>
+       m.specialId === this.specialId && m.inLanguage.id.toLocaleLowerCase() === event.lang);
+
+       this.previewMaterial = this.educationalMaterial.materials[0];
     });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
+    this.langChangeSubscription.unsubscribe();
   }
 
   goBack(): void {
