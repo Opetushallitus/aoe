2f015df77e8d2df1235afaf5286fe222f4d6715d
Kalle Lehtonen <coadiespla@gmail.com>
2019-03-01 12:37:11 +0200

    Added english texts, and made links work

--- src/app/mocks/demo/home-page.mock.ts
+++ src/app/mocks/demo/home-page.mock.ts
@@ -10,6 +10,16 @@ export const HOMEPAGE = {
       'Vuoden 2019 lopulla julkaistaan oppimateriaalipalvelun ensimmäinen tuotantoversio, josta käyttäjät voivat hakea keskitetysti avoimia oppimateriaaleja ja lisätä omien avoimien oppimateriaaliensa tietoja.',
     ],
   },
-  en: {},
+  en: {
+    title: 'Welcome to aoe.fi!',
+    paragraphs: [
+      // tslint:disable-next-line
+      'The Ministry of Education and Culture and the Finnish National Agency for Education are developing a national service for open educational resources (OERs). The service is coordinated by CSC – IT Center for Science and it is intended for all levels of education to promote the use of open educational resources. The service and the OERs are free to use for teachers, learners and for all citizens.',
+      // tslint:disable-next-line
+      'Aoe.fi provides you information about the development of the service. Here you will also find OERs intended for competence development for teachers’ from different educational levels.',
+      // tslint:disable-next-line
+      'In the end of 2019 the first production version of the service will be released. The service will offer a shared data warehouse for searching for and saving open educational resources and their metadata. Users can save their own learning resources and the associated metadata in the service.',
+    ],
+  },
   sv: {},
 };
