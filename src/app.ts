2b3f978ab1d516d2b2d974f195d095a1addb969e
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-03-06 15:08:51 +0200

    Set yso data on startup

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,11 @@ import compression from "compression"; // compress requests
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import session from "express-session";
+import cors from "cors";
 
 import router from "./routes";
+
+import { setYsoData } from "./controllers/finto";
 
 dotenv.config();
 
@@ -23,7 +26,14 @@ app.use(session({
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors()); // Enable CORS for dev purposes
 app.set("port", 3000);
+
+// Init
+// set data to redis (if not already set)
+setYsoData("/yso/data");
+
+// set cron jobs to run daily/weekly
 
 // Prefixed routes
 app.use("/api/v1", router);
