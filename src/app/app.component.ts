1cc734edb74c4c8f81bd27405c8196d6dd085d37
kallelehtonen <coadiespla@gmail.com>
2019-03-12 08:45:51 +0200

    Added logic for changing html lang attribute

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
+
 import { getLanguage, setLanguage } from './shared/shared.module';
 
 @Component({
@@ -11,7 +13,9 @@ import { getLanguage, setLanguage } from './shared/shared.module';
 export class AppComponent implements OnInit {
   constructor(
     private router: Router,
-    private translate: TranslateService
+    private translate: TranslateService,
+    @Inject(DOCUMENT) doc: Document,
+    private renderer: Renderer2
   ) {
     translate.addLangs(['fi', 'en', 'sv']);
     translate.setDefaultLang('fi');
@@ -26,6 +30,10 @@ export class AppComponent implements OnInit {
     } else {
       translate.use(lang);
     }
+
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      renderer.setAttribute(doc.documentElement, 'lang', event.lang);
+    });
 
     //  Google Analytics
     this.router.events.subscribe(event => {
