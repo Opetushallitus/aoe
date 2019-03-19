09b5b83f6781d6364ea471d38f2327f320716173
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-03-19 13:31:16 +0200

    Started refactoring routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,20 @@ import { Router } from "express";
 
 import { deleteKey, getData } from "./controllers/common";
 import { getTieteenalat } from "./controllers/tilastokeskus";
+import { getKoulutusaste, getKoulutusasteet, getKoulutusasteetChildren } from "./controllers/koulutusasteet";
 
 const router: Router = Router();
 
 // GET routes
 /**
  * Palauttaa tietueen redis tietokannasta
- * @route GET /{avain}/{kieli}
- * @param {string} avain.path.required - Redis avain
- * @param {string} kieli.path.required - ISO 639-1 standardin mukainen kielitunnus
+ * @route GET /{key}/{lang}
+ * @param {string} key.path.required - Redis key
+ * @param {string} lang.path.required - ISO 639-1 standardin mukainen langtunnus
  * @returns {object} 200 - OK
  * @returns {error} 404 - Not Found
  */
-router.get("/:key/:lang", getData);
+router.get("/legacy/:key/:lang", getData);
 
 /**
  * @typedef Koodi
@@ -28,7 +29,7 @@ router.get("/:key/:lang", getData);
  * @returns {Array.<Tieteenalat>} 200 - OK
  * @returns {error} 404 - Not Found
  */
-router.get("/tieteenalat", getTieteenalat);
+router.get("/legacy/tieteenalat", getTieteenalat);
 
 /**
  * @typedef Tieteenalat
@@ -47,11 +48,48 @@ router.get("/tieteenalat", getTieteenalat);
 // DELETE routes
 /**
  * Poistaa tietueen redis tietokannasta
- * @route DELETE /redis/delete/{avain}
- * @param {string} avain.path.required - Redis avain
+ * @route DELETE /redis/delete/{key}
+ * @param {string} key.path.required - Redis key
  * @returns {object} 200 - OK
  * @returns {error} 404 - Not Found
  */
 router.delete("/redis/delete/:key", deleteKey);
+
+// Refactor everything
+
+// router.get("/asiasanat/:lang");
+// router.get("/asiasanat/:lang/:key");
+//
+// router.get("/oppimateriaalityypit/:lang");
+// router.get("/oppimateriaalityypit/:lang/:key");
+
+router.get("/koulutusasteet/:lang", getKoulutusasteet);
+router.get("/koulutusasteet/single/:key/:lang", getKoulutusaste);
+router.get("/koulutusasteet/children/:key/:lang", getKoulutusasteetChildren);
+
+// router.get("/tieteenalat/:lang");
+// router.get("/tieteenalat/:lang/:key");
+// router.get("/tieteenalat/:lang/koulutusasteet/:koulutusasteet");
+//
+// router.get("/kohderyhmat/:lang");
+// router.get("/kohderyhmat/:lang/:key");
+//
+// router.get("/opiskelumuodot/:lang");
+// router.get("/opiskelumuodot/:lang/:key");
+//
+// router.get("/saavutettavuudentukitoiminnot/:lang");
+// router.get("/saavutettavuudentukitoiminnot/:lang/:key");
+//
+// router.get("/saavutettavuudenavustavatteknologiat/:lang");
+// router.get("/saavutettavuudenavustavatteknologiat/:lang/:key");
+//
+// router.get("/saavutettavuudenkayttotavat/:lang");
+// router.get("/saavutettavuudenkayttotavat/:lang/:key");
+//
+// router.get("/saavutettavuudenesteet/:lang");
+// router.get("/saavutettavuudenesteet/:lang/:key");
+//
+// router.get("/kielet/:lang");
+// router.get("/kielet/:lang/:key");
 
 export default router;
