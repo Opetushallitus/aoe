a74b0ed9771587007119ea98a6894aaf4148d66d
Kalle Lehtonen <coadiespla@gmail.com>
2019-03-21 13:47:41 +0200

    Added routes for add material form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -9,6 +9,11 @@ import { LoginComponent } from './views/login/login.component';
 import { EducationalMaterialViewComponent } from './views/educational-material-view/educational-material-view.component';
 import { InfoViewComponent } from './views/info-view/info-view.component';
 import { KoodistoTestiComponent } from './views/koodisto-testi/koodisto-testi.component';
+import { BasicDetailsComponent } from './views/add-educational-resource-form/basic-details/basic-details.component';
+import { EducationalDetailsComponent } from './views/add-educational-resource-form/educational-details/educational-details.component';
+import { ExtendedDetailsComponent } from './views/add-educational-resource-form/extended-details/extended-details.component';
+import { LicenseDetailsComponent } from './views/add-educational-resource-form/license-details/license-details.component';
+import { BasedOnDetailsComponent } from './views/add-educational-resource-form/based-on-details/based-on-details.component';
 
 export const routes: Routes = [
   {
@@ -52,6 +57,36 @@ export const routes: Routes = [
       {
         path: 'koodisto-testi',
         component: KoodistoTestiComponent,
+      },
+      {
+        path: 'lisaa-oppimateriaali',
+        children: [
+          {
+            path: '',
+            redirectTo: 'perustiedot',
+            pathMatch: 'full',
+          },
+          {
+            path: 'perustiedot',
+            component: BasicDetailsComponent,
+          },
+          {
+            path: 'koulutustiedot',
+            component: EducationalDetailsComponent,
+          },
+          {
+            path: 'tarkemmat-tiedot',
+            component: ExtendedDetailsComponent,
+          },
+          {
+            path: 'lisenssitiedot',
+            component: LicenseDetailsComponent,
+          },
+          {
+            path: 'hyodynnetyt-materiaalit',
+            component: BasedOnDetailsComponent,
+          },
+        ],
       },
     ],
   },
