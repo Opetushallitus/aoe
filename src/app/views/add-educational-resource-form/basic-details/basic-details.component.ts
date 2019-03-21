acf586c2308e912ca8ad09e6513dbbbfd5c6bb7b
Kalle Lehtonen <coadiespla@gmail.com>
2019-03-21 15:56:57 +0200

    Added data

--- src/app/views/add-educational-resource-form/basic-details/basic-details.component.ts
+++ src/app/views/add-educational-resource-form/basic-details/basic-details.component.ts
@@ -1,14 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable, Subscription } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
 
 @Component({
   selector: 'app-basic-details',
   templateUrl: './basic-details.component.html',
 })
-export class BasicDetailsComponent implements OnInit {
+export class BasicDetailsComponent implements OnInit, OnDestroy {
+  private lang: string = this.translate.currentLang;
+  public organisations$: Observable<any>;
+  private organisationsSubscription: Subscription;
 
-  constructor() { }
+  constructor(
+    public koodistoProxySvc: KoodistoProxyService,
+    private translate: TranslateService
+  ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+
+    this.organisationsSubscription = this.koodistoProxySvc.getData('organisaatiot', this.lang).subscribe(data => {
+      this.organisations$ = data;
+    });
   }
 
+  ngOnDestroy(): void {
+    this.organisationsSubscription.unsubscribe();
+  }
 }
