5d62a4bd1083aa88d69012ee444f4889439c18f9
kallelehtonen <coadiespla@gmail.com>
2019-03-26 09:19:36 +0200

    Merged commits 1cc734e, ec8ce09 and 27cc8fb from master

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
-import { TranslateService } from '@ngx-translate/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { DOCUMENT } from '@angular/common';
+
 import { getLanguage, setLanguage } from './shared/shared.module';
 
 @Component({
@@ -11,7 +13,9 @@ import { getLanguage, setLanguage } from './shared/shared.module';
 export class AppComponent implements OnInit {
   constructor(
     private router: Router,
-    private translate: TranslateService
+    private translate: TranslateService,
+    @Inject(DOCUMENT) doc: Document,
+    private renderer: Renderer2
   ) {
     translate.addLangs(['fi', 'en', 'sv']);
     translate.setDefaultLang('fi');
@@ -27,7 +31,11 @@ export class AppComponent implements OnInit {
       translate.use(lang);
     }
 
-    //  Google Analytics
+    translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      renderer.setAttribute(doc.documentElement, 'lang', event.lang);
+    });
+
+    // Google Analytics
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         (<any>window).gtag('config', 'UA-135550416-1', { 'page_path': event.urlAfterRedirects });
