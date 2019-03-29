44d2dd04319d069d503e7b30a12cdab04e3f710b
Kalle Lehtonen <coadiespla@gmail.com>
2019-03-29 12:00:33 +0200

    Added keywords with virtual scroll

--- src/app/views/add-educational-resource-form/basic-details/basic-details.component.ts
+++ src/app/views/add-educational-resource-form/basic-details/basic-details.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, map, switchMap } from 'rxjs/operators';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
@@ -8,13 +9,19 @@ import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
   selector: 'app-basic-details',
   templateUrl: './basic-details.component.html',
 })
-export class BasicDetailsComponent implements OnInit, OnDestroy {
+export class BasicDetailsComponent implements OnInit {
   private lang: string = this.translate.currentLang;
   public organisations$: Observable<any>;
-  private organisationsSubscription: Subscription;
+
+  // https://stackblitz.com/edit/ng-select-infinite
+  private keywords = [];
+  public keywordsBuffer = [];
+  private bufferSize = 50;
+  public loading = false;
+  public input$ = new Subject<string>();
 
   constructor(
-    public koodistoProxySvc: KoodistoProxyService,
+    private koodistoProxySvc: KoodistoProxyService,
     private translate: TranslateService
   ) { }
 
@@ -23,12 +30,41 @@ export class BasicDetailsComponent implements OnInit, OnDestroy {
       this.lang = event.lang;
     });
 
-    this.organisationsSubscription = this.koodistoProxySvc.getData('organisaatiot', this.lang).subscribe(data => {
-      this.organisations$ = data;
+    this.organisations$ = this.koodistoProxySvc.getData('organisaatiot', this.lang);
+
+    this.koodistoProxySvc.getData('asiasanat', this.lang).subscribe(keywords => {
+      this.keywords = keywords;
+    });
+
+    this.onSearch();
+  }
+
+  public fetchMore(value: string) {
+    const len = this.keywordsBuffer.length;
+    const more = this.keywords
+      .filter(x => x.value.includes(value))
+      .slice(len, this.bufferSize + len);
+
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      this.keywordsBuffer = this.keywordsBuffer.concat(more);
+    }, 200);
+  }
+
+  private onSearch() {
+    this.input$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      switchMap(value => this.fakeService(value))
+    ).subscribe(data => {
+      this.keywordsBuffer = data.slice(0, this.bufferSize);
     });
   }
 
-  ngOnDestroy(): void {
-    this.organisationsSubscription.unsubscribe();
+  private fakeService(value: string) {
+    return this.koodistoProxySvc.getData('asiasanat', this.lang)
+      .pipe(map(data => data.filter((x: { value: string }) => x.value.includes(value))));
   }
 }
