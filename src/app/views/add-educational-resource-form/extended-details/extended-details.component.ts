ea1c7455d29353392175d115d147b39300859b53
Kalle Lehtonen <coadiespla@gmail.com>
2019-04-03 10:10:04 +0300

    Started working with extended details form

--- src/app/views/add-educational-resource-form/extended-details/extended-details.component.ts
+++ src/app/views/add-educational-resource-form/extended-details/extended-details.component.ts
@@ -1,14 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
 
 @Component({
   selector: 'app-extended-details',
   templateUrl: './extended-details.component.html',
 })
 export class ExtendedDetailsComponent implements OnInit {
+  private lang: string = this.translate.currentLang;
+  public educationalRoles$: Observable<any>;
+  public educationalUse$: Observable<any>;
+  public accessibilityFeatures$: Observable<any>;
+  public accessibilityAPIs$: Observable<any>;
+  public accessibilityControls$: Observable<any>;
+  public accessibilityHazards$: Observable<any>;
 
-  constructor() { }
+  extendedDetailsForm = new FormGroup({
+    educationalRoles: new FormControl(''),
+    educationalUse: new FormControl(''),
+    accessibilityFeatures: new FormControl(''),
+    accessibilityAPIs: new FormControl(''),
+    accessibilityControls: new FormControl(''),
+    accessibilityHazards: new FormControl(''),
+  });
+
+  constructor(
+    private koodistoProxySvc: KoodistoProxyService,
+    private translate: TranslateService,
+  ) { }
 
   ngOnInit() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+
+    this.educationalRoles$ = this.koodistoProxySvc.getData('kohderyhmat', this.lang);
+    this.educationalUse$ = this.koodistoProxySvc.getData('kayttokohteet', this.lang);
+    this.accessibilityFeatures$ = this.koodistoProxySvc.getData('saavutettavuudentukitoiminnot', this.lang);
+    this.accessibilityAPIs$ = this.koodistoProxySvc.getData('saavutettavuudenavustavatteknologiat', this.lang);
+    this.accessibilityControls$ = this.koodistoProxySvc.getData('saavutettavuudenkayttotavat', this.lang);
+    this.accessibilityHazards$ = this.koodistoProxySvc.getData('saavutettavuudenesteet', this.lang);
   }
 
+  onSubmit() {
+    console.warn(this.extendedDetailsForm.value);
+  }
 }
