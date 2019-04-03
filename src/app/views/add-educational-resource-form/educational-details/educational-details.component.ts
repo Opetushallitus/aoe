cbacc95b879c5b84f08041d2ee3a53f74416817e
Kalle Lehtonen <coadiespla@gmail.com>
2019-04-03 09:52:40 +0300

    Started working with educational details form

--- src/app/views/add-educational-resource-form/educational-details/educational-details.component.ts
+++ src/app/views/add-educational-resource-form/educational-details/educational-details.component.ts
@@ -1,14 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
 
 @Component({
   selector: 'app-educational-details',
   templateUrl: './educational-details.component.html',
 })
 export class EducationalDetailsComponent implements OnInit {
+  private lang: string = this.translate.currentLang;
+  public educationalLevels$: Observable<any>;
 
-  constructor() { }
+  educationalDetailsForm = new FormGroup({
+    educationalLevels: new FormControl(''),
+  });
+
+  constructor(
+    private koodistoProxySvc: KoodistoProxyService,
+    private translate: TranslateService
+  ) { }
 
   ngOnInit() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+
+    this.educationalLevels$ = this.koodistoProxySvc.getData('koulutusasteet', this.lang);
   }
 
+  onSubmit() {
+    console.warn(this.educationalDetailsForm.value);
+  }
 }
