fe1bc7f635df7978f523197aba16cb150f4fc882
kallelehtonen <coadiespla@gmail.com>
2019-04-05 13:28:12 +0300

    Added based on form

--- src/app/views/add-educational-resource-form/based-on-details/based-on-details.component.ts
+++ src/app/views/add-educational-resource-form/based-on-details/based-on-details.component.ts
@@ -1,14 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+import { KoodistoProxyService } from '../../../services/koodisto-proxy.service';
 
 @Component({
   selector: 'app-based-on-details',
   templateUrl: './based-on-details.component.html',
 })
 export class BasedOnDetailsComponent implements OnInit {
+  private lang: string = this.translate.currentLang;
 
-  constructor() { }
+  basedOnDetailsForm = new FormGroup({
+    author: new FormControl(''),
+    material: new FormControl(''),
+  });
+
+  constructor(
+    private koodistoProxySvc: KoodistoProxyService,
+    private translate: TranslateService,
+  ) { }
 
   ngOnInit() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
   }
 
+  onSubmit() {
+    console.warn(this.basedOnDetailsForm.value);
+  }
 }
