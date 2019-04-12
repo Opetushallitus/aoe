edb699366e1a14cb4fe0f7c0e0e1cab14f8b7a32
Kalle Lehtonen <coadiespla@gmail.com>
2019-04-12 14:54:53 +0300

    Started working on file upload

--- src/app/views/add-educational-resource-form/file-upload/file-upload.component.ts
+++ src/app/views/add-educational-resource-form/file-upload/file-upload.component.ts
@@ -1,12 +1,57 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-file-upload',
   templateUrl: './file-upload.component.html',
 })
 export class FileUploadComponent implements OnInit {
-  constructor() { }
+  private localStorageKey = 'aoe.new-educational-resource';
+  public submitted = false;
+  public fileUploadForm: FormGroup;
+  public files: FormArray;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    this.fileUploadForm = this.fb.group({
+      files: this.fb.array([ this.createFile() ]),
+    });
+  }
+
+  createFile(): FormGroup {
+    return this.fb.group({
+      file: null,
+      subtitles: this.fb.array([ this.createSubtitle()]),
+    });
+  }
+
+  addFile(): void {
+    this.files = this.fileUploadForm.get('files') as FormArray;
+    this.files.push(this.createFile());
+  }
+
+  createSubtitle(): FormGroup {
+    return this.fb.group({
+      file: null,
+      lang: null,
+    });
+  }
+
+  // addSubtitle(): void {
+  //   this.files = this.fileUploadForm.get('subtitles') as FormArray;
+  //   this.files.push(this.createSubtitle());
+  // }
+
+  onSubmit() {
+    this.submitted = true;
+
+    console.log(this.fileUploadForm.value);
+  }
+
+  // @todo: some kind of confirmation
+  resetForm() {
+    // reset form values
+    this.fileUploadForm.reset();
   }
 }
