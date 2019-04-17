6679ee74b33e0ce43df79629adf204e983cb993c
Kalle Lehtonen <coadiespla@gmail.com>
2019-04-17 13:01:17 +0300

    Cleaned up form component

--- src/app/views/educational-resource-form/educational-resource-form.component.ts
+++ src/app/views/educational-resource-form/educational-resource-form.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 import { TabsetComponent } from 'ngx-bootstrap';
 
 @Component({
@@ -9,27 +8,11 @@ import { TabsetComponent } from 'ngx-bootstrap';
 export class EducationalResourceFormComponent implements OnInit {
   @ViewChild('formTabs') formTabs: TabsetComponent;
 
-  public fileUploadForm: FormGroup;
+  constructor() { }
 
-  constructor(private fb: FormBuilder) { }
+  ngOnInit() { }
 
-  ngOnInit() {
-    this.fileUploadForm = this.fb.group({
-      files: this.fb.array([
-        this.fb.control(null)
-      ]),
-    });
-  }
-
-  get files() {
-    return this.fileUploadForm.get('files') as FormArray;
-  }
-
-  addFile() {
-    this.files.push(this.fb.control(null));
-  }
-
-  public onFilesSubmit() {
-    console.log(this.fileUploadForm.value);
+  private selectTab(tabId: number): void {
+    this.formTabs.tabs[tabId].active = true;
   }
 }
