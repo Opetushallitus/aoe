ce63e08956c32423678c5a38d2883b23ab58549b
Kalle Lehtonen <coadiespla@gmail.com>
2019-04-24 15:00:08 +0300

    Added save data logic

--- src/app/views/educational-resource-form/tabs/extended-details/extended-details.component.ts
+++ src/app/views/educational-resource-form/tabs/extended-details/extended-details.component.ts
@@ -13,7 +13,11 @@ import { KoodistoProxyService } from '../../../../services/koodisto-proxy.servic
 export class ExtendedDetailsComponent implements OnInit {
   @Input() tabs: TabsetComponent;
 
+  private localStorageKey = 'aoe.new-educational-resource';
+  public submitted = false;
   private lang: string = this.translate.currentLang;
+  private savedData = JSON.parse(localStorage.getItem(this.localStorageKey));
+
   public educationalRoles$: Observable<any>;
   public educationalUse$: Observable<any>;
   public accessibilityFeatures$: Observable<any>;
@@ -22,7 +26,7 @@ export class ExtendedDetailsComponent implements OnInit {
   public accessibilityHazards$: Observable<any>;
   public languages$: Observable<any>;
 
-  extendedDetailsForm = new FormGroup({
+  public extendedDetailsForm = new FormGroup({
     educationalRoles: new FormControl(''),
     educationalUse: new FormControl(''),
     accessibilityFeatures: new FormControl(''),
@@ -57,9 +61,55 @@ export class ExtendedDetailsComponent implements OnInit {
     this.accessibilityHazards$ = this.koodistoProxySvc.getData('saavutettavuudenesteet', this.lang);
 
     this.languages$ = this.koodistoProxySvc.getData('kielet', this.lang);
+
+    if (this.savedData) {
+      this.extendedDetailsForm.get('educationalRoles').setValue(this.savedData.educationalRole);
+      this.extendedDetailsForm.get('educationalUse').setValue(this.savedData.educationalUse);
+      this.extendedDetailsForm.get('accessibilityFeatures').setValue(this.savedData.accessibilityFeature);
+      this.extendedDetailsForm.get('accessibilityAPIs').setValue(this.savedData.accessibilityAPI);
+      this.extendedDetailsForm.get('accessibilityControls').setValue(this.savedData.accessibilityControl);
+      this.extendedDetailsForm.get('accessibilityHazards').setValue(this.savedData.accessibilityHazard);
+      this.extendedDetailsForm.get('typicalAgeRangeMin').setValue(this.savedData.typicalAgeRange[0].min);
+      this.extendedDetailsForm.get('typicalAgeRangeMax').setValue(this.savedData.typicalAgeRange[0].max);
+      this.extendedDetailsForm.get('inLanguage').setValue(this.savedData.inLanguage);
+    }
   }
 
   onSubmit() {
-    console.warn(this.extendedDetailsForm.value);
+    this.submitted = true;
+
+    if (!this.extendedDetailsForm.invalid) {
+      const newData = {
+        educationalRole: this.extendedDetailsForm.get('educationalRoles').value,
+        educationalUse: this.extendedDetailsForm.get('educationalUse').value,
+        accessibilityFeature: this.extendedDetailsForm.get('accessibilityFeatures').value,
+        accessibilityAPI: this.extendedDetailsForm.get('accessibilityAPIs').value,
+        accessibilityControl: this.extendedDetailsForm.get('accessibilityControls').value,
+        accessibilityHazard: this.extendedDetailsForm.get('accessibilityHazards').value,
+        typicalAgeRange: [{
+          min: this.extendedDetailsForm.get('typicalAgeRangeMin').value,
+          max: this.extendedDetailsForm.get('typicalAgeRangeMax').value,
+        }],
+        inLanguage: this.extendedDetailsForm.get('inLanguage').value,
+      };
+
+      const data = Object.assign({}, this.savedData, newData);
+
+      // save data to local storage
+      localStorage.setItem(this.localStorageKey, JSON.stringify(data));
+    }
+  }
+
+  // @todo: some kind of confirmation
+  resetForm() {
+    // reset form values
+    this.extendedDetailsForm.reset();
+
+    // clear data from local storage
+    localStorage.removeItem(this.localStorageKey);
+  }
+
+  previousTab() {
+    this.tabs.tabs[2].active = true;
   }
 }
