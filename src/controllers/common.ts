4d45c46442244b342bd731e22e69230dab46064f
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-05-08 14:53:33 +0300

    Removed legacy functions

--- src/controllers/common.ts
+++ src/controllers/common.ts
@@ -1,10 +1,6 @@
-import { NextFunction, Request, Response } from "express";
 import rp from "request-promise";
 
-import RedisWrapper from "../utils/redis-wrapper";
 import { HttpHeaders } from "../models/httpheaders";
-
-const client = new RedisWrapper();
 
 export async function getDataFromApi(api: string, route: string, headers: HttpHeaders): Promise<any> {
   const options = {
@@ -20,39 +16,3 @@ export async function getDataFromApi(api: string, route: string, headers: HttpHe
     return body;
   }
 }
-
-export const getData = async (req: Request, res: Response, next: NextFunction) => {
-  if (await client.exists(req.params.key) !== true) {
-    res.sendStatus(404);
-
-    return next();
-  }
-
-  const input = JSON.parse(await client.get(req.params.key));
-  const output: object[] = [];
-
-  input.map((row: any) => {
-    if (row.value[req.params.lang] !== undefined) {
-      output.push({
-        "key": row.key,
-        "value": row.value[req.params.lang],
-      });
-    }
-  });
-
-  if (output.length > 0) {
-    res.status(200).json(output);
-  } else {
-    res.sendStatus(404);
-  }
-};
-
-export const deleteKey = async (req: Request, res: Response) => {
-  const deleteStatus = client.del(req.params.key);
-
-  if (deleteStatus > 0) {
-    res.sendStatus(200);
-  } else {
-    res.sendStatus(404);
-  }
-};
