1a7b57d86605948cd26e984947a615aa4577bd30
Kalle Lehtonen <coadiespla@gmail.com>
2019-06-13 12:15:19 +0300

    Added higher education options

--- src/app/views/educational-resource-form/tabs/educational-details/educational-details.component.ts
+++ src/app/views/educational-resource-form/tabs/educational-details/educational-details.component.ts
@@ -19,9 +19,13 @@ export class EducationalDetailsComponent implements OnInit {
 
   public educationalDetailsForm: FormGroup;
   public hasBasicStudies = false;
+  public hasHigherEducation = false;
+  private basicStudyKeys: string[];
+  private higherEducationKeys: string[];
 
   public educationalLevels$: Observable<any>;
   public basicStudySubjects$: any[];
+  public branchesOfScience$: any[];
 
   constructor(
     private fb: FormBuilder,
@@ -37,6 +41,7 @@ export class EducationalDetailsComponent implements OnInit {
     this.educationalDetailsForm = this.fb.group({
       educationalLevels: this.fb.control(null),
       basicStudySubjects: this.fb.array([]),
+      branchesOfScience: this.fb.array([]),
     });
 
     this.educationalLevels$ = this.koodistoProxySvc.getData('koulutusasteet', this.lang);
@@ -46,6 +51,39 @@ export class EducationalDetailsComponent implements OnInit {
 
       this.basicStudySubjects$.forEach(() => this.basicStudySubjects.push(this.fb.control(false)));
     });
+
+    this.koodistoProxySvc.getData('tieteenalat', this.lang).subscribe(data => {
+      this.branchesOfScience$ = data;
+
+      this.branchesOfScience$.forEach(branch => {
+        const controls = this.fb.array([]);
+
+        branch.children.forEach(() => controls.push(this.fb.control(false)));
+
+        this.branchesOfScience.push(this.fb.group({
+          all: this.fb.control(false),
+          children: controls,
+        }));
+      });
+    });
+
+    // @todo: Replace with data from koodisto-service endpoint
+    this.basicStudyKeys = [
+      '8cb1a02f-54cb-499a-b470-4ee980519707',
+      '5410475a-a2fb-46d7-9eb4-c572b5d92dbb',
+      '7eb3d5be-0575-44db-ab8a-883cf0ae2f26',
+      '75e8bed1-b965-483d-8ba4-48a5614c69ba',
+      'a2a70a14-b150-4f37-9e20-2bbb71731807',
+      '14fe3b08-8516-4999-946b-96eb90c2d563',
+    ];
+
+    // @todo: Replace with data from koodisto-service endpoint
+    this.higherEducationKeys = [
+      'e5a48ada-3de0-4246-9b8f-32d4ff68e22f',
+      'ff3334db-2a71-4459-8f0d-c42ce2b12a70',
+      '9c14f097-68e3-4e6b-a772-71a44442f72f',
+      '7c722ac4-f06c-4f2a-a41f-b0c5aa10070a',
+    ];
   }
 
   get educationalLevels(): FormControl {
@@ -56,8 +94,14 @@ export class EducationalDetailsComponent implements OnInit {
     return this.educationalDetailsForm.get('basicStudySubjects') as FormArray;
   }
 
+  get branchesOfScience(): FormArray {
+    return this.educationalDetailsForm.get('branchesOfScience') as FormArray;
+  }
+
   educationalLevelsChange($event): void {
-    this.hasBasicStudies = $event.filter((e: any) => e.key === '8cb1a02f-54cb-499a-b470-4ee980519707').length > 0;
+    this.hasBasicStudies = $event.filter((e: any) => this.basicStudyKeys.includes(e.key)).length > 0;
+
+    this.hasHigherEducation = $event.filter((e: any) => this.higherEducationKeys.includes(e.key)).length > 0;
   }
 
   onSubmit() {
@@ -68,6 +112,13 @@ export class EducationalDetailsComponent implements OnInit {
       .filter(value => value !== null);
 
     console.log(selectedBasicStudySubjects);
+
+    // @todo: Check if all or loop children
+    const selectedBranchesOfScience = this.educationalDetailsForm.value.branchesOfScience
+      .map((branch, index) => branch.all ? this.branchesOfScience$[index].key : null)
+      .filter(value => value !== null);
+
+    console.log(selectedBranchesOfScience);
   }
 
   public previousTab() {
