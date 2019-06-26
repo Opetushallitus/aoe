a1f7bde932ec996a9b186773082a6a864e244909
kallelehtonen <coadiespla@gmail.com>
2019-06-26 11:22:52 +0300

    Imported missing browser animations module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
@@ -67,6 +68,7 @@ import { LicenseComponent } from './views/educational-resource-form/tabs/license
 @NgModule({
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     AppBreadcrumbModule.forRoot(),
     AppFooterModule,
