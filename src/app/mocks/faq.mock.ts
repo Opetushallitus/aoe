51cd584445bc5e0323724ad45953bf806bb1d286
kallelehtonen <coadiespla@gmail.com>
2019-06-27 16:34:18 +0300

    Added faqs

--- src/app/mocks/faq.mock.ts
+++ src/app/mocks/faq.mock.ts
@@ -1,31 +1,56 @@
 import { Faq } from '../models/faq';
 
+// tslint:disable:max-line-length
+
 /**
  * Frequently Asked Questions mock
  */
 export const FAQ: Faq[] = [
   {
     id: 1,
-    question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit?',
-    // tslint:disable-next-line
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada vehicula augue eget facilisis. Integer mi ante, condimentum quis malesuada sit amet, pretium vel nibh. Curabitur a congue tellus. Donec et orci ornare, commodo dolor sit amet, sollicitudin elit. Nam quis sapien odio. Fusce iaculis tortor nec nisl auctor, in fermentum nisi varius. Cras metus ligula, dapibus in condimentum non, porttitor quis risus. Curabitur suscipit interdum eros, id consequat justo interdum id. Nullam urna dui, malesuada non condimentum a, suscipit ut lorem.'
+    question: 'Mikä on avointen oppimateriaalien palvelu? Miksi ja mihin tarpeeseen avointen oppimateriaalien palvelua kehitetään?',
+    answer: [
+      'Avointen oppimateriaalien palvelu on opetus- ja kulttuuriministeriön ja Opetushallituksen palvelu, joka kokoaa avoimia oppimateriaaleja yhteen kaikkien koulutusasteiden yhteiseen näkymään. Palvelusta voi etsiä ja käyttää avoimia oppimateriaaleja ja sinne voi tallentaa omia avoimia oppimateriaaleja ja niiden tietoja.',
+      'Avointen oppimateriaalien palvelu pyrkii edistämään avointen oppimateriaalien käyttöä mahdollistaen eri hankkeissa tuotetuille ja yksittäisten ihmisten tuottamille materiaaleille pitkäikäisyyttä sekä näkyvyyttä. Tallentamalla oppimateriaalin palveluun ei tarvitse huolehtia siitä kuka vastaa että oppimateriaalia voi käyttää vielä pitkään hankkeen päättyymisen jälkeenkin niin omassa oppilaitoksessa kuin myös muissa oppilaitoksissa ja niiden ulkopuolella.',
+    ]
   },
   {
     id: 2,
-    question: 'Lorem ipsum dolor sit amet?',
-    // tslint:disable-next-line
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada vehicula augue eget facilisis. Integer mi ante, condimentum quis malesuada sit amet, pretium vel nibh. Curabitur a congue tellus. Donec et orci ornare, commodo dolor sit amet, sollicitudin elit.'
+    question: 'Mitä ovat avoimet oppimateriaalit?',
+    answer: [
+      'Avoimet oppimateriaalit (OER) ovat eri muotoisia digitaalisia tai muita opetukseen, kasvatukseen tai oppimiseen kohdennettuja materiaaleja, jotka on vapautettu public domainiin tai jotka on lisensoitu avoimella lisenssillä, joka antaa muille oikeuden maksuttomaan pääsyyn, käyttöön, muokkaukseen ja uudelleen jakamiseen olemattomin tai vähin rajoituksin.',
+      'Avointen oppimateriaalien palvelussa yksi avoin oppimateriaali on pedagogisesti järkevä kokonaisuus, joka on lisensoitu käyttäen Creative Commons -lisenssiä.',
+    ]
   },
   {
     id: 3,
-    question: 'Lorem ipsum dolor sit amet, consectetur?',
-    // tslint:disable-next-line
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin malesuada vehicula augue eget facilisis. Integer mi ante, condimentum quis malesuada sit amet, pretium vel nibh. Curabitur a congue tellus. Donec et orci ornare, commodo dolor sit amet, sollicitudin elit. Nam quis sapien odio. Fusce iaculis tortor nec nisl auctor, in fermentum nisi varius.'
+    question: 'Kenelle avointen oppimateriaalien palvelu on tarkoitettu?',
+    answer: [
+      'Avointen oppimateriaalien palvelu on suunnattu kaikille koulutusasteille ja jatkuvaan oppimiseen. Se palvelee niin opettajia, oppijoita kuin oppimisen tukijoita näiden etsiessä avoimia oppimateriaaleja oman opetuksen ja osaamisenkehittämisen tarpeisiin.',
+    ]
   },
   {
     id: 4,
-    question: 'Lorem ipsum dolor sit elit?',
-    // tslint:disable-next-line
-    answer: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    question: 'Vaatiiko palvelu tunnistautumista?',
+    answer: [
+      'Oppimateriaalien käyttäminen ja niiden hakeminen on kaikille mahdollista ilman sisäänkirjautumista.',
+      'Oppimateriaalien tallentaminen ja kommenttien jättäminen vaatii kirjautumisen. Tämä on mahdollista MPASSid:llä ja Hakalla sekä Suomi.fi-tunnistautumisella.',
+    ]
+  },
+  {
+    id: 5,
+    question: 'Mitä avointen oppimateriaalien palveluun tallennettavalta oppimateriaalilta vaaditaan?',
+    answer: [
+      'Avointen oppimateriaalien palveluun lisättävä oppimateriaali on oltava avoimesti lisensoitu. Palvelussa käytetään Creative Commons -lisenssejä, joista lisätietoa täältä: https://creativecommons.org/choose/?lang=fi',
+      'Jokaiseen lisättävään oppimateriaaliin on myös täydennettävä kuvailutietoa, jotka helpottavat oppimateriaalien löytämistä ja arviointia. Kuvailutiedot kertovat mm. kuka oppimateriaalin on tehnyt ja miten se opettaa suhteessa opetussuunnitelmiin. On myös hyvä huomioida materiaalin saavutettavuus. Lisätietoja: www.saavutettavasti.fi',
+    ]
+  },
+  {
+    id: 6,
+    question: 'Mitä ovat Creative Commons -lisenssit?',
+    answer: [
+      'Creative Commons lisenssien avulla jaat teoksen käyttäjille haluamasi vapaudet teokseesi, esimerkiksi oikeuden käyttää, jakaa tai muokata materiaalia. Creative Commons -lisenssit rakentuvat Nimeä-ehdon päälle, joka antaa muiden kopioida, välittää, levittää ja esittää sinun tekijänoikeuksiisi kuuluvaa teosta sekä sen pohjalta tehtyjä muokattuja versioita teoksestasi vain, jos he mainitsevat nimesi alkuperäisenä tekijänä. Tämän lisäksi lisenssiin voi valita lisäehtoja kuten kaupallisen käytön tai muokattujen teosten kieltäminen. Lisätietoa https://creativecommons.fi/lisenssit/',
+      'Mikäli oppimateriaalilla on useita tekijöitä tai omistajia, tulee avoimesta lisenssistä sopia kaikkien osapuolten kesken. Tämän voi tehdä esimerkiksi hyödyntämällä oheisia sopimuspohjia: https://www.opettajantekijanoikeus.fi/aineisto/sopimusmallit-avoimeen-sisallontuotantoon/',
+    ]
   }
 ];
