0a1e9f474e5475fd7d17b423d42e06b49b0f13b3
kallelehtonen <coadiespla@gmail.com>
2019-08-06 13:10:45 +0300

    Changed args type to any[]

--- src/app/pipes/truncate.pipe.ts
+++ src/app/pipes/truncate.pipe.ts
@@ -7,7 +7,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'truncate'
 })
 export class TruncatePipe implements PipeTransform {
-  transform(value: string, args?: string[]): string {
+  transform(value: string, args?: any[]): string {
     const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
 
     return value.length > limit ? `${value.substring(0, limit)}...` : value;
