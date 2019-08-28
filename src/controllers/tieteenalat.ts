0f22c272d2020245914da44aa40e9930553ea1a6
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-08-28 13:14:05 +0300

    Implemented error handling

--- src/controllers/tieteenalat.ts
+++ src/controllers/tieteenalat.ts
@@ -11,102 +11,104 @@ const params = "koodi";
  * Set data into redis database
  *
  * @returns {Promise<any>}
- *
- * @todo Implement error handling
  */
 export async function setTieteenalat(): Promise<any> {
-  const results = await getDataFromApi(
-    process.env.KOODISTO_SERVICE_URL,
-    `/${endpoint}/`,
-    { "Accept": "application/json" },
-    params
-  );
-  const data: any[] = [];
+  try {
+    const results = await getDataFromApi(
+      process.env.KOODISTO_SERVICE_URL,
+      `/${endpoint}/`,
+      {"Accept": "application/json"},
+      params
+    );
+    const data: any[] = [];
 
-  data.push({
-    key: "1",
-    value: {
-      fi: "Luonnontieteet",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  data.push({
-    key: "2",
-    value: {
-      fi: "Tekniikka",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  data.push({
-    key: "3",
-    value: {
-      fi: "Lääke- ja terveystieteet",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  data.push({
-    key: "4",
-    value: {
-      fi: "Maatalous- ja metsätieteet",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  data.push({
-    key: "5",
-    value: {
-      fi: "Yhteiskuntatieteet",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  data.push({
-    key: "6",
-    value: {
-      fi: "Humanistiset tieteet",
-      // en: "",
-      // sv: "",
-    },
-    children: []
-  });
-
-  results.forEach((result: any) => {
-    const metadataFi = result.metadata.find((e: any) => e.kieli.toLowerCase() === "fi");
-    const metadataEn = result.metadata.find((e: any) => e.kieli.toLowerCase() === "en");
-    const metadataSv = result.metadata.find((e: any) => e.kieli.toLowerCase() === "sv");
-
-    const parent = data.find((e: any) => e.key === result.koodiArvo.charAt(0));
-
-    parent.children.push({
-      key: result.koodiArvo,
+    data.push({
+      key: "1",
       value: {
-        fi: metadataFi.nimi.trim(),
-        en: metadataEn.nimi.trim(),
-        sv: metadataSv.nimi.trim(),
-      }
+        fi: "Luonnontieteet",
+        // en: "",
+        // sv: "",
+      },
+      children: []
     });
-  });
 
-  data.sort((a: any, b: any) => a.key - b.key);
+    data.push({
+      key: "2",
+      value: {
+        fi: "Tekniikka",
+        // en: "",
+        // sv: "",
+      },
+      children: []
+    });
 
-  data.forEach((parent: any) => {
-    parent.children.sort((a: any, b: any) => a.key - b.key);
-  });
+    data.push({
+      key: "3",
+      value: {
+        fi: "Lääke- ja terveystieteet",
+        // en: "",
+        // sv: "",
+      },
+      children: []
+    });
 
-  await setAsync(rediskey, JSON.stringify(data));
+    data.push({
+      key: "4",
+      value: {
+        fi: "Maatalous- ja metsätieteet",
+        // en: "",
+        // sv: "",
+      },
+      children: []
+    });
+
+    data.push({
+      key: "5",
+      value: {
+        fi: "Yhteiskuntatieteet",
+        // en: "",
+        // sv: "",
+      },
+      children: []
+    });
+
+    data.push({
+      key: "6",
+      value: {
+        fi: "Humanistiset tieteet",
+        // en: "",
+        // sv: "",
+      },
+      children: []
+    });
+
+    results.forEach((result: any) => {
+      const metadataFi = result.metadata.find((e: any) => e.kieli.toLowerCase() === "fi");
+      const metadataEn = result.metadata.find((e: any) => e.kieli.toLowerCase() === "en");
+      const metadataSv = result.metadata.find((e: any) => e.kieli.toLowerCase() === "sv");
+
+      const parent = data.find((e: any) => e.key === result.koodiArvo.charAt(0));
+
+      parent.children.push({
+        key: result.koodiArvo,
+        value: {
+          fi: metadataFi.nimi.trim(),
+          en: metadataEn.nimi.trim(),
+          sv: metadataSv.nimi.trim(),
+        }
+      });
+    });
+
+    data.sort((a: any, b: any) => a.key - b.key);
+
+    data.forEach((parent: any) => {
+      parent.children.sort((a: any, b: any) => a.key - b.key);
+    });
+
+    await setAsync(rediskey, JSON.stringify(data));
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 /**
@@ -119,35 +121,40 @@ export async function setTieteenalat(): Promise<any> {
  * @returns {Promise<any>}
  */
 export const getTieteenalat = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const redisData = await getAsync(rediskey);
+  try {
+    const redisData = await getAsync(rediskey);
 
-  if (redisData) {
-    const input = JSON.parse(redisData);
+    if (redisData) {
+      const input = JSON.parse(redisData);
 
-    const output = input.map((row: any) => {
-      const children = row.children.map((child: any) => {
+      const output = input.map((row: any) => {
+        const children = row.children.map((child: any) => {
+          return {
+            key: child.key,
+            value: child.value[req.params.lang]
+          };
+        });
+
         return {
-          key: child.key,
-          value: child.value[req.params.lang]
+          key: row.key,
+          value: row.value[req.params.lang] != undefined ? row.value[req.params.lang] : row.value.fi,
+          children: children,
         };
       });
 
-      return {
-        key: row.key,
-        value: row.value[req.params.lang] != undefined ? row.value[req.params.lang] : row.value.fi,
-        children: children,
-      };
-    });
-
-    if (output.length > 0) {
-      res.status(200).json(output);
+      if (output.length > 0) {
+        res.status(200).json(output);
+      } else {
+        res.sendStatus(404);
+      }
     } else {
       res.sendStatus(404);
-    }
-  } else {
-    res.sendStatus(404);
 
-    return next();
+      return next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
   }
 };
 
@@ -161,28 +168,33 @@ export const getTieteenalat = async (req: Request, res: Response, next: NextFunc
  * @returns {Promise<any>}
  */
 export const getTieteenala = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
-  const redisData = await getAsync(rediskey);
+  try {
+    const redisData = await getAsync(rediskey);
 
-  if (redisData) {
-    const input = JSON.parse(redisData);
-    const row = input.find((e: any) => e.key === req.params.key);
-    let output: object;
+    if (redisData) {
+      const input = JSON.parse(redisData);
+      const row = input.find((e: any) => e.key === req.params.key);
+      let output: object;
 
-    if (row != undefined) {
-      output = {
-        key: row.key,
-        value: row.value[req.params.lang] != undefined ? row.value[req.params.lang] : row.value.fi,
-      };
-    }
+      if (row != undefined) {
+        output = {
+          key: row.key,
+          value: row.value[req.params.lang] != undefined ? row.value[req.params.lang] : row.value.fi,
+        };
+      }
 
-    if (output != undefined) {
-      res.status(200).json(output);
+      if (output != undefined) {
+        res.status(200).json(output);
+      } else {
+        res.sendStatus(404);
+      }
     } else {
       res.sendStatus(404);
-    }
-  } else {
-    res.sendStatus(404);
 
-    return next();
+      return next();
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Something went wrong");
   }
 };
