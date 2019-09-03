c31d6630aa46eb5adcd2d2e2b0461c0425fc1c43
kallelehtonen <coadiespla@gmail.com>
2019-09-03 15:13:08 +0300

    Added extended alignment object model

---
+++ src/app/models/alignment-object-extended.ts
@@ -0,0 +1,9 @@
+export interface AlignmentObjectExtended {
+  key?: string;
+  source?: string;
+  alignmentType: 'assesses' | 'teaches' | 'requires' | 'textComplexity' | 'readingLevel' | 'educationalSubject' | 'educationalLevel';
+  educationalFramework?: string;
+  targetDescription?: string;
+  targetName: string;
+  targetUrl?: string;
+}
