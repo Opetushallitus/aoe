94d4cb21f7f4553fc6b414b3b5b93f20116fd180
kallelehtonen <coadiespla@gmail.com>
2019-09-05 15:03:09 +0300

    Added dev uri for api

--- src/app/services/koodisto-proxy.service.ts
+++ src/app/services/koodisto-proxy.service.ts
@@ -2,18 +2,25 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+import { environment } from '../../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
 export class KoodistoProxyService {
-  private apiUri = 'https://koodisto.aoe.fi/api/v1';
+  private apiUri: string;
+  private apiUriProd = 'https://koodisto.aoe.fi/api/v1';
+  private apiUriDev = 'http://localhost:3000/api/v1';
+
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
     }),
   };
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this.apiUri = environment.production ? this.apiUriProd : this.apiUriDev;
+  }
 
   getData(rediskey: string, lang: string): Observable<any> {
     return this.http.get(`${this.apiUri}/${rediskey}/${lang}`, this.httpOptions);
