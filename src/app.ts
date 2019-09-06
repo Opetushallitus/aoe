fc9c2e0a84dceee113cb46650a5aaea659280c20
Kalle Lehtonen <kalle.lehtonen@digia.com>
2019-09-06 09:21:40 +0300

    Disable cors

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,7 @@ app.use(session({
 app.use(compression());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors()); // Enable CORS for dev purposes
+// app.use(cors()); // Enable CORS for dev purposes
 app.set("port", 3000);
 
 client.on("error", (error: any) => {
