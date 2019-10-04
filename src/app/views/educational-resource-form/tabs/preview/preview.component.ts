8019ae38c6356bf1b7f4946954acb7f5e061c256
Kalle Lehtonen <coadiespla@gmail.com>
2019-10-04 12:49:30 +0300

    Added most of the alignment objects

--- src/app/views/educational-resource-form/tabs/preview/preview.component.ts
+++ src/app/views/educational-resource-form/tabs/preview/preview.component.ts
@@ -5,6 +5,7 @@ import { Router } from '@angular/router';
 import { environment } from '../../../../../environments/environment';
 import { getLocalStorageData } from '../../../../shared/shared.module';
 import { BackendService } from '../../../../services/backend.service';
+import { AlignmentObjectExtended } from '../../../../models/alignment-object-extended';
 
 @Component({
   selector: 'app-preview',
@@ -15,6 +16,19 @@ export class PreviewComponent implements OnInit {
   private fileUploadLSKey = environment.fileUploadLSKey;
   public savedData: any;
   private fileUpload: any;
+
+  public earlyChildhoodEducationSubjects: AlignmentObjectExtended[];
+  public earlyChildhoodEducationObjectives: AlignmentObjectExtended[];
+  public prePrimaryEducationSubjects: AlignmentObjectExtended[];
+  public prePrimaryEducationObjectives: AlignmentObjectExtended[];
+  public basicStudySubjects: AlignmentObjectExtended[];
+  public upperSecondarySchoolSubjects: AlignmentObjectExtended[];
+  public upperSecondarySchoolObjectives: AlignmentObjectExtended[];
+  public vocationalDegrees: AlignmentObjectExtended[];
+  public vocationalEducationObjectives: AlignmentObjectExtended[];
+  public selfMotivatedEducationSubjects: AlignmentObjectExtended[];
+  public selfMotivatedEducationObjectives: AlignmentObjectExtended[];
+  public scienceBranchObjectives: AlignmentObjectExtended[];
 
   public previewForm: FormGroup;
 
@@ -31,6 +45,46 @@ export class PreviewComponent implements OnInit {
     this.previewForm = this.fb.group({
       confirm: this.fb.control(false, [ Validators.requiredTrue ])
     });
+
+    if (this.savedData && this.savedData.alignmentObjects) {
+      this.earlyChildhoodEducationSubjects = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'earlyChildhoodEducationSubjects');
+
+      this.earlyChildhoodEducationObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'earlyChildhoodEducationObjectives');
+
+      // @todo: framework
+
+      this.prePrimaryEducationSubjects = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'prePrimaryEducationSubjects');
+
+      this.prePrimaryEducationObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'prePrimaryEducationObjectives');
+
+      this.basicStudySubjects = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'basicStudySubjects');
+
+      this.upperSecondarySchoolSubjects = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'upperSecondarySchoolSubjects');
+
+      this.upperSecondarySchoolObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'upperSecondarySchoolObjectives');
+
+      this.vocationalDegrees = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'vocationalDegrees');
+
+      this.vocationalEducationObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'vocationalEducationObjectives');
+
+      this.selfMotivatedEducationSubjects = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'selfMotivatedEducationSubjects');
+
+      this.selfMotivatedEducationObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'selfMotivatedEducationObjectives');
+
+      this.scienceBranchObjectives = this.savedData.alignmentObjects
+        .filter(alignmentObject => alignmentObject.source === 'scienceBranchObjectives');
+    }
   }
 
   public onSubmit() {
