131d469e8cd9d6588e7c91160227081df366f968
Kalle Lehtonen <coadiespla@gmail.com>
2019-10-09 08:49:37 +0300

    Added all supported MIME types

--- src/app/components/educational-material-preview/educational-material-preview.component.ts
+++ src/app/components/educational-material-preview/educational-material-preview.component.ts
@@ -10,12 +10,60 @@ export class EducationalMaterialPreviewComponent {
   @Input() material: Material;
 
   public checkMimeType(mimeType: string) {
-    const imageMimeTypes = [
-      'image/jpeg',
-      'image/png',
+    const videoMIMETypes = [
+      'application/x-troff-msvideo',
+      'video/avi',
+      'video/msvideo',
+      'video/x-msvideo',
+      'video/mp4',
     ];
 
-    if (imageMimeTypes.includes(mimeType)) {
+    const audioMIMETypes = [
+      'audio/mp3',
+      'audio/mpeg',
+    ];
+
+    const pdfMIMETypes = [
+      'application/pdf',
+    ];
+
+    const officeMIMETypes = [
+      'application/msword',
+      'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+      'application/vnd.oasis.opendocument.text',
+      'application/vnd.ms-powerpoint',
+      'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+      'application/vnd.oasis.opendocument.presentation',
+      'application/rtf',
+    ];
+
+    const imageMIMETypes = [
+      'image/bmp',
+      'image/gif',
+      'image/jpeg',
+      'image/png',
+      'image/svg+xml',
+      'image/tiff',
+      'image/webp',
+    ];
+
+    if (videoMIMETypes.includes(mimeType)) {
+      return 'video';
+    }
+
+    if (audioMIMETypes.includes(mimeType)) {
+      return 'audio';
+    }
+
+    if (pdfMIMETypes.includes(mimeType)) {
+      return 'pdf';
+    }
+
+    if (officeMIMETypes.includes(mimeType)) {
+      return 'office';
+    }
+
+    if (imageMIMETypes.includes(mimeType)) {
       return 'image';
     }
   }
