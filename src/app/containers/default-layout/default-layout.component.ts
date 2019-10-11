555264d8a5c74d5192e78f1370cc1a3fab26b9a5
Kalle Lehtonen <coadiespla@gmail.com>
2019-10-11 09:42:08 +0300

    Added i18n logos

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -12,6 +12,37 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './default-layout.component.html'
 })
 export class DefaultLayoutComponent {
+  logos = {
+    okm: {
+      fi: {
+        src: 'assets/img/logos/okm-fi.jpg',
+        alt: 'Opetus- ja kulttuuriministeriö',
+      },
+      en: {
+        src: 'assets/img/logos/okm-en.jpg',
+        alt: 'Ministry of Education and Culture',
+      },
+      sv: {
+        src: 'assets/img/logos/okm-sv.jpg',
+        alt: 'Undervisnings- och kulturministeriet',
+      },
+    },
+    oph: {
+      fi: {
+        src: 'assets/img/logos/oph-fisv.png',
+        alt: 'Opetushallitus',
+      },
+      en: {
+        src: 'assets/img/logos/oph-en.png',
+        alt: 'Finnish National Agency for Education',
+      },
+      sv: {
+        src: 'assets/img/logos/oph-fisv.png',
+        alt: 'Utbildningsstyrelsen',
+      },
+    },
+  };
+
   constructor(
     public translate: TranslateService,
     public authSvc: AuthService
