738ed0a590d2caf9466e5d64d31ab4741d3307cc
kallelehtonen <coadiespla@gmail.com>
2019-11-05 13:45:05 +0200

    Added cookie settings

--- src/app/components/cookie-notice/cookie-notice.component.ts
+++ src/app/components/cookie-notice/cookie-notice.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CookieService } from '../../services/cookie.service';
 
 @Component({
   selector: 'app-cookie-notice',
   templateUrl: './cookie-notice.component.html',
 })
 export class CookieNoticeComponent implements OnInit {
-  constructor() { }
+  cookies: FormGroup;
+  @Output() hideCookieNotice = new EventEmitter();
+
+  constructor(
+    private fb: FormBuilder,
+    private cookieSvc: CookieService,
+  ) { }
 
   ngOnInit() {
+    this.cookies = this.fb.group({
+      aoe: this.fb.control({ value: true, disabled: true }, [ Validators.requiredTrue ]),
+      googleAnalytics: this.fb.control(false),
+    });
+  }
+
+  onSubmit(): void {
+    // set cookie settings
+    this.cookieSvc.setCookieSettings(this.cookies.value);
+
+    // hide cookie notice
+    this.hideCookieNotice.emit();
   }
 }
