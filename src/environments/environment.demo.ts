cb56f8da3d35e9b8ba1250bcf11b855bd6c20879
kallelehtonen <coadiespla@gmail.com>
2019-11-28 15:48:41 +0200

    Added demo environment

---
+++ src/environments/environment.demo.ts
@@ -0,0 +1,10 @@
+/**
+ * @ignore
+ */
+export const environment = {
+  production: true,
+  backendUrl: 'https://demo.aoe.fi/api',
+  fileUploadLSKey: 'aoe.fileUpload',
+  newERLSKey: 'aoe.new-educational-resource',
+  koodistoUrl: 'https://koodisto.aoe.fi/api/v1'
+};
