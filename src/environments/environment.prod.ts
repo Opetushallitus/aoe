7d5e081f85995544f0931d4204d798520b26578d
kallelehtonen <coadiespla@gmail.com>
2019-11-29 14:14:56 +0200

    Updated environment variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -6,5 +6,7 @@ export const environment = {
   backendUrl: 'https://86.50.27.30/api',
   fileUploadLSKey: 'aoe.fileUpload',
   newERLSKey: 'aoe.new-educational-resource',
-  koodistoUrl: 'https://koodisto.aoe.fi/api/v1'
+  koodistoUrl: 'https://koodisto.aoe.fi/api/v1',
+  sessionCookie: 'connect.sid',
+  userdataKey: 'aoe.userdata',
 };
