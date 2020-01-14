b0018dd3e2e132fef1d4e3a3c71e5364ed830a17
kallelehtonen <coadiespla@gmail.com>
2020-01-14 15:21:48 +0200

    Updated accessibility policy

--- src/app/views/accessibility-policy-view/accessibility-policy-view.component.ts
+++ src/app/views/accessibility-policy-view/accessibility-policy-view.component.ts
@@ -1,7 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-accessibility-policy-view',
   templateUrl: './accessibility-policy-view.component.html',
 })
-export class AccessibilityPolicyViewComponent { }
+export class AccessibilityPolicyViewComponent implements OnInit {
+  lang: string = this.translate.currentLang;
+
+  constructor(
+    private translate: TranslateService,
+  ) { }
+
+  ngOnInit() {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+  }
+}
