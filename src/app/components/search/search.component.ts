bf73947ad3681a216008aabe8f43208727a00db3
kallelehtonen <coadiespla@gmail.com>
2020-01-27 14:59:34 +0200

    Added form controls

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,16 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { SearchService } from '@services/search.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
+  searchForm: FormGroup;
+  resultsSubscription: Subscription;
+  searchResults: any;
+
   constructor(
     private searchSvc: SearchService,
+    private fb: FormBuilder,
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.searchForm = this.fb.group({
+      keywords: this.fb.control(null, [ Validators.required ]),
+    });
+
+    this.resultsSubscription = this.searchSvc.searchResults$
+      .subscribe(results => {
+        this.searchResults = results;
+      });
+
+    console.log(this.searchResults);
+  }
+
+  ngOnDestroy(): void {
+    this.resultsSubscription.unsubscribe();
+  }
+
+  onSubmit(): void {
+    if (this.searchForm.valid) {
+      this.searchSvc.updateSearchResults(this.searchForm.value);
+    }
   }
 }
