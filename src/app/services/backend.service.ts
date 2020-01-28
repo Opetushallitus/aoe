e51328425f0eecb81529524c3335f930497ea16f
kallelehtonen <coadiespla@gmail.com>
2020-01-28 12:29:36 +0200

    Fixed authors structure

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -247,7 +247,7 @@ export class BackendService {
             .filter((alignmentObject: AlignmentObjectExtended) => alignmentObject.source === koodistoSources.prerequisites),
           references: res.isBasedOn
             .map(r => ({
-              authors: r.author,
+              authors: r.author.map(author => author.authorname),
               url: r.url,
               name: r.materialname,
             })),
