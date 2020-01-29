9e2af719ec18e5c5745a30deef37a90a77efdda4
kallelehtonen <coadiespla@gmail.com>
2020-01-29 17:41:10 +0200

    Started working on search results

---
+++ src/app/components/search-result/search-result.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { SearchResult } from '@models/search/search-results';
+
+@Component({
+  selector: 'app-search-result',
+  templateUrl: './search-result.component.html',
+  styleUrls: ['./search-result.component.scss']
+})
+export class SearchResultComponent implements OnInit {
+  @Input() result: SearchResult;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/models/search/search-results.ts
+++ src/app/models/search/search-results.ts
@@ -1,10 +1,22 @@
+import { Author } from '@models/author';
+import { EducationalLevel } from '@models/educational-level';
+import { LearningResourceType } from '@models/learning-resource-type';
+
 export interface SearchResults {
   hits: number;
   results: SearchResult[];
 }
 
 export interface SearchResult {
+  id: number | string;
+  createdAt: Date;
+  publishedAt?: Date;
+  updatedAt?: Date;
   name: string;
   description: string;
+  authors: Author[];
+  learningResourceTypes: LearningResourceType[];
   license: string;
+  educationalLevels: EducationalLevel[];
+  thumbnail?: string;
 }
