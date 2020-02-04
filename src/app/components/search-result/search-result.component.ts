7654e07a73c87109d1ad1e0ff8e6a8344df3f4bf
kallelehtonen <coadiespla@gmail.com>
2020-02-04 15:25:59 +0200

    Added download url

--- src/app/components/search-result/search-result.component.ts
+++ src/app/components/search-result/search-result.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SearchResult } from '@models/search/search-results';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-search-result',
@@ -8,10 +9,12 @@ import { SearchResult } from '@models/search/search-results';
 })
 export class SearchResultComponent implements OnInit {
   @Input() result: SearchResult;
+  downloadUrl: string;
 
   constructor() { }
 
   ngOnInit() {
+    this.downloadUrl = `${environment.backendUrl}/material/file/${this.result.id}`;
   }
 
 }
