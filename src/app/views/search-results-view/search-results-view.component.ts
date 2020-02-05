9c31c4613033d665587fa8a1fe677235b456a095
kallelehtonen <coadiespla@gmail.com>
2020-02-05 15:09:49 +0200

    Updated search results to match current response from backend

--- src/app/views/search-results-view/search-results-view.component.ts
+++ src/app/views/search-results-view/search-results-view.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { SearchService } from '@services/search.service';
-import { SearchResult } from '@models/search/search-results';
+import { SearchResult, SearchResults } from '@models/search/search-results';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -12,8 +12,7 @@ import { Subscription } from 'rxjs';
 export class SearchResultsViewComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
   resultSubscription: Subscription;
-  results: any[];
-  mockResults: SearchResult[] = [];
+  results: SearchResults;
 
   constructor(
     private searchSvc: SearchService,
@@ -25,60 +24,8 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
       keywords: this.fb.control(null, [ Validators.required ]),
     });
 
-    this.resultSubscription = this.searchSvc.searchResults$.subscribe((results: any[]) => {
+    this.resultSubscription = this.searchSvc.searchResults$.subscribe((results: SearchResults) => {
       this.results = results;
-    });
-
-    this.mockResults.push({
-      id: 1,
-      createdAt: new Date(2019, 1, 3),
-      name: 'Mock material',
-      description: 'Kuvaus',
-      authors: [
-        {
-          authorname: 'Mehiläinen, Maija',
-        },
-      ],
-      learningResourceTypes: [
-        {
-          learningresourcetypekey: 'key',
-          value: 'video',
-        }
-      ],
-      license: 'CCBY4.0',
-      educationalLevels: [
-        {
-          educationallevelkey: 'key',
-          value: 'Yläaste',
-        },
-      ],
-      thumbnail: 'https://aoethumbnail.object.pouta.csc.fi/thumbnail1580736649891.png',
-    });
-
-    this.mockResults.push({
-      id: 2,
-      createdAt: new Date(2020, 0, 15),
-      name: 'Mock material 2',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean accumsan sed est et varius. Nulla posuere in justo eget vehicula. Nulla et sem ultrices, tincidunt quam facilisis, finibus eros. Donec vitae enim in erat pretium ultrices ut eu neque.',
-      authors: [
-        {
-          authorname: 'Mehiläinen, Maija 2',
-        },
-      ],
-      learningResourceTypes: [
-        {
-          learningresourcetypekey: 'key',
-          value: 'audio',
-        }
-      ],
-      license: 'CCBY4.0',
-      educationalLevels: [
-        {
-          educationallevelkey: 'key',
-          value: 'Yläaste',
-        },
-      ],
-      thumbnail: 'https://aoethumbnail.object.pouta.csc.fi/thumbnail1580736649891.png',
     });
   }
 
