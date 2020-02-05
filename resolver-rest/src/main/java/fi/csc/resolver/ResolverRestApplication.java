eb521d07205214f6de504ce5137db7ef1f22491f
micaro <mika.ropponen@csc.fi>
2020-02-05 13:16:40 +0200

    Removed unnecessary code and files

--- resolver-rest/src/main/java/fi/csc/resolver/ResolverRestApplication.java
+++ resolver-rest/src/main/java/fi/csc/resolver/ResolverRestApplication.java
@@ -1,31 +1,12 @@
 package fi.csc.resolver;
 
-import fi.csc.resolver.model.Link;
-import fi.csc.resolver.repository.LinkRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ResolverRestApplication {
 
-    private LinkRepository linkRepository;
-
-    @Autowired
-    public void setLinkRepository(LinkRepository linkRepository) {
-        this.linkRepository = linkRepository;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ResolverRestApplication.class, args);
     }
-
-    @PostConstruct
-    public void populateH2() {
-        Link link = new Link(2, 1, "latest", (short) 1, "85e7ab0180b6b0e09a54c7a750cc91e73776b94b", "https://demo.aoe.fi");
-        // this.linkRepository.save(link);
-    }
-
 }
--- resolver-rest/src/main/java/fi/csc/resolver/controller/ResolverController.java
+++ resolver-rest/src/main/java/fi/csc/resolver/controller/ResolverController.java
@@ -14,6 +14,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+@SuppressWarnings("unused")
 @RestController
 public class ResolverController {
 
