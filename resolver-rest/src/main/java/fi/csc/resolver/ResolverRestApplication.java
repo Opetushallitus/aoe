53de7bde8758cc0db7016c9740e17cf71dfbcab1
micaro <mika.ropponen@csc.fi>
2020-02-05 11:36:52 +0200

    Naming convention change in in-memory models

--- resolver-rest/src/main/java/fi/csc/resolver/ResolverRestApplication.java
+++ resolver-rest/src/main/java/fi/csc/resolver/ResolverRestApplication.java
@@ -1,13 +1,31 @@
 package fi.csc.resolver;
 
+import fi.csc.resolver.model.Link;
+import fi.csc.resolver.repository.LinkRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class ResolverRestApplication {
 
+    private LinkRepository linkRepository;
+
+    @Autowired
+    public void setLinkRepository(LinkRepository linkRepository) {
+        this.linkRepository = linkRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(ResolverRestApplication.class, args);
+    }
+
+    @PostConstruct
+    public void populateH2() {
+        Link link = new Link(1, 1, "latest", (short) 1, "85e7ab0180b6b0e09a54c7a750cc91e73776b94a", "https://aoe.fi");
+        this.linkRepository.save(link);
     }
 
 }
--- resolver-rest/src/main/resources/data.sql
+++ resolver-rest/src/main/resources/data.sql
@@ -9,6 +9,3 @@ CREATE TABLE link(
   target_url VARCHAR(2048) NOT NULL,
   CONSTRAINT link_pk PRIMARY KEY (meta_id, material_id, version)
 );
-
-INSERT INTO link VALUES (1, 1, 'latest', 1, '85e7ab0180b6b0e09a54c7a750cc91e73776b94a', 'https://aoe.fi');
-INSERT INTO link VALUES (1, 2, '1.0', 0, '85e7ab0180b6b0e09a54c7a750cc91e73776b94b', 'https://aoe.fi');
