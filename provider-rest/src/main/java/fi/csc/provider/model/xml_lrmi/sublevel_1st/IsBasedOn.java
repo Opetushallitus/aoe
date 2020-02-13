9f6ad15903616a9ca0d9561b65128733311e6057
micaro <mika.ropponen@csc.fi>
2020-02-13 12:56:27 +0200

    Provider field isBasedOn->author changed to a string array

--- provider-rest/src/main/java/fi/csc/provider/model/xml_lrmi/sublevel_1st/IsBasedOn.java
+++ provider-rest/src/main/java/fi/csc/provider/model/xml_lrmi/sublevel_1st/IsBasedOn.java
@@ -1,43 +1,25 @@
 package fi.csc.provider.model.xml_lrmi.sublevel_1st;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlElement;
 
+@Getter
+@Setter
 @SuppressWarnings("unused")
 @XmlAccessorType(XmlAccessType.NONE)
 public class IsBasedOn {
 
-    @XmlElement(name = "lrmi_fi:title")
-    private String title;
+    @XmlElement(name = "lrmi_fi:url")
+    private String url;
 
-    @XmlElement(name = "lrmi_fi:contributor")
-    private String contributor;
+    @XmlElement(name = "lrmi_fi:name")
+    private String name;
 
-    @XmlElement(name = "lrmi_fi:identifier")
-    private String identifier;
+    @XmlElement(name = "lrmi_fi:author")
+    private String[] author;
 
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContributor() {
-        return contributor;
-    }
-
-    public void setContributor(String contributor) {
-        this.contributor = contributor;
-    }
-
-    public String getIdentifier() {
-        return identifier;
-    }
-
-    public void setIdentifier(String identifier) {
-        this.identifier = identifier;
-    }
 }
