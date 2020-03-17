6f96f2e2daf43d5688796c03d28f67b95e3356f6
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-03-17 16:56:58 +0200

    Added basics for edit form

--- src/app/views/educational-material-edit-form/educational-material-edit-form.component.ts
+++ src/app/views/educational-material-edit-form/educational-material-edit-form.component.ts
@@ -1,19 +1,45 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { BackendService } from '@services/backend.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-educational-material-edit-form',
   templateUrl: './educational-material-edit-form.component.html',
   styleUrls: ['./educational-material-edit-form.component.scss']
 })
-export class EducationalMaterialEditFormComponent implements OnInit {
+export class EducationalMaterialEditFormComponent implements OnInit, OnDestroy {
   materialId: string;
+  materialSubscription: Subscription;
+  material: any;
+  tabId: number;
+  routeSubscription: Subscription;
 
   constructor(
     private route: ActivatedRoute,
+    private router: Router,
+    private backendSvc: BackendService,
   ) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.materialId = this.route.snapshot.paramMap.get('materialId');
+
+    this.materialSubscription = this.backendSvc.editMaterial$.subscribe((material) => {
+      this.material = material;
+    });
+    this.backendSvc.updateEditMaterial(+this.materialId);
+
+    this.routeSubscription = this.route.paramMap.subscribe((params: Params) => {
+      this.tabId = params.get('tabId');
+
+      if (!this.tabId) {
+        this.router.navigate(['/muokkaa-oppimateriaalia', this.materialId, 1]);
+      }
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.materialSubscription.unsubscribe();
+    this.routeSubscription.unsubscribe();
   }
 }
