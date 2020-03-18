fa424b74b513329bb147ea00937327a4e08f37b4
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-03-18 10:16:47 +0200

    Updated interface

--- src/app/models/educational-material-form.ts
+++ src/app/models/educational-material-form.ts
@@ -1,3 +1,5 @@
+import { AlignmentObjectExtended } from '@models/alignment-object-extended';
+
 export interface EducationalMaterialForm {
   // files
   name: {
@@ -69,7 +71,74 @@ export interface EducationalMaterialForm {
       value: string;
     }
   ];
+  earlyChildhoodEducationSubjects?: AlignmentObjectExtended[];
+  suitsAllEarlyChildhoodSubjects?: boolean;
+  earlyChildhoodEducationObjectives?: AlignmentObjectExtended[];
+  earlyChildhoodEducationFramework?: string;
+  prePrimaryEducationSubjects?: AlignmentObjectExtended[];
+  suitsAllPrePrimarySubjects?: boolean;
+  prePrimaryEducationObjectives?: AlignmentObjectExtended[];
+  prePrimaryEducationFramework?: string;
+  basicStudySubjects?: AlignmentObjectExtended[];
+  suitsAllBasicStudySubjects?: boolean;
+  basicStudyObjectives?: AlignmentObjectExtended[];
+  basicStudyContents?: AlignmentObjectExtended[];
+  basicStudyFramework?: string;
+  upperSecondarySchoolSubjects?: AlignmentObjectExtended[];
+  suitsAllUpperSecondarySubjects?: boolean;
+  upperSecondarySchoolObjectives?: AlignmentObjectExtended[];
+  upperSecondarySchoolFramework?: string;
+  upperSecondarySchoolSubjectsNew?: AlignmentObjectExtended[];
+  upperSecondarySchoolModulesNew?: AlignmentObjectExtended[];
+  upperSecondarySchoolObjectivesNew?: AlignmentObjectExtended[];
+  upperSecondarySchoolContentsNew?: AlignmentObjectExtended[];
+  suitsAllUpperSecondarySubjectsNew?: boolean;
+  vocationalDegrees?: AlignmentObjectExtended[];
+  suitsAllVocationalDegrees?: boolean;
+  vocationalUnits?: AlignmentObjectExtended[];
+  vocationalEducationObjectives?: AlignmentObjectExtended[];
+  vocationalEducationFramework?: string;
+  selfMotivatedEducationSubjects?: AlignmentObjectExtended[];
+  suitsAllSelfMotivatedSubjects?: boolean;
+  selfMotivatedEducationObjectives?: AlignmentObjectExtended[];
+  branchesOfScience?: AlignmentObjectExtended[];
+  suitsAllBranches?: boolean;
+  scienceBranchObjectives?: AlignmentObjectExtended[];
+  higherEducationFramework?: string;
   // extended details
+  accessibilityFeatures?: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  accessibilityHazards?: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  typicalAgeRange?: {
+    typicalAgeRangeMin?: number;
+    typicalAgeRangeMax?: number;
+  };
+  timeRequired?: string;
+  publisher?: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  expires?: Date;
+  prerequisites?: AlignmentObjectExtended[];
   // license
+  license: string;
   // references
+  externals?: [
+    {
+      author: string[];
+      url: string;
+      name: string;
+    }
+  ];
 }
