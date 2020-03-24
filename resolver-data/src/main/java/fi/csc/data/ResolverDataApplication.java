05f8ee498a3853dec173b699645e080517bd0702
mroppon <mika.ropponen@csc.fi>
2020-03-24 18:52:28 +0200

    Removed console banners from module startups

--- resolver-data/src/main/java/fi/csc/data/ResolverDataApplication.java
+++ resolver-data/src/main/java/fi/csc/data/ResolverDataApplication.java
@@ -1,5 +1,6 @@
 package fi.csc.data;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,27 +8,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ResolverDataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ResolverDataApplication.class, args);
+        // SpringApplication.run(ResolverDataApplication.class, args);
+        SpringApplication app = new SpringApplication(ResolverDataApplication.class);
+        app.setBannerMode(Banner.Mode.OFF);
+        app.run(args);
     }
-
-    /*@Profile("dev")
-    @PostConstruct
-    private void populateDatabase() {
-        Record record1 = new Record(1L, 1L, "14tekijanoikeudetvirtuaaliluokassaopettajanoikeudet-1580209524845.pptx");
-        Record record2 = new Record(2L, 2L, "Johdatustekolyyn-1572942352112.pdf");
-        Material material1 = new Material(1L, 5L, "material-1", record1);
-        Material material2 = new Material(2L, 8L, "material-2", record2);
-        List<Material> materials1 = new ArrayList<>() {{
-            add(material1);
-        }};
-        List<Material> materials2 = new ArrayList<>() {{
-            add(material2);
-        }};
-        OffsetDateTime dateTime1 = OffsetDateTime.now(ZoneId.of("UTC")).minusDays(10L);
-        OffsetDateTime dateTime2 = OffsetDateTime.now(ZoneId.of("UTC")).minusDays(20L);
-        EducationalMaterial educationalMaterial1 = new EducationalMaterial(5L, dateTime1, "admin", materials1);
-        EducationalMaterial educationalMaterial2 = new EducationalMaterial(8L, dateTime2, "admin", materials2);
-        this.educationalMaterialRepository.save(educationalMaterial1);
-        this.educationalMaterialRepository.save(educationalMaterial2);
-    }*/
 }
