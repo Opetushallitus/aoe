c5c905072b4e816ff2de57be7c4a7830e3b7828f
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-04-16 14:34:21 +0300

    Added get single rating

--- src/app/services/ratings.service.ts
+++ src/app/services/ratings.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { RatingPost } from '@models/rating-post';
-import { Ratings } from '@models/backend/ratings';
+import { Rating, Ratings } from '@models/backend/ratings';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
 
@@ -31,7 +31,20 @@ export class RatingsService {
    * @returns {Observable<Ratings>} List of material ratings
    */
   getRatings(materialId: number | string): Observable<Ratings> {
-    return this.http.get<Ratings>(`${environment.backendUrl}/rating/${materialId}`, {
+    return this.http.get<Ratings>(`${environment.backendUrl}/ratings/${materialId}`, {
+      headers: new HttpHeaders({
+        'Accept': 'application/json',
+      }),
+    });
+  }
+
+  /**
+   * Returns users rating.
+   * @param {number | string} materialId Material ID
+   * @returns {Observable<Rating>} Users rating
+   */
+  getRating(materialId: number | string): Observable<Rating> {
+    return this.http.get<Rating>(`${environment.backendUrl}/rating/${materialId}`, {
       headers: new HttpHeaders({
         'Accept': 'application/json',
       }),
