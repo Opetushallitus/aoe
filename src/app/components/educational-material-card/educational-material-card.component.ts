52b6ab8acc48886fbba131de1898492d84841fb5
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-05-07 11:38:44 +0300

    Fixed name/description logic on educational material cards

--- src/app/components/educational-material-card/educational-material-card.component.ts
+++ src/app/components/educational-material-card/educational-material-card.component.ts
@@ -1,7 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
-import { EducationalMaterialList } from '@models/educational-material-list';
+import { EducationalMaterialCard } from '@models/educational-material-card';
 import { Keyword } from '@models/keyword';
 import { EducationalLevel } from '@models/educational-level';
 
@@ -10,7 +10,7 @@ import { EducationalLevel } from '@models/educational-level';
   templateUrl: './educational-material-card.component.html',
 })
 export class EducationalMaterialCardComponent implements OnInit {
-  @Input() educationalMaterial: EducationalMaterialList;
+  @Input() educationalMaterial: EducationalMaterialCard;
   lang: string = this.translate.currentLang;
 
   materialName: string;
@@ -25,33 +25,11 @@ export class EducationalMaterialCardComponent implements OnInit {
   ngOnInit(): void {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.lang = event.lang;
-
-      this.updateMaterialName();
-      this.updateDescription();
     });
-
-    this.updateMaterialName();
-    this.updateDescription();
 
     // @todo: redo this with api data
     this.keywords = this.getValuesWithinLimits(this.educationalMaterial.keywords);
     this.educationalLevels = this.getValuesWithinLimits(this.educationalMaterial.educationalLevels);
-  }
-
-  updateMaterialName(): void {
-    if (this.educationalMaterial.name.find(n => n.language === this.lang).materialname !== '') {
-      this.materialName = this.educationalMaterial.name.find(n => n.language === this.lang).materialname;
-    } else {
-      this.materialName = this.educationalMaterial.name.find(n => n.language === 'fi').materialname;
-    }
-  }
-
-  updateDescription(): void {
-    if (this.educationalMaterial.description.find(d => d.language === this.lang).description !== '') {
-      this.description = this.educationalMaterial.description.find(d => d.language === this.lang).description;
-    } else {
-      this.description = this.educationalMaterial.description.find(d => d.language === 'fi').description;
-    }
   }
 
   getValuesWithinLimits(input: Keyword[] | EducationalLevel[]): string[] {
