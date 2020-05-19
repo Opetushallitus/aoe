fdf3ede3bca064212fe296e76acc1339d04cb72a
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-05-19 13:00:42 +0300

    WIP: Collection service

---
+++ src/app/models/collections/add-to-collection-post.ts
@@ -0,0 +1,4 @@
+export interface AddToCollectionPost {
+  collectionId: number;
+  emId: number[];
+}
---
+++ src/app/models/collections/create-collection-post.ts
@@ -0,0 +1,7 @@
+export interface CreateCollectionPost {
+  name: {
+    fi: string;
+    sv: string;
+    en: string;
+  };
+}
