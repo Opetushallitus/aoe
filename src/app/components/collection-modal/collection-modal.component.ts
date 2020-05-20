8f8768752950f12781717a4a1c862c183d69e045
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-05-20 14:58:25 +0300

    Working collection modal

--- src/app/components/collection-modal/collection-modal.component.ts
+++ src/app/components/collection-modal/collection-modal.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { Collection } from '@models/collections/collection';
+import { UserCollection } from '@models/collections/user-collection';
 import { CollectionService } from '@services/collection.service';
 import { BsModalRef } from 'ngx-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { AddToCollectionPost } from '@models/collections/add-to-collection-post';
+import { AddToCollectionResponse } from '@models/collections/add-to-collection-response';
 
 @Component({
   selector: 'app-collection-modal',
@@ -11,15 +14,18 @@ import { BsModalRef } from 'ngx-bootstrap';
   styleUrls: ['./collection-modal.component.scss']
 })
 export class CollectionModalComponent implements OnInit, OnDestroy {
+  materialId: number;
   newCollectionForm: FormGroup;
-  addToCollectionForm: FormGroup;
+  newCollectionSubmitted = false;
   userCollectionSubscription: Subscription;
-  userCollections: Collection[];
+  userCollections: UserCollection[];
+  selectedCollections: number[] = [];
 
   constructor(
     public bsModalRef: BsModalRef,
     private fb: FormBuilder,
     private collectionSvc: CollectionService,
+    private toastr: ToastrService,
   ) { }
 
   ngOnInit(): void {
@@ -29,16 +35,7 @@ export class CollectionModalComponent implements OnInit, OnDestroy {
       ]),
     });
 
-    this.addToCollectionForm = this.fb.group({
-      collectionId: this.fb.control(null, [
-        Validators.required,
-      ]),
-      emId: this.fb.control(null, [
-        Validators.required,
-      ]),
-    });
-
-    this.userCollectionSubscription = this.collectionSvc.userCollections$.subscribe((collections: Collection[]) => {
+    this.userCollectionSubscription = this.collectionSvc.userCollections$.subscribe((collections: UserCollection[]) => {
       this.userCollections = collections;
     });
     this.collectionSvc.updateUserCollections();
@@ -48,7 +45,58 @@ export class CollectionModalComponent implements OnInit, OnDestroy {
     this.userCollectionSubscription.unsubscribe();
   }
 
-  onCreateCollectionSubmit(): void {}
+  get nameCtrl(): FormControl {
+    return this.newCollectionForm.get('name') as FormControl;
+  }
 
-  onAddToCollectionSubmit(): void {}
+  onCollectionIdChange($event, collectionId: string): void {
+    if ($event.target.checked) {
+      this.selectedCollections.push(+collectionId);
+    } else {
+      this.selectedCollections = this.selectedCollections.filter((collection: number) => collection !== +collectionId);
+    }
+  }
+
+  onCreateCollectionSubmit(): void {
+    this.newCollectionSubmitted = true;
+
+    if (this.newCollectionForm.valid) {
+      if (this.newCollectionForm.dirty) {
+        this.collectionSvc.createCollection(this.newCollectionForm.value).subscribe(() => {
+          // show toast
+          this.toastr.success(`Kokoelma ${this.nameCtrl.value} lisätty onnistuneesti`, 'Kokoelma lisätty');
+
+          // update collections
+          this.collectionSvc.updateUserCollections();
+
+          // reset form
+          this.newCollectionSubmitted = false;
+          this.newCollectionForm.reset();
+        });
+      }
+    }
+  }
+
+  onAddToCollectionSubmit(): void {
+    if (this.selectedCollections.length > 0) {
+      this.selectedCollections.forEach((collection: number) => {
+        const payload: AddToCollectionPost = {
+          collectionId: collection,
+          emId: [
+            this.materialId,
+          ],
+        };
+
+        this.collectionSvc.addToCollection(payload).subscribe((response: AddToCollectionResponse) => {
+          if (response.status === 'ok') {
+            // show toast
+            this.toastr.success('Materiaali lisätty kokoelmaan', 'Kokoelma päivitetty');
+
+            // hide modal
+            this.bsModalRef.hide();
+          }
+        });
+      });
+    }
+  }
 }
