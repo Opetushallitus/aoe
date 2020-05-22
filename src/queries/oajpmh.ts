07d00943f55e597a4c32d51ab3ae3403d5d86e52
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-05-22 13:26:49 +0300

    do not send data where publishedat is null

--- src/queries/oajpmh.ts
+++ src/queries/oajpmh.ts
@@ -7,12 +7,12 @@ const db = connection.db;
 async function getMaterialMetaData(req: Request , res: Response) {
     try {
         const min = req.body.min;
-        let query2 = "select count(*) from educationalmaterial";
+        let query2 = "select count(*) from educationalmaterial where publishedat is not null";
         const params: any = [];
         if (req.body.dateMin !== undefined && req.body.dateMax !== undefined && req.body.materialPerPage !== undefined && req.body.pageNumber !== undefined) {
             params.push(req.body.dateMin);
             params.push(req.body.dateMax);
-            query2 = "select count(*) from educationalmaterial where updatedat >= timestamp $1 and updatedat < timestamp $2";
+            query2 = "select count(*) from educationalmaterial where updatedat >= timestamp $1 and updatedat < timestamp $2 and publishedat is not null";
         }
         const documentcount = await db.oneOrNone(query2, params);
         let pagecount = 0;
@@ -23,7 +23,7 @@ async function getMaterialMetaData(req: Request , res: Response) {
         db.task(async (t: any)  => {
             const params: any = [];
             let query = "select em.id, em.createdat, em.publishedat, em.updatedat, em.archivedat, em.timerequired, em.agerangemin, em.agerangemax, em.licensecode, em.obsoleted, em.originalpublishedat, em.expires, em.suitsallearlychildhoodsubjects, em.suitsallpreprimarysubjects, em.suitsallbasicstudysubjects, em.suitsalluppersecondarysubjects, em.suitsallvocationaldegrees, em.suitsallselfmotivatedsubjects, em.suitsallbranches" +
-            " from educationalmaterial as em order by em.id asc;";
+            " from educationalmaterial as em where em.publishedat is not null order by em.id asc;";
             if (req.body.dateMin !== undefined && req.body.dateMax !== undefined && req.body.materialPerPage !== undefined && req.body.pageNumber !== undefined) {
                 console.log(req.body.dateMin);
                 console.log(req.body.dateMax);
@@ -33,7 +33,7 @@ async function getMaterialMetaData(req: Request , res: Response) {
                 params.push(req.body.pageNumber * req.body.materialPerPage);
                 params.push(req.body.materialPerPage);
                 query = "select em.id, em.createdat, em.publishedat, em.updatedat, em.archivedat, em.timerequired, em.agerangemin, em.agerangemax, em.licensecode, em.obsoleted, em.originalpublishedat, em.expires, em.suitsallearlychildhoodsubjects, em.suitsallpreprimarysubjects, em.suitsallbasicstudysubjects, em.suitsalluppersecondarysubjects, em.suitsallvocationaldegrees, em.suitsallselfmotivatedsubjects, em.suitsallbranches" +
-                " from educationalmaterial as em where em.updatedat >= timestamp $1 and em.updatedat < timestamp $2 order by em.id asc OFFSET $3 LIMIT $4;";
+                " from educationalmaterial as em where em.updatedat >= timestamp $1 and em.updatedat < timestamp $2 and em.publishedat is not null order by em.id asc OFFSET $3 LIMIT $4;";
             }
             console.log(query, params);
             return t.map(query, params, async (q: any) => {
