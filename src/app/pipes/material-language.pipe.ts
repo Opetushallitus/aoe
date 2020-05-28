5e2c44fb22bf35878dcd5f30e73cc7af4d703ae9
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-05-28 14:15:45 +0300

    Added missing return type

--- src/app/pipes/material-language.pipe.ts
+++ src/app/pipes/material-language.pipe.ts
@@ -5,7 +5,7 @@ import { Material } from '@models/material';
   name: 'materialLanguage'
 })
 export class MaterialLanguagePipe implements PipeTransform {
-  transform(materials: Material[], lang: string): any {
+  transform(materials: Material[], lang: string): Material[] {
     return materials.filter((material: Material) => material.language === lang);
   }
 }
