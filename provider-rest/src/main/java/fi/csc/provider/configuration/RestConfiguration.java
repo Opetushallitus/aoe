e12f5ad681d595dc4db4cb55d9716f9233eeb895
mroppon <mika.ropponen@csc.fi>
2020-05-31 23:06:05 +0300

    Defined API documentation and excluded duplicate Guava from JAXB maven dependencies

--- provider-rest/src/main/java/fi/csc/provider/configuration/RestConfiguration.java
+++ provider-rest/src/main/java/fi/csc/provider/configuration/RestConfiguration.java
@@ -3,29 +3,38 @@ package fi.csc.provider.configuration;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
-import org.apache.http.client.HttpClient;
+import com.google.common.base.Predicates;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.TrustStrategy;
 import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import javax.net.ssl.SSLContext;
 import java.security.KeyManagementException;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.X509Certificate;
+import java.util.Collections;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 @Configuration
+@EnableSwagger2
 public class RestConfiguration implements WebMvcConfigurer {
 
     @Bean
@@ -84,5 +93,36 @@ public class RestConfiguration implements WebMvcConfigurer {
         requestFactory.setHttpClient(httpClient);
 
         return new RestTemplate(requestFactory);
+    }
+
+    /**
+     * Swagger UI available at /rest/swagger-ui.html and JSON presentation at /rest/v2/api-docs
+     * @return Docket bean
+     */
+    @Bean
+    public Docket productApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            //.groupName("aoe")
+            .apiInfo(apiInfo())
+            .select()
+            .paths(regex("/oaipmh.*"))
+            //.apis(RequestHandlerSelectors.basePackage("fi.csc.provider.controller"))
+            //.apis(Predicates.not(RequestHandlerSelectors.basePackage("org.springframework.boot")))
+            //.paths(PathSelectors.any())
+            .build();
+            //.pathMapping("/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+            "OAI-PMH Harvesting API",
+            "Avointen Oppimateriaalien Kirjasto - CSC IT Center for Science Ltd.",
+            "2.0",
+            "",
+            new Contact("Contact", "https://aoe.fi", "oppimateriaalivaranto@csc.fi"),
+            "",
+            "",
+            Collections.emptyList()
+        );
     }
 }
--- provider-rest/src/main/java/fi/csc/provider/controller/ProviderController.java
+++ provider-rest/src/main/java/fi/csc/provider/controller/ProviderController.java
@@ -2,6 +2,7 @@ package fi.csc.provider.controller;
 
 import fi.csc.provider.model.xml_oaipmh.OaiPmhFrame;
 import fi.csc.provider.service.MetadataService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletRequest;
 
 @RestController
+@Api(value = "OAI-PMH Harvesting Endpoint")
 public class ProviderController {
 
     private MetadataService metadataService;
@@ -22,28 +24,45 @@ public class ProviderController {
         this.metadataService = metadataService;
     }
 
+    @ApiOperation(value = "Retrieve LRMI metadata batch", response = ResponseEntity.class)
+    @ApiResponses(value = {
+        @ApiResponse(code = 200, message = "Successfully retrieved a LRMI metadata batch")
+    })
     @GetMapping(path = "/oaipmh", produces = MediaType.APPLICATION_XML_VALUE)
     public ResponseEntity<OaiPmhFrame> getMetadata(
-            @RequestParam(required = false, defaultValue = "") String verb,
-            @RequestParam(required = false, defaultValue = "") String identifier,
-            @RequestParam(required = false, defaultValue = "") String metadataPrefix,
-            @RequestParam(required = false, defaultValue = "") String from,
-            @RequestParam(required = false, defaultValue = "") String until,
-            @RequestParam(required = false, defaultValue = "") String resumptionToken,
-            HttpServletRequest request) {
+
+        @ApiParam(name = "verb", value = "OAI-PMH request method like Identify, ListRecords or ListIdentifiers")
+        @RequestParam(required = false, defaultValue = "") String verb,
+
+        @ApiParam(name = "identifier", value = "Identifier")
+        @RequestParam(required = false, defaultValue = "") String identifier,
+
+        @ApiParam(name = "metadataPrefix", value = "Provide the prefix value of oai_dc")
+        @RequestParam(required = false, defaultValue = "") String metadataPrefix,
+
+        @ApiParam(name = "from", value = "URL encoded sequence start time")
+        @RequestParam(required = false, defaultValue = "") String from,
+
+        @ApiParam(name = "until", value = "URL encoded sequence end time")
+        @RequestParam(required = false, defaultValue = "") String until,
+
+        @ApiParam(name = "resumtionToken", value = "Batch sequence number starting from 0")
+        @RequestParam(required = false, defaultValue = "") String resumptionToken,
+
+        HttpServletRequest request) {
         String requestUrl = request.getScheme()
-                + "://"
-                + request.getServerName()
-                + (request.getServerPort() != 0 ? ":" + request.getServerPort() : "")
-                + request.getRequestURI();
+            + "://"
+            + request.getServerName()
+            + (request.getServerPort() != 0 ? ":" + request.getServerPort() : "")
+            + request.getRequestURI();
         return new ResponseEntity<>(
-                this.metadataService.getMetadata(verb, identifier, metadataPrefix, from, until, resumptionToken,
-                    requestUrl),
-                HttpStatus.OK);
+            this.metadataService.getMetadata(verb, identifier, metadataPrefix, from, until, resumptionToken,
+                requestUrl),
+            HttpStatus.OK);
     }
 
     @GetMapping(path = "/health", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> healthCheck() {
-        return new ResponseEntity<>("Alive!", HttpStatus.OK);
+        return new ResponseEntity<>("{\"status\":\"ok\"}", HttpStatus.OK);
     }
 }
