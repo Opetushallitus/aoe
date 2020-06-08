35f2cf73446eace2396600a692cb6678c46e3794
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-06-08 13:23:20 +0300

    remove files from html folder

--- src/aoeScheduler.ts
+++ src/aoeScheduler.ts
@@ -1,3 +1,12 @@
 const fh = require("./queries/fileHandling");
+import { scheduleJob } from "node-schedule";
+import { rmDir } from "./helpers/fileRemover";
+
+// schedule job to start 4.00 server time
+scheduleJob("4 * * *", function() {
+    console.log("Removing files from html folder: " + process.env.HTMLFOLDER);
+    rmDir(process.env.HTMLFOLDER, false);
+});
+
 setInterval(() => fh.checkTemporaryRecordQueue(), 3600000);
 setInterval(() => fh.checkTemporaryAttachmentQueue(), 3600000);
