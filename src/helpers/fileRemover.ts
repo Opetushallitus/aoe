189b25e1a47e3e09d757c8090fd5f5144797dee7
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-06-09 12:59:31 +0300

    fileRemover

---
+++ src/helpers/fileRemover.ts
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const path = require("path");
+
+export function rmDir(dirPath, removeSelf) {
+    try {
+        const files = fs.readdirSync(dirPath);
+        if (files.length > 0)
+        for (let i = 0; i < files.length; i++) {
+            const filePath = path.join(dirPath, files[i]);
+            if (fs.statSync(filePath).isFile())
+                fs.unlinkSync(filePath);
+            else
+                rmDir(filePath, true);
+        }
+        if (removeSelf)
+        fs.rmdirSync(dirPath);
+        }
+        catch (e) {
+            console.error(e);
+            return;
+        }
+  }
