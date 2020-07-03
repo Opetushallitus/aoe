20dd08627565c1bffe08be1788f6e12362d6c68d
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-07-03 13:48:07 +0300

    Added filename validation

---
+++ src/app/pipes/clean-filename.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CleanFilenamePipe } from './clean-filename.pipe';
+
+describe('CleanFilenamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new CleanFilenamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
