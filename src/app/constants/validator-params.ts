1514b8604be80084be3acb28ebc80c4fb6927ab8
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-07-16 12:51:51 +0300

    Added validator parameters

---
+++ src/app/constants/validator-params.ts
@@ -0,0 +1,54 @@
+export const validatorParams = {
+  name: {
+    maxLength: 255,
+  },
+  file: {
+    link: {
+      pattern: 'https?://.*',
+      maxLength: 2000,
+    },
+    displayName: {
+      maxLength: 255,
+    },
+    subtitle: {
+      label: {
+        maxLength: 25,
+      },
+    },
+  },
+  author: {
+    author: {
+      maxLength: 50,
+    },
+  },
+  description: {
+    maxLength: 2000,
+  },
+  educationalFramework: {
+    maxLength: 50,
+  },
+  ageRange: {
+    min: {
+      min: 0,
+      pattern: '[0-9]*',
+      maxLength: 3,
+    },
+    max: {
+      min: 0,
+      pattern: '[0-9]*',
+      maxLength: 3,
+    },
+  },
+  timeRequired: {
+    maxLength: 25,
+  },
+  reference: {
+    url: {
+      pattern: 'https?://.*',
+      maxLength: 2000,
+    },
+    name: {
+      maxLength: 255,
+    },
+  },
+};
