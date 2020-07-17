c875df53dad2a84518b72cb1effdcf475bd52cee
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-07-17 09:22:57 +0300

    Updated list of allowed characters

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -246,7 +246,7 @@ export function validateFilename(value: string): string {
  * @returns {ValidatorFn}
  */
 export function textInputValidator(): ValidatorFn {
-  const textInputRe: RegExp = /[^\wåäö\s.\-!'@#£€$%&()=?,:]/i;
+  const textInputRe: RegExp = /[^\wåäö\s.\-!'´`@#£€$%&()=?,:]/i;
 
   return (control: AbstractControl): {[key: string]: any} | null => {
     const invalid = textInputRe.test(control.value);
@@ -260,7 +260,7 @@ export function textInputValidator(): ValidatorFn {
  * @returns {ValidatorFn}
  */
 export function descriptionValidator(): ValidatorFn {
-  const descriptionRe: RegExp = /[^\wåäö\s.\-§!'"@#£€$%&(){}=?+,;:\/\[\]]/i;
+  const descriptionRe: RegExp = /[^\wåäö\s.\-§!'"´`@#£€$%&(){}=?+,;:\/\[\]]/i;
 
   return (control: AbstractControl): {[key: string]: any} | null => {
     const invalid = descriptionRe.test(control.value);
