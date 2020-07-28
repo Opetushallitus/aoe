be6867a9c8fbad55013f92c1ef3335ff716c4375
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-07-28 14:38:19 +0300

    WIP: Move all required validations to preview tab

--- src/app/views/educational-resource-form/tabs/based-on-details/based-on-details.component.ts
+++ src/app/views/educational-resource-form/tabs/based-on-details/based-on-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
@@ -12,13 +12,11 @@ import { validatorParams } from '../../../../constants/validator-params';
   selector: 'app-tabs-based-on-details',
   templateUrl: './based-on-details.component.html',
 })
-export class BasedOnDetailsComponent implements OnInit {
-  private savedDataKey = environment.newERLSKey;
-  private fileUploadLSKey = environment.fileUploadLSKey;
+export class BasedOnDetailsComponent implements OnInit, OnDestroy {
   lang: string = this.translate.currentLang;
   savedData: any;
 
-  basedOnDetailsForm: FormGroup;
+  form: FormGroup;
   submitted = false;
 
   constructor(
@@ -37,9 +35,9 @@ export class BasedOnDetailsComponent implements OnInit {
       this.setTitle();
     });
 
-    this.savedData = JSON.parse(sessionStorage.getItem(this.savedDataKey));
+    this.savedData = JSON.parse(sessionStorage.getItem(environment.newERLSKey));
 
-    this.basedOnDetailsForm = this.fb.group({
+    this.form = this.fb.group({
       // internals: this.fb.array([ this.createInternal() ]),
       externals: this.fb.array([ this.createExternal() ]),
     });
@@ -55,6 +53,13 @@ export class BasedOnDetailsComponent implements OnInit {
     }
   }
 
+  ngOnDestroy(): void {
+    // save data if its valid, dirty and not submitted
+    if (this.submitted === false && this.form.dirty && this.form.valid) {
+      this.saveData();
+    }
+  }
+
   setTitle(): void {
     this.translate.get('titles.addMaterial').subscribe((translations: any) => {
       this.titleSvc.setTitle(`${translations.main}: ${translations.references} ${environment.title}`);
@@ -62,11 +67,11 @@ export class BasedOnDetailsComponent implements OnInit {
   }
 
   /*get internals() {
-    return this.basedOnDetailsForm.get('internals') as FormArray;
+    return this.form.get('internals') as FormArray;
   }*/
 
   get externals() {
-    return this.basedOnDetailsForm.get('externals') as FormArray;
+    return this.form.get('externals') as FormArray;
   }
 
   /*createInternal(): FormGroup {
@@ -128,25 +133,31 @@ export class BasedOnDetailsComponent implements OnInit {
 
     this.validateExternals();
 
-    if (this.basedOnDetailsForm.valid) {
-      const basedOnData = {
-        isBasedOn: {
-          // internals: this.basedOnDetailsForm.get('internals').value,
-          externals: this.basedOnDetailsForm.get('externals').value,
-        },
-      };
-
-      const data = Object.assign(
-        {},
-        JSON.parse(sessionStorage.getItem(this.savedDataKey)),
-        basedOnData,
-      );
-
-      // save data to session storage
-      sessionStorage.setItem(this.savedDataKey, JSON.stringify(data));
+    if (this.form.valid) {
+      if (this.form.dirty) {
+        this.saveData();
+      }
 
       this.router.navigate(['/lisaa-oppimateriaali', 7]);
     }
+  }
+
+  saveData(): void {
+    const basedOnData = {
+      isBasedOn: {
+        // internals: this.form.get('internals').value,
+        externals: this.form.get('externals').value,
+      },
+    };
+
+    const data = Object.assign(
+      {},
+      JSON.parse(sessionStorage.getItem(environment.newERLSKey)),
+      basedOnData,
+    );
+
+    // save data to session storage
+    sessionStorage.setItem(environment.newERLSKey, JSON.stringify(data));
   }
 
   // @todo: some kind of confirmation
@@ -155,11 +166,11 @@ export class BasedOnDetailsComponent implements OnInit {
     this.submitted = false;
 
     // reset form values
-    this.basedOnDetailsForm.reset();
+    this.form.reset();
 
     // clear data from session storage
-    sessionStorage.removeItem(this.savedDataKey);
-    sessionStorage.removeItem(this.fileUploadLSKey);
+    sessionStorage.removeItem(environment.newERLSKey);
+    sessionStorage.removeItem(environment.fileUploadLSKey);
 
     this.router.navigateByUrl('/');
   }
