c2733eb16fdea587178c5627a95e2cd2a5f0a4a3
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-07-30 15:23:43 +0300

    Added abort editing modal

--- src/app/views/collection-form/collection-form.component.ts
+++ src/app/views/collection-form/collection-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, TemplateRef } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CollectionService } from '@services/collection.service';
