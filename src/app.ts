6e6941bf99cdf597172ae6c276c497540d48268c
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-08-03 10:53:56 +0300

    add redis password

--- src/app.ts
+++ src/app.ts
@@ -26,6 +26,7 @@ const redis = require("redis");
 const redisclient = redis.createClient({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASS,
 }
 );
 const RedisStore = require("connect-redis")(session);
