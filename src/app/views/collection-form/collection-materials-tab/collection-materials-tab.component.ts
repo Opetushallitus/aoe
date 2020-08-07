701794d8cd2e8397b831d391af21740c5176c650
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-07 15:08:05 +0300

    WIP: Materials and headings

--- src/app/views/collection-form/collection-materials-tab/collection-materials-tab.component.ts
+++ src/app/views/collection-form/collection-materials-tab/collection-materials-tab.component.ts
@@ -1,10 +1,12 @@
 import { Component, EventEmitter, Input, OnDestroy, OnInit, Output } from '@angular/core';
-import { CollectionForm } from '@models/collections/collection-form';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { CollectionForm, CollectionFormMaterial, CollectionFormMaterialAndHeading } from '@models/collections/collection-form';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { environment } from '../../../../environments/environment';
+import { validatorParams } from '../../../constants/validator-params';
+import { descriptionValidator, textInputValidator } from '../../../shared/shared.module';
 
 @Component({
   selector: 'app-collection-materials-tab',
@@ -19,6 +21,7 @@ export class CollectionMaterialsTabComponent implements OnInit, OnDestroy {
   form: FormGroup;
   lang = this.translate.currentLang;
   submitted = false;
+  materials = new Map();
 
   constructor(
     private fb: FormBuilder,
@@ -36,12 +39,18 @@ export class CollectionMaterialsTabComponent implements OnInit, OnDestroy {
       this.setTitle();
     });
 
-    this.form = this.fb.group({});
+    this.form = this.fb.group({
+      materialsAndHeadings: this.fb.array([]),
+    });
 
     if (sessionStorage.getItem(environment.collection) === null) {
-      this.form.patchValue(this.collection);
+      this.patchMaterialsAndHeadings(this.collection.materialsAndHeadings);
+      this.mapMaterials(this.collection.materials);
     } else {
-      this.form.patchValue(JSON.parse(sessionStorage.getItem(environment.collection)));
+      const changedCollection = JSON.parse(sessionStorage.getItem(environment.collection)).materialsAndHeadings;
+
+      this.patchMaterialsAndHeadings(changedCollection.materialsAndHeadings);
+      this.mapMaterials(changedCollection.materials);
     }
   }
 
@@ -60,16 +69,84 @@ export class CollectionMaterialsTabComponent implements OnInit, OnDestroy {
     });
   }
 
+  /** @getters */
+  get materialsAndHeadingsArray(): FormArray {
+    return this.form.get('materialsAndHeadings') as FormArray;
+  }
+
+  /**
+   * Patches materials and headings array.
+   * @param {CollectionFormMaterialAndHeading[]} materialsAndHeadings
+   */
+  patchMaterialsAndHeadings(materialsAndHeadings: CollectionFormMaterialAndHeading[]): void {
+    materialsAndHeadings.forEach((materialOrHeading: CollectionFormMaterialAndHeading) => {
+      if (materialOrHeading.id) {
+        this.materialsAndHeadingsArray.push(this.createMaterial(materialOrHeading));
+      } else {
+        this.materialsAndHeadingsArray.push(this.createHeading(materialOrHeading));
+      }
+    });
+  }
+
+  /**
+   * Creates material FormGroup.
+   * @param {CollectionFormMaterialAndHeading} material
+   * @returns {FormGroup}
+   */
+  createMaterial(material: CollectionFormMaterialAndHeading): FormGroup {
+    return this.fb.group({
+      id: this.fb.control(material.id),
+      priority: this.fb.control(material.priority),
+    });
+  }
+
+  /**
+   * Creates heading FormGroup. Fills controls if heading given as param.
+   * @param {CollectionFormMaterialAndHeading|null} heading
+   * @returns {FormGroup}
+   */
+  createHeading(heading?: CollectionFormMaterialAndHeading): FormGroup {
+    return this.fb.group({
+      heading: this.fb.control(heading ? heading.heading : null, [
+        Validators.maxLength(validatorParams.name.maxLength),
+        textInputValidator(),
+      ]),
+      description: this.fb.control(heading ? heading.description : null, [
+        Validators.maxLength(validatorParams.description.maxLength),
+        descriptionValidator(),
+      ]),
+      priority: this.fb.control(heading ? heading.priority : null),
+    });
+  }
+
+  /**
+   * Adds new heading FormGroup
+   * @param {number} idx
+   */
+  addHeading(idx: number): void {
+    this.materialsAndHeadingsArray.insert(idx, this.createHeading());
+  }
+
+  /**
+   * Maps collection materials to materials Map.
+   * @param {CollectionFormMaterial[]} materials
+   */
+  mapMaterials(materials: CollectionFormMaterial[]): void {
+    materials.forEach((material: CollectionFormMaterial) => {
+      this.materials.set(material.id, material);
+    });
+  }
+
   /**
    * Runs on submit. Redirects user to the next tab if form is valid.
    */
   onSubmit(): void {
     this.submitted = true;
 
+    console.log(this.materialsAndHeadingsArray.value);
+
     if (this.form.valid) {
-      if (this.form.dirty) {
-        this.saveCollection();
-      }
+      this.saveCollection();
 
       this.router.navigate(['/kokoelma', this.collectionId, 'muokkaa', this.tabId + 1]);
     }
