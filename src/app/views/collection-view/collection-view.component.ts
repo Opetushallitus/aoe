37d538ca33a530d92e98b9a3400e8c863f15b3f0
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-20 10:08:59 +0300

    Replaced iframe hack with previous solution

--- src/app/views/collection-view/collection-view.component.ts
+++ src/app/views/collection-view/collection-view.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { CollectionService } from '@services/collection.service';
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+import { Material } from '@models/material';
 import { BackendService } from '@services/backend.service';
 import { Title } from '@angular/platform-browser';
 import { environment } from '../../../environments/environment';
@@ -18,11 +19,13 @@ export class CollectionViewComponent implements OnInit, OnDestroy {
   collectionId: string;
   collectionSubscription: Subscription;
   collection: Collection;
+  materialsLoading = new Map();
   collectionMaterials = new Map();
+  previewMaterials = new Map();
   materialLanguages = new Map();
+  selectedLanguages = new Map();
   detailsExpanded = false;
   materialDetails = new Map();
-  iframeSrc: string;
 
   constructor(
     private route: ActivatedRoute,
@@ -38,6 +41,12 @@ export class CollectionViewComponent implements OnInit, OnDestroy {
 
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.lang = event.lang;
+
+      for (const [key, value] of this.materialLanguages.entries()) {
+        if (value.includes(event.lang.toLowerCase())) {
+          this.setSelectedLanguage(key, event.lang.toLowerCase());
+        }
+      }
     });
 
     this.collectionSubscription = this.collectionSvc.collection$.subscribe((collection: Collection) => {
@@ -49,10 +58,40 @@ export class CollectionViewComponent implements OnInit, OnDestroy {
 
       this.setTitle();
       this.setMaterialDetails(collection.educationalMaterials);
+
+      collection.educationalMaterials.forEach((collectionMaterial) => {
+        // set loading true
+        this.materialsLoading.set(collectionMaterial.id, true);
+
+        this.backendSvc.getCollectionMaterials(collectionMaterial.id).subscribe((materials: Material[]) => {
+          // set collection materials
+          this.collectionMaterials.set(collectionMaterial.id, materials);
+
+          // set loading false
+          this.materialsLoading.set(collectionMaterial.id, false);
+
+          // set material languages
+          const materialLanguages = [...new Set(materials.map((material: Material) => material.language.toLowerCase()))];
+          this.materialLanguages.set(collectionMaterial.id, materialLanguages);
+
+          // set default language (1. UI lang, 2. FI, 3. first language in array)
+          this.selectedLanguages.set(collectionMaterial.id, materialLanguages.find((lang: string) => lang === this.lang)
+            ? materialLanguages.find((lang: string) => lang === this.lang)
+            : materialLanguages.find((lang: string) => lang === 'fi')
+              ? materialLanguages.find((lang: string) => lang === 'fi')
+              : materialLanguages[0]
+          );
+
+          // set preview material
+          this.setPreviewMaterial(
+            collectionMaterial.id,
+            materials
+              .find((material: Material) => material.language === this.selectedLanguages.get(collectionMaterial.id))
+          );
+        });
+      });
     });
     this.collectionSvc.updateCollection(this.collectionId);
-
-    this.iframeSrc = `${environment.frontendUrl}/#/kokoelma/materiaali/`;
   }
 
   ngOnDestroy(): void {
@@ -61,6 +100,28 @@ export class CollectionViewComponent implements OnInit, OnDestroy {
 
   setTitle(): void {
     this.titleSvc.setTitle(`${this.collection.name} ${environment.title}`);
+  }
+
+  /**
+   * Sets preview material to selected material.
+   * @param {string} materialId
+   * @param {Material} material
+   */
+  setPreviewMaterial(materialId: string, material: Material): void {
+    this.previewMaterials.set(materialId, material);
+  }
+
+  /**
+   * Sets selected language to preview language. Updates preview material to match selected language.
+   * @param {string} materialId
+   * @param {string} language
+   */
+  setSelectedLanguage(materialId: string, language: string): void {
+    // set selected language
+    this.selectedLanguages.set(materialId, language);
+
+    // set preview material to first material that matches selected language
+    this.setPreviewMaterial(materialId, this.collectionMaterials.get(materialId).find((m) => m.language === language));
   }
 
   /**
