cd1594109734d8ede31f267848dc6759e222bfe2
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-25 11:12:32 +0300

    Enabled maintenance alert

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -59,7 +59,7 @@ export class DefaultLayoutComponent {
   };
 
   showNotice = true;
-  showMaintenanceAlert = false;
+  showMaintenanceAlert = true;
 
   constructor(
     public translate: TranslateService,
@@ -87,12 +87,12 @@ export class DefaultLayoutComponent {
   /**
    * Set language
    */
-  public changeLanguage(lang: string): void {
+  changeLanguage(lang: string): void {
     setLanguage(lang);
     this.translate.use(lang);
   }
 
-  private hideCookieNotice(): void {
+  hideCookieNotice(): void {
     this.showNotice = false;
   }
 }
