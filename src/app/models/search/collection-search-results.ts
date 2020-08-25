db74a01245f62d49ba5ee079f5d8f39f24d61fad
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-25 16:24:39 +0300

    Use actual data from backend

--- src/app/models/search/collection-search-results.ts
+++ src/app/models/search/collection-search-results.ts
@@ -5,14 +5,49 @@ export interface CollectionSearchResults {
 
 export interface CollectionSearchResult {
   id: string;
+  publishedat: Date | null;
+  updatedat: Date | null;
+  createdat: Date | null;
   name: string;
-  thumbnail: string | null;
   description: string;
-  authors: string[];
   keywords: [
     {
       key: string;
       value: string;
     }
   ];
+  languages: string[];
+  alignmentObjects: any[];
+  educationalUses: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  educationalRoles: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  accessibilityHazards: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  accessibilityFeatures: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  educationalLevels: [
+    {
+      key: string;
+      value: string;
+    }
+  ];
+  thumbnail?: string;
+  authors: string[];
 }
