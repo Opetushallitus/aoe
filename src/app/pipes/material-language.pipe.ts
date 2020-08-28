973e7131240e550fbc58766a071ad34e3df76b96
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-28 12:04:41 +0300

    Show material with subtitles even if material lang doesn't match selection

--- src/app/pipes/material-language.pipe.ts
+++ src/app/pipes/material-language.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 import { Material } from '@models/material';
+import { Subtitle } from '@models/subtitle';
 
 @Pipe({
   name: 'materialLanguage'
 })
 export class MaterialLanguagePipe implements PipeTransform {
   transform(materials: Material[], lang: string): Material[] {
-    return materials.filter((material: Material) => material.language === lang);
+    return materials.filter((material: Material) => {
+      if (material.language === lang || material.subtitles.find((subtitle: Subtitle) => subtitle.srclang === lang)) {
+        return material;
+      }
+    });
   }
 }
