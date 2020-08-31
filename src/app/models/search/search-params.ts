0a51ca38a104e4cdcff27decfdb90eee46365cf7
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-08-31 13:08:48 +0300

    Updated model

---
+++ src/app/models/search/search-params.ts
@@ -0,0 +1,16 @@
+export interface SearchParams {
+  keywords: string | null;
+  filters: {
+    languages?: string[] | null;
+    educationalLevels?: string[] | null;
+    learningResourceTypes?: string[] | null;
+    authors?: string[] | null;
+    organizations?: string[] | null;
+    educationalRoles?: string[] | null;
+    keywords?: string[] | null;
+    educationalSubjects?: string[] | null;
+    teaches?: string[] | null;
+  };
+  from?: number;
+  size?: number;
+}
