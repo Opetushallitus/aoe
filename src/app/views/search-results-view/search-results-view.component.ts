4aa667167e5d0f9b9df18c2e287f9c8bfbc33bdb
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-09-25 11:20:19 +0300

    PoC: Selected filters are displayed as tags

--- src/app/views/search-results-view/search-results-view.component.ts
+++ src/app/views/search-results-view/search-results-view.component.ts
@@ -55,7 +55,7 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
   showAllRoles = true;
   isCollapsedKeywords = true;
   showAllKeywords = true;
-  selectedFilters: any[] = [];
+  usedFilters: any[] = [];
 
   constructor(
     private searchSvc: SearchService,
@@ -115,7 +115,7 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
       this.searchFilters = filters;
 
       this.setAvailableFilters(filters);
-      this.updateSelectedFilters();
+      this.updateUsedFilters();
 
       this.showAllLanguages = this.languagesArray.controls.length > this.filtersShownAtFirst;
       this.showAllSubjects = this.subjectsArray.controls.length > this.filtersShownAtFirst;
@@ -294,15 +294,15 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
     return (this.page - 1) * this.resultsPerPage;
   }
 
-  updateSelectedFilters(): void {
-    const selectedFilters: any[] = [];
+  updateUsedFilters(): void {
+    const usedFilters: any[] = [];
 
     // languages
     this.filters.value.languages
       .map((checked: boolean, index: number) => checked ? { key: this.searchFilters.languages[index], value: this.getLanguageLabel(this.searchFilters.languages[index]), type: 'language', index: index } : null)
       .filter((language: any) => {
         if (language !== null) {
-          selectedFilters.push(language);
+          usedFilters.push(language);
         }
       });
 
@@ -311,11 +311,11 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
       .map((checked: boolean, index: number) => checked ? { ...this.learningResourceTypes[index], type: 'type', index: index } : null)
       .filter((value: LearningResourceType) => {
         if (value !== null) {
-          selectedFilters.push(value);
+          usedFilters.push(value);
         }
       });
 
-    this.selectedFilters = selectedFilters;
+    this.usedFilters = usedFilters;
   }
 
   removeFilter(key: string, type: string, index: number) {
@@ -323,6 +323,7 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
       case 'language':
         this.languagesArray.at(index).setValue(false);
         break;
+
       case 'type':
         this.learningResourceTypesArray.at(index).setValue(false);
         break;
@@ -331,7 +332,9 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
         break;
     }
 
-    this.selectedFilters = this.selectedFilters.filter((filter: any) => filter.key !== key);
+    this.onSubmit();
+
+    // this.usedFilters = this.usedFilters.filter((filter: any) => filter.key !== key);
   }
 
   setAvailableFilters(searchFilters: SearchFilters): void {
