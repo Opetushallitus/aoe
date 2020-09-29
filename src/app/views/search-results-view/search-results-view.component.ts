6c650066413963291eab4b1bf53a56d518bce817
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-09-29 11:21:27 +0300

    Removed redundant line

--- src/app/views/search-results-view/search-results-view.component.ts
+++ src/app/views/search-results-view/search-results-view.component.ts
@@ -348,7 +348,6 @@ export class SearchResultsViewComponent implements OnInit, OnDestroy {
   setAvailableFilters(searchFilters: SearchFilters): void {
     const searchParams: SearchParams = JSON.parse(sessionStorage.getItem(environment.searchParams));
 
-    // this.usedFilters = [];
     this.languagesArray.clear();
     this.subjectsArray.clear();
     this.teachesArray.clear();
