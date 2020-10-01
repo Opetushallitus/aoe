34984461608c4d719eea63d1c1acfc5062c9a7e5
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-10-01 08:25:45 +0300

    rtf and others

--- src/helpers/officeToPdfConverter.ts
+++ src/helpers/officeToPdfConverter.ts
@@ -63,10 +63,28 @@ const officeMimeTypes = [
     "application/vnd.ms-powerpoint.slideshow.macroEnabled.12",
 
     // .mdb
-    "application/vnd.ms-access"];
+    "application/vnd.ms-access",
+// openoffice
+    "application/rtf",
+    "application/vnd.oasis.opendocument.text",
+    "application/vnd.oasis.opendocument.spreadsheet",
+    "application/vnd.oasis.opendocument.presentation",
+    "application/vnd.oasis.opendocument.graphics",
+    "application/vnd.oasis.opendocument.chart",
+    "application/vnd.oasis.opendocument.formula",
+    "application/vnd.oasis.opendocument.image",
+    "application/vnd.oasis.opendocument.text-master",
+    "application/vnd.oasis.opendocument.text-template",
+    "application/vnd.oasis.opendocument.spreadsheet-template",
+    "application/vnd.oasis.opendocument.presentation-template",
+    "application/vnd.oasis.opendocument.graphics-template",
+    "application/vnd.oasis.opendocument.chart-template",
+    "application/vnd.oasis.opendocument.formula-template",
+    "application/vnd.oasis.opendocument.image-template",
+    "application/vnd.oasis.opendocument.text-web"
+];
 
 export async function isOfficeMimeType(s: string) {
-    console.log("isOfficeMimeType: " + s);
     if (officeMimeTypes.indexOf(s) >= 0) {
         return true;
     }
