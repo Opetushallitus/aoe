3e5de247c136599bc14fe710b1d75a10a2fc075c
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-10-20 10:41:10 +0300

    Fixed search via tag clicking

--- src/app/components/taglist/taglist.component.ts
+++ src/app/components/taglist/taglist.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { SearchParams } from '@models/search/search-params';
 import { environment } from '../../../environments/environment';
 import { Router } from '@angular/router';
+import { UsedFilter } from '@models/search/used-filter';
 
 @Component({
   selector: 'app-taglist',
@@ -28,17 +29,26 @@ export class TaglistComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  search(searchValue: string): void {
+  search(key: string, value: string): void {
     const searchParams: SearchParams = {
       keywords: null,
       filters: {
-        [this.filterType]: [searchValue],
+        [this.filterType]: [key],
       },
       from: this.from,
       size: this.resultsPerPage,
     };
 
+    const usedFilters: UsedFilter[] = [
+      {
+        key: key,
+        value: value,
+        type: this.filterType,
+      }
+    ];
+
     sessionStorage.setItem(environment.searchParams, JSON.stringify(searchParams));
+    sessionStorage.setItem(environment.usedFilters, JSON.stringify(usedFilters));
 
     this.router.navigate(['/haku']);
   }
