157a3d217057ba690f5bd860c71997a81d9d4d9c
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-10-26 14:14:36 +0200

    change message text

--- src/services/mailService.ts
+++ src/services/mailService.ts
@@ -20,21 +20,9 @@ export async function sendExpirationMail() {
     const mailOptions = {
         from: process.env.EMAIL_FROM,
         to: undefined,
-        subject: "Sending Email using Node.js",
-        text: "That was easy!"
+        subject: "Materiaali vanhenee - Avointen oppimateriaalien kirjasto (aoe.fi)",
+        text: expirationEmailText
       };
-    // const testAccount = await createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
-//   const transporter = createTransport({
-//     host: "smtp.ethereal.email",
-//     port: 587,
-//     secure: false, // true for 465, false for other ports
-//     auth: {
-//       user: testAccount.user, // generated ethereal user
-//       pass: testAccount.pass, // generated ethereal password
-//     },
-//   });
     try {
         const materials = await getExpiredMaterials();
         console.log(materials);
@@ -47,9 +35,6 @@ export async function sendExpirationMail() {
         else {
             const materials = await getExpiredMaterials();
             console.log(materials);
-            // const info = await transporter.sendMail(mailOptions);
-            // console.log("Message sent: %s", info.messageId);
-            // console.log("Message sent: %s", info.response);
             for (const element of emailArray) {
                 mailOptions.to = element;
                 const info = await transporter.sendMail(mailOptions);
@@ -62,13 +47,6 @@ export async function sendExpirationMail() {
         console.log(error);
     }
 }
-// transporter.sendMail(mailOptions, function(error, info){
-//   if (error) {
-//     console.log(error);
-//   } else {
-//     console.log("Email sent: " + info.response);
-//   }
-// });
 
 export async function getExpiredMaterials() {
     const query = "select distinct email from educationalmaterial join users on educationalmaterial.usersusername = username where expires < NOW() + INTERVAL '3 days' and expires >= NOW() + INTERVAL '1 days';";
@@ -77,7 +55,6 @@ export async function getExpiredMaterials() {
 }
 
 export async function updateEmail(user: string, email: string) {
-    // req.session.passport.user.uid
     try {
         const query = "update users set email = $1, verifiedemail = false where username = $2;";
         await db.none(query, [email, user]);
@@ -88,7 +65,6 @@ export async function updateEmail(user: string, email: string) {
 }
 
 export async function updateVerifiedEmail(user: string) {
-    // req.session.passport.user.uid
     try {
         const query = "update users set verifiedemail = true where username = $1;";
         await db.none(query, [user]);
@@ -131,11 +107,12 @@ export async function sendVerificationEmail(user: string, email: string) {
 
     const url = process.env.BASE_URL + "verify?id=" + token_mail_verification;
     console.log(url);
+    console.log(verificationEmailText);
     const mailOptions = {
         from: process.env.EMAIL_FROM,
         to: email,
-        subject: "Email verification",
-        text: "Click on the link below to veriy your email " + url
+        subject: "Sähköpostin vahvistus - Avointen oppimateriaalien kirjasto (aoe.fi)",
+        text: verificationEmailText
     };
     if (process.env.SEND_EMAIL) {
     const info = await transporter.sendMail(mailOptions);
@@ -163,3 +140,37 @@ export async function verifyEmailToken(req: Request, res: Response, next: NextFu
         return res.sendStatus(403);
     }
 }
+
+const verificationEmailText =
+"Hei,\n" +
+"olet syöttänyt sähköpostisi Avointen oppimateriaalien kirjaston Omat tiedot -sivulle. Vahvista sähköpostiosoitteesi ilmoitusten vastaanottamiseksi klikkaamalla linkkiä: \n" +
+"${url}\n" +
+"Ystävällisin terveisin,\n" +
+"AOE-tiimi\n\n" +
+"Hi,\n" +
+"you have submitted your email at the My Account page at the Library of Open Educational Resources. To receive notifications please verify your email by clicking here:\n" +
+"${url}\n" +
+"Best regards,\n" +
+"AOE Team\n\n" +
+"Hej,\n" +
+"du har angett din e-postadress på sidan Mitt konto i Biblioteket för öppna lärresurser. Bekräfta din e-postadress för att få meddelanden genom att klicka på länken: \n" +
+"${url}\n" +
+"Med vänlig hälsning,\n" +
+"AOE-team\n";
+
+const expirationEmailText =
+"Hei,\n" +
+"Oppimateriaalille asettamasi vanhenee-päivämäärä lähestyy. Voit halutessasi muokata vanhenee-päivämäärää ja tarvittaessa päivittää materiaalisi Omat oppimateriaalit –näkymässä.\n" +
+"Ystävällisin terveisin,\n" +
+"AOE-tiimi\n" +
+"Tämä on automaattinen viesti. Mikäli et halua enää saada näitä viestejä, voit muuttaa viestiasetuksia Avointen oppimateriaalien kirjaston Omat tiedot –näkymässä.\n\n" +
+"Hi,\n" +
+"The expires date you have given to your educational resource is near. You can change the date and update your resource from My open educational resources view.\n" +
+"Best Regards,\n" +
+"AOE Team\n" +
+"This is an automated message. If you do not wish to receive these messages anymore you can change your settings in the My Account view at the Library of Open Educational Resources.\n\n" +
+"Hej,\n" +
+"Det föråldras-datum som du har gett till din lärresurs är nära. Du kan redigera det föråldras-datum och uppdatera din lärresurs från Mina lärresurser-sidan.\n" +
+"Med vänliga hälsningar,\n" +
+"AOE-team\n" +
+"Detta är ett automatiskt meddelande. Om du vill inte få dessa meddelandena, kan du förändra dina inställningar I vyn Mitt konto på Biblioteket för öppna lärresurser.";
