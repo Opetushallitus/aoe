871ad205bfdaf8f7bffd0b867f1ca7dcbe02de7a
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-10-28 13:31:44 +0200

    update user settings

---
+++ src/users/userSettings.ts
@@ -0,0 +1,58 @@
+import { Request, Response, NextFunction } from "express";
+import { sendVerificationEmail } from "./../services/mailService";
+import { ErrorHandler } from "./../helpers/errorHandler";
+
+const connection = require("./../db");
+const pgp = connection.pgp;
+const db = connection.db;
+export interface UserSettings {
+    notifications: {
+      newRatings: boolean;
+      almostExpired: boolean;
+      termsUpdated: boolean;
+    };
+    email: string;
+    allowTransfer: boolean;
+}
+
+export async function updateUserSettings(req: Request, res: Response, next: NextFunction) {
+  try {
+    if (!req.isAuthenticated()) {
+      return res.sendStatus(403);
+    }
+    else if (!req.body) {
+      return res.sendStatus(404);
+    }
+    const settings: UserSettings = req.body;
+    await updateNotificationSettings(req.session.passport.user.uid, settings);
+    if (settings.email) {
+      await updateEmail(req.session.passport.user.uid, settings.email);
+      await sendVerificationEmail(req.session.passport.user.uid, settings.email);
+    }
+    return res.status(200).json({"status": "ok"});
+  }
+  catch (error) {
+    console.error(error);
+    next(new ErrorHandler(500, "Issue updating user settings"));
+  }
+}
+
+export async function updateEmail(user: string, email: string) {
+  try {
+      const query = "update users set email = $1, verifiedemail = false where username = $2;";
+      await db.none(query, [email, user]);
+  }
+  catch (error) {
+      throw new Error(error);
+  }
+}
+
+export async function updateNotificationSettings(user: string, settings: UserSettings) {
+  try {
+    const query = "update users set newratings = $1, almostexpired = $2, termsupdated = $3, allowtransfer = $4 where username = $5;";
+    await db.none(query, [settings.notifications.newRatings, settings.notifications.almostExpired, settings.notifications.termsUpdated, settings.allowTransfer, user]);
+  }
+  catch (error) {
+      throw new Error(error);
+  }
+}
