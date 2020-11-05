63192c4cea1a841bf338083d1903e0c97f6e2391
Jussi Nieminen <jussi.nieminen@csc.fi>
2020-11-05 11:50:41 +0200

    logging added

--- src/server.ts
+++ src/server.ts
@@ -19,6 +19,25 @@ const server = app.listen(process.env.PORT || 3000, () => {
   console.log(" Press CTRL-C to stop\n");
   console.log(server.address());
 });
+
+server.on("connection", function (socket) {
+  console.log("SOCKET OPENED" + JSON.stringify(socket.address()));
+socket.on("end", function () {
+  console.log("SOCKET END: other end of the socket sends a FIN packet");
+});
+
+socket.on("timeout", function () {
+  console.log("SOCKET TIMEOUT");
+});
+
+socket.on("error", function (error) {
+  console.log("SOCKET ERROR: " + JSON.stringify(error));
+});
+
+socket.on("close", function (had_error) {
+  console.log("SOCKET CLOSED. IT WAS ERROR: " + had_error);
+});
+});
 // const server = app.listen(3000, "127.0.0.1");
 
 export default server;
