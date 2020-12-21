1c245d502c5c9582191ae63e65c84337b3e5862d
Kalle Lehtonen <kalle.lehtonen@digia.com>
2020-12-21 14:05:43 +0200

    Removed redundant lines

--- src/app/guards/unsaved-changes.guard.ts
+++ src/app/guards/unsaved-changes.guard.ts
@@ -19,10 +19,6 @@ export class UnsavedChangesGuard implements CanDeactivate<ComponentCanDeactivate
     const currentPath = currentState.url.substring(1).split('/').slice(0, -1);
     const nextPath = nextState.url.substring(1).split('/').slice(0, -1);
 
-    // console.log(JSON.stringify(currentPath) === JSON.stringify(nextPath));
-    // console.log(currentPath);
-    // console.log(nextPath);
-
     return JSON.stringify(currentPath) !== JSON.stringify(nextPath)
       ? component.canDeactivate()
       : true;
