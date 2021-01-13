499993ec8745f8fd490f1f93bd9459748cbd46c6
Kalle Lehtonen <kalle.lehtonen@digia.com>
2021-01-13 12:14:28 +0200

    Added urn

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -276,6 +276,7 @@ export class BackendService {
         viewCounter: +material.viewCounter,
         downloadCounter: +material.downloadCounter,
         typicalAgeRange: material.typicalAgeRange,
+        urn: material.urn,
       });
     });
   }
