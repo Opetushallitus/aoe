65cb9e9a27c1392a0bb377f315b4163dec5a8916
Mika Ropponen <mroppone@csc.fi>
2021-03-03 15:26:35 +0200

    JavaDoc comments for the metadata service methods

--- provider-rest/src/main/java/fi/csc/provider/service/impl/MetadataServiceImpl.java
+++ provider-rest/src/main/java/fi/csc/provider/service/impl/MetadataServiceImpl.java
@@ -97,6 +97,14 @@ public class MetadataServiceImpl implements MetadataService {
                 .setSampleIdentifier(env.getProperty("aoe.oai-identifier.sample-identifier"));
     }
 
+    /**
+     * Fetch, convert and build OAI-PMH metadata for the harvesting response.
+     * @param frame           OAI-PMH frame metadata structure {@link fi.csc.provider.model.xml_oaipmh.OaiPmhFrame}.
+     * @param from            Format: "2018-09-10 09:11:40" - interval start date.
+     * @param until           Format: "2018-09-10 09:11:40" - interval end date.
+     * @param resumptionToken Integer number for the next batch.
+     * @param identifiersOnly Boolean value for the identifiers (headers) only request.
+     */
     private void setLrmiMetadata(OaiPmhFrame frame, String from, String until, String resumptionToken, boolean identifiersOnly) {
         Integer resumptionCounter = resolveResumptionValue(resumptionToken);
 
@@ -135,6 +143,14 @@ public class MetadataServiceImpl implements MetadataService {
         }
     }
 
+    /**
+     * Set OAI-PMH resumption token to the end of metadata structure. Resumption token is a number of the next
+     * metadata batch. Token is not present in the last batch of OAI-PMH metadata.
+     * @param frame             OAI-PMH frame metadata structure {@link fi.csc.provider.model.xml_oaipmh.OaiPmhFrame}.
+     * @param aoeMetaFrame      Generic data model for ListIdentifiers and ListRecords requests.
+     * @param resumptionCounter Integer helper variable to provide the value of the next batch.
+     * @param identifiersOnly   Boolean value to set the resumption token to identifiers data model.
+     */
     private void addResumptionToken(OaiPmhFrame frame, AoeMetaFrame<?> aoeMetaFrame, Integer resumptionCounter, boolean identifiersOnly) {
         Integer next = ++resumptionCounter;
         Integer cursor = aoeMetaFrame.getPageNumber() * aoeMetaFrame.getMaterialPerPage();
@@ -154,6 +170,11 @@ public class MetadataServiceImpl implements MetadataService {
             aoeMetaFrame.getCompleteListSize(), cursor, resumptionString));
     }
 
+    /**
+     * Convert String value of the resumption token into an integer.
+     * @param resumptionToken String presentation of the numeric resumption token from the request parameters.
+     * @return Integer value.
+     */
     private Integer resolveResumptionValue(String resumptionToken) {
         if (resumptionToken.isEmpty()) {
             return 0;
