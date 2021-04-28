27e140676ef6a6c494846661ff55816da3305e2d
Kalle Lehtonen <kalle.lehtonen@digia.com>
2021-04-28 10:22:23 +0300

    JIRA-891 Added download link to unsupported files, domain to links

--- src/app/components/educational-material-preview/educational-material-preview.component.ts
+++ src/app/components/educational-material-preview/educational-material-preview.component.ts
@@ -1,16 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { Material } from '@models/material';
 import { mimeTypes } from '../../constants/mimetypes';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-educational-material-preview',
   templateUrl: './educational-material-preview.component.html',
   styleUrls: ['./educational-material-preview.component.scss']
 })
-export class EducationalMaterialPreviewComponent {
+export class EducationalMaterialPreviewComponent implements OnInit {
   @Input() material: Material;
   @Input() isCollection = false;
+  lang: string = this.translate.currentLang;
+
+  constructor(
+    private translate: TranslateService,
+  ) { }
+
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+  }
 
   public checkMimeType(mimeType: string) {
     if (mimeTypes.video.includes(mimeType)) {
