b3f42646e091fd7d3bb6b660a4c2cdb844bd7caf
Kalle Lehtonen <kalle.lehtonen@digia.com>
2021-08-19 16:15:25 +0300

    Introduced index.ts files for cleaner import statements

---
+++ src/app/views/educational-material-edit-form/tabs/index.ts
@@ -0,0 +1,7 @@
+export * from './edit-based-on-details/edit-based-on-details.component';
+export * from './edit-basic-details/edit-basic-details.component';
+export * from './edit-educational-details/edit-educational-details.component';
+export * from './edit-files/edit-files.component';
+export * from './edit-license/edit-license.component';
+export * from './edit-extended-details/edit-extended-details.component';
+export * from './edit-preview/edit-preview.component';
