b0f10cf9e6c77807558de34d20b15f428dae2db8
Kalle Lehtonen <kalle.lehtonen@digia.com>
2021-10-01 15:07:01 +0300

    Fixed lint errors

--- src/app/directives/focus-remover.directive.ts
+++ src/app/directives/focus-remover.directive.ts
@@ -1,12 +1,11 @@
 import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: 'button, a, [role=button]'
+  // eslint-disable-next-line
+  selector: 'button, a, [role=button]',
 })
 export class FocusRemoverDirective {
-  constructor(
-    private elRef: ElementRef,
-  ) { }
+  constructor(private elRef: ElementRef) {}
 
   @HostListener('click') onClick() {
     this.elRef.nativeElement.blur();
