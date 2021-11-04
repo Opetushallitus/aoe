3d37e9576782ad48a7bbf771f54146e7fe3ba54e
Mika Ropponen <mroppone@csc.fi>
2021-11-04 09:28:28 +0200

    Retry maximum for S3 download requests

--- src/service/storage-service.ts
+++ src/service/storage-service.ts
@@ -1,5 +1,5 @@
 import AWS, { S3 } from 'aws-sdk';
-import { BucketName, GetObjectRequest, ObjectKey } from 'aws-sdk/clients/s3';
+import { BucketName, ClientConfiguration, GetObjectRequest, ObjectKey } from 'aws-sdk/clients/s3';
 import { ServiceConfigurationOptions } from 'aws-sdk/lib/service';
 import contentDisposition from 'content-disposition';
 import { Request, Response } from 'express';
@@ -8,20 +8,23 @@ import { winstonLogger } from '../util';
 export const getObjectAsStream = async (req: Request, res: Response): Promise<any> => {
     return new Promise(async (resolve, reject) => {
         try {
-            const config: ServiceConfigurationOptions = {
+            const configAWS: ServiceConfigurationOptions = {
                 accessKeyId: process.env.STORAGE_KEY as string,
                 secretAccessKey: process.env.STORAGE_SECRET as string,
                 endpoint: process.env.STORAGE_URL as string,
                 region: process.env.STORAGE_REGION as string
             };
+            const configS3: ClientConfiguration = {
+                maxRetries: 10
+            };
             const fileName: string = req.params.filename as string;
-            const params: GetObjectRequest = {
+            const requestObject: GetObjectRequest = {
                 Bucket: process.env.STORAGE_BUCKET as BucketName,
                 Key: fileName as ObjectKey
             };
-            AWS.config.update(config);
-            const s3: S3 = new AWS.S3();
-            const fileStream = s3.getObject(params).createReadStream();
+            AWS.config.update(configAWS);
+            const s3: S3 = new AWS.S3(configS3);
+            const fileStream = s3.getObject(requestObject).createReadStream();
             res.attachment(fileName);
             res.header('Content-Disposition', contentDisposition(fileName));
             fileStream
