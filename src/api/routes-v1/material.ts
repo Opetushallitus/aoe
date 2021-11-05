2c2dde125ed41736b7e840cc3752ba617297022e
Mika Ropponen <mroppone@csc.fi>
2021-11-05 16:10:21 +0200

    Added milliseconds to logging timestamp

--- src/api/routes-v1/material.ts
+++ src/api/routes-v1/material.ts
@@ -15,7 +15,7 @@ export default (router: Router): void => {
             return next();
         } catch (error) {
             res.removeHeader('Content-Disposition');
-            error.message = `Download failed for ${req.params.filename as string}`;
+            error.message = `Download from the object storage failed for ${req.params.filename as string}`;
             return next(error);
         }
     });
