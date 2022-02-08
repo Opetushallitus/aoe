36f2abdb7be6895a84511e921c914445be6d3849
Mika Ropponen <mroppone@csc.fi>
2022-02-08 15:00:41 +0200

    Modular project structure for data processing microservices

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# aoe-data-analytics
+# AOE Data Analytics
 
+Java-based microservices for the processing of message queues and analytical data storages.
+
+Logical application components are separated in the modular structure of the project.
+
+## Status Check
+Status check endpoint provided by Actuator at
+<pre>[root]/actuator/health</pre>
+
+## Compile Project
+Compile the project with all its modules at root
+<pre>mvn package</pre>
+OR repackage
+<pre>mvn clean package spring-boot:repackage</pre>
+
+## Run Project
+Run the project with all its modules at root
+<pre>mvn spring-boot:run</pre>
+OR individually
+<pre>mvn spring-boot:run -pl [module]</pre>
---
+++ service-etl-processor/src/main/java/fi/csc/processor/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package fi.csc.processor.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/").permitAll();
+    }
+
+    /*@Bean
+    public SecurityWebFilterChain securityWebFilterChain(
+            ServerHttpSecurity http) {
+        return http.authorizeExchange()
+                .pathMatchers("/actuator/**").permitAll()
+                .anyExchange().authenticated()
+                .and().build();
+    }*/
+}
