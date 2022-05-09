d6646802d34a2623fd58ee7dfff4973353c68e82
Mika Ropponen <mroppone@csc.fi>
2022-05-09 13:12:15 +0300

    Formal improvements for Digivisio logging

--- src/util/digivisioLogger.ts
+++ src/util/digivisioLogger.ts
@@ -2,6 +2,6 @@ import winstonLogger from './winstonLogger';
 import { NextFunction, Request, Response } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
-    winstonLogger.log('dw', 'HTTP: %s', JSON.stringify(req));
+    winstonLogger.log('dw', 'HTTP: %s %s [%o]', req.method, req.originalUrl, req.body);
     next();
 };
