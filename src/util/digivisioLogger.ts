6edbf916b2c2e31ecd0e9e4fa043452ef64e369e
Mika Ropponen <mroppone@csc.fi>
2022-05-09 15:07:53 +0300

    Inline logging for Digivisio request information

--- src/util/digivisioLogger.ts
+++ src/util/digivisioLogger.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
     try {
-        winstonLogger.log('digi', '%s %s %o', req.method, req.originalUrl, req.headers);
+        winstonLogger.log('digi', '%s %s %s', req.method, req.originalUrl, JSON.stringify(req.headers));
     } catch (error) {
         winstonLogger.error(`Digivisio logging failed: ${error}`);
     }
