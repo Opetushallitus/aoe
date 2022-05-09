8a10fa68c76ceb7c63280aa941b49a0612e84435
Mika Ropponen <mroppone@csc.fi>
2022-05-09 13:30:14 +0300

    Formal improvements for Digivisio logging (3)

--- src/util/digivisioLogger.ts
+++ src/util/digivisioLogger.ts
@@ -3,7 +3,7 @@ import { NextFunction, Request, Response } from 'express';
 
 export default (req: Request, res: Response, next: NextFunction): void => {
     try {
-        winstonLogger.log('dw', 'HTTP: %s %s %s', req.method, req.originalUrl, req.headers);
+        winstonLogger.log('dw', 'HTTP: %s %s' + req.method, req.originalUrl);
     } catch (error) {
         winstonLogger.error(`DW logging failed: ${error}`);
     }
