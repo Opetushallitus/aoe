f4b953f8f63efec261f7e488eb3ef443f15a01b2
Mika Ropponen <mroppone@csc.fi>
2022-05-11 13:39:02 +0300

    Winston refctoring

--- src/util/winston-logger.ts
+++ src/util/winston-logger.ts
@@ -21,7 +21,7 @@ const consoleOptions: ConsoleTransportOptions = {
 };
 
 // Dedicated file logging and rotation for the Digivisio HTTP requests.
-const digivisioLogPath: string = process.env.DIGIVISIO_LOG_PATH as string;
+// const digivisioLogPath: string = process.env.DIGIVISIO_LOG_PATH as string;
 
 // Configuration for logging format and transports
 export default winston.createLogger({
@@ -33,10 +33,11 @@ export default winston.createLogger({
     ),
     levels: loggingLevels.levels,
     transports: [
-        new winston.transports.Console(consoleOptions),
         new winston.transports.File({
-            filename: `${digivisioLogPath}/digivisio.log`,
+            filename: 'digivisio/digivisio.log',
+            handleExceptions: true,
             level: 'digi'
         }),
-    ]
+        new winston.transports.Console(consoleOptions),
+    ],
 });
