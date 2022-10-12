99cece551319159e584b51e0ad24fff79a9b78eb
Aino Tani <atani@csc.fi>
2022-10-12 19:46:59 +0300

    embed-view module created

---
+++ src/app/embed-view/preview-materials/educational-material-preview/educational-material-preview.component.ts
@@ -0,0 +1,50 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { Material } from '@models/material';
+import { mimeTypes } from '../../../constants/mimetypes';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-educational-material-preview',
+  templateUrl: './educational-material-preview.component.html',
+  styleUrls: ['./educational-material-preview.component.scss'],
+})
+export class EducationalMaterialPreviewComponent implements OnInit {
+  @Input() material: Material;
+  @Input() isCollection = false;
+  lang: string = this.translate.currentLang;
+
+  constructor(private translate: TranslateService) {}
+
+  ngOnInit(): void {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+  }
+
+  public checkMimeType(mimeType: string): string {
+    if (mimeTypes.video.includes(mimeType)) {
+      return 'video';
+    }
+
+    if (mimeTypes.audio.includes(mimeType)) {
+      return 'audio';
+    }
+
+    if (mimeTypes.pdf.includes(mimeType)) {
+      return 'pdf';
+    }
+
+    if (mimeTypes.html.includes(mimeType)) {
+      return 'html';
+    }
+
+    if (mimeTypes.office.includes(mimeType)) {
+      return 'office';
+    }
+
+    if (mimeTypes.image.includes(mimeType)) {
+      return 'image';
+    }
+  }
+}
