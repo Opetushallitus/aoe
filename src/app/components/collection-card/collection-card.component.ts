78c256bd8ec43e7c8d4302b8a0cf48f1e30177b3
Mika Ropponen <mroppone@csc.fi>
2022-10-26 13:47:50 +0300

    Collection card language tags updated with async observable

--- src/app/components/collection-card/collection-card.component.ts
+++ src/app/components/collection-card/collection-card.component.ts
@@ -5,10 +5,11 @@ import {
     CollectionCardKeyword,
 } from '@models/collections/collection-card';
 import { getValuesWithinLimits } from '../../shared/shared.module';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { KoodistoProxyService } from '@services/koodisto-proxy.service';
 import { TranslateService } from '@ngx-translate/core';
 import { Language } from '@models/koodisto-proxy/language';
+import { filter, map, tap } from 'rxjs/operators';
 
 @Component({
     selector: 'app-collection-card',
@@ -21,19 +22,28 @@ export class CollectionCardComponent implements OnInit, OnDestroy {
     educationalLevels: CollectionCardEducationalLevel[];
     keywords: CollectionCardKeyword[];
     languageSubscription: Subscription;
-    languages: Language[];
+    languages$: Observable<string[]>;
 
     constructor(private translate: TranslateService, private koodistoSvc: KoodistoProxyService) {}
 
     ngOnInit(): void {
+        this.languages$ = this.koodistoSvc.languages$.asObservable().pipe(
+            map((languages: Language[]) =>
+                languages.filter((language: Language) =>
+                    this.collection.languages.includes(language.key.toLowerCase()),
+                ),
+            ),
+            map((languages: Language[]) => languages.map((language: Language) => language.value)),
+        );
         // this.translate.onLangChange.subscribe(() => {
         //     this.koodistoSvc.updateLanguages();
         // });
-        this.languageSubscription = this.koodistoSvc.languages$.subscribe((languages: Language[]) => {
-            this.languages = languages.filter((lang: Language) =>
-                this.collection.languages.includes(lang.key.toLowerCase()),
-            );
-        });
+        // this.languageSubscription = this.koodistoSvc.languages$.subscribe((languages: Language[]) => {
+        //     this.languages = languages.filter((lang: Language) =>
+        //         this.collection.languages.includes(lang.key.toLowerCase()),
+        //     );
+        //     console.debug('');
+        // });
         // this.koodistoSvc.updateLanguages();
         this.educationalLevels = getValuesWithinLimits(this.collection.educationalLevels, 'value');
         this.keywords = getValuesWithinLimits(this.collection.keywords, 'value');
