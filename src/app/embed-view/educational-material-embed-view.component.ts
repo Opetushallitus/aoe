f6f59fa125c825c03ca36167e7186b987dc8e9d0
Aino Tani <atani@csc.fi>
2022-10-31 14:48:27 +0200

    english as secondary default language in educational-material-embed-view.component

--- src/app/embed-view/educational-material-embed-view.component.ts
+++ src/app/embed-view/educational-material-embed-view.component.ts
@@ -73,6 +73,9 @@ export class EducationalMaterialEmbedViewComponent implements OnInit, OnDestroy 
                 this.materials = this.educationalMaterial.materials.filter((m: Material) => m.language === this.lang);
             } else {
                 this.materials = this.educationalMaterial.materials.filter((m: Material) => m.language === 'fi');
+                if (this.materials.length == 0) {
+                  this.materials = this.educationalMaterial.materials.filter((m) => m.language === 'en');
+                }        
             }
 
             if (this.materials.length > 0) {
