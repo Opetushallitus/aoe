ebe062ad4f907bd8973b08cc401b78124f457764
Mika Ropponen <mroppone@csc.fi>
2022-11-23 13:50:55 +0200

    Removed unnecessary Configuration annotation alongside EnableWebSecurity

--- service-etl-processor/src/main/java/fi/csc/processor/configuration/SecurityConfiguration.java
+++ service-etl-processor/src/main/java/fi/csc/processor/configuration/SecurityConfiguration.java
@@ -1,14 +1,12 @@
 package fi.csc.processor.configuration;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfiguration {
