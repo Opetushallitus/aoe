43d610bdd0b4f189cdc54e623b2ff3525a43e320
Mika Ropponen <mroppone@csc.fi>
2022-11-24 16:35:02 +0200

    Organizations and relevant timestamps added to the activity collection

--- service-etl-processor/src/main/java/fi/csc/processor/model/Metadata.java
+++ service-etl-processor/src/main/java/fi/csc/processor/model/Metadata.java
@@ -3,9 +3,14 @@ package fi.csc.processor.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class Metadata {
+    private LocalDateTime created;
+    private LocalDateTime updated;
+    private String[] organizations;
     private String[] educationalLevels;
     private String[] educationalSubjects;
 }
