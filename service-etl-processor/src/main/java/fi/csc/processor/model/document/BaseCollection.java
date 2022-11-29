651060e1006439a88408bf10e42ba2305c531d5e
Mika Ropponen <mroppone@csc.fi>
2022-11-29 10:32:55 +0200

    Session ID indexing for all collections

--- service-etl-processor/src/main/java/fi/csc/processor/model/document/BaseCollection.java
+++ service-etl-processor/src/main/java/fi/csc/processor/model/document/BaseCollection.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
@@ -17,8 +18,9 @@ public class BaseCollection implements Serializable {
     @Id
     protected ObjectId id;
 
-//    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    protected LocalDateTime timestamp;
+    @Indexed
     protected String sessionId;
+
+    protected LocalDateTime timestamp;
 
 }
