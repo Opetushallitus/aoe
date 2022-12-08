c7b778b53fe862293828abdc8499e9c66c6b013a
Mika Ropponen <mroppone@csc.fi>
2022-12-08 23:11:58 +0200

    Resolved dual data source and Hibernate configuration issues

--- module-rdb-analytics/src/main/java/fi/csc/analytics/configuration/JPAConfigurationPrimary.java
+++ module-rdb-analytics/src/main/java/fi/csc/analytics/configuration/JPAConfigurationPrimary.java
@@ -1,18 +1,19 @@
 package fi.csc.analytics.configuration;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -26,16 +27,23 @@ public class JPAConfigurationPrimary {
     @Primary
     @Bean(name = "entityManagerFactoryPrimary")
     public LocalContainerEntityManagerFactoryBean entityManagerFactoryPrimary(
-        @Qualifier("dataSourcePrimary") DataSource dataSource,
-        EntityManagerFactoryBuilder builder) {
-        return builder
-            .dataSource(dataSource)
-            .build();
+        @Qualifier("dataSourcePrimary") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("fi.csc.analytics.entity");
+        entityManagerFactoryBean.setPersistenceUnitName("primary");
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
     }
 
     @Bean(name = "transactionManagerPrimary")
     public PlatformTransactionManager transactionManagerPrimary(
-        @Qualifier("entityManagerFactoryPrimary") LocalContainerEntityManagerFactoryBean todosEntityManagerFactory) {
-        return new JpaTransactionManager(Objects.requireNonNull(todosEntityManagerFactory.getObject()));
+        @Qualifier("entityManagerFactoryPrimary") LocalContainerEntityManagerFactoryBean entityManagerFactoryPrimary) {
+        return new JpaTransactionManager(Objects.requireNonNull(entityManagerFactoryPrimary.getObject()));
     }
 }
--- module-rdb-analytics/src/main/java/fi/csc/analytics/configuration/JPAConfigurationSecondary.java
+++ module-rdb-analytics/src/main/java/fi/csc/analytics/configuration/JPAConfigurationSecondary.java
@@ -1,17 +1,18 @@
 package fi.csc.analytics.configuration;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 import java.util.Objects;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -24,16 +25,23 @@ public class JPAConfigurationSecondary {
 
     @Bean(name = "entityManagerFactorySecondary")
     public LocalContainerEntityManagerFactoryBean entityManagerFactorySecondary(
-        @Qualifier("dataSourceSecondary") DataSource dataSource,
-        EntityManagerFactoryBuilder builder) {
-        return builder
-            .dataSource(dataSource)
-            .build();
+        @Qualifier("dataSourceSecondary") DataSource dataSource) {
+        LocalContainerEntityManagerFactoryBean entityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+        entityManagerFactoryBean.setDataSource(dataSource);
+        entityManagerFactoryBean.setPackagesToScan("fi.csc.analytics.entity");
+        entityManagerFactoryBean.setPersistenceUnitName("secondary");
+        entityManagerFactoryBean.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+
+        Properties jpaProperties = new Properties();
+        jpaProperties.put(org.hibernate.cfg.Environment.DIALECT, "org.hibernate.dialect.PostgreSQL95Dialect");
+        entityManagerFactoryBean.setJpaProperties(jpaProperties);
+
+        return entityManagerFactoryBean;
     }
 
     @Bean(name = "transactionManagerSecondary")
     public PlatformTransactionManager transactionManagerSecondary(
-        @Qualifier("entityManagerFactorySecondary") LocalContainerEntityManagerFactoryBean todosEntityManagerFactory) {
-        return new JpaTransactionManager(Objects.requireNonNull(todosEntityManagerFactory.getObject()));
+        @Qualifier("entityManagerFactorySecondary") LocalContainerEntityManagerFactoryBean entityManagerFactorySecondary) {
+        return new JpaTransactionManager(Objects.requireNonNull(entityManagerFactorySecondary.getObject()));
     }
 }
