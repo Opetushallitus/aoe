8c32c533a4634a4dd6eb848ccc8acc27693ef79e
atani <atani@csc.fi>
2023-01-05 09:50:27 +0200

    filter updates moved from analytics component to admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { environment } from '../../environments/environment';
+import { KoodistoService } from './services/koodisto.service';
 
 @Component({
     selector: 'app-admin',
@@ -9,9 +10,12 @@ import { environment } from '../../environments/environment';
     encapsulation: ViewEncapsulation.None,
 })
 export class AdminComponent implements OnInit {
-    constructor(private titleSvc: Title) {}
+    constructor(private titleSvc: Title,  public koodistoService: KoodistoService,) {}
 
     ngOnInit(): void {
         this.titleSvc.setTitle(`Bryssel ${environment.title}`);
+        this.koodistoService.updateOrganizations();
+        this.koodistoService.updateEducationalLevels();
+        this.koodistoService.updateSubjectFilters();
     }
 }
