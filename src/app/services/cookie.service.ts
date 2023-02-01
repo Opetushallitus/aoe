8540bf6efab0b7b0c194ddd39012a80f16d40d0f
atani <atani@csc.fi>
2023-02-01 09:49:07 +0200

    lint fix

--- src/app/services/cookie.service.ts
+++ src/app/services/cookie.service.ts
@@ -17,32 +17,30 @@ export class CookieService {
     isCookiePolicyAccepted(): boolean {
         try {
             return sessionStorage.getItem('cookiePolicy') === 'accepted' || this.cookiePolicyAccepted === true;
-        } catch( e ) {
+        } catch (e) {
             return this.cookiePolicyAccepted;
         }
-        
     }
-    
+
     /**
      * Checks if browser has sessionStorage and saves cookie policy acceptance.
-     * 
+     *
      */
     acceptCookiePolicy(): void {
         let support = false;
         try {
-            sessionStorage.setItem('test','true');
-            if (typeof window.sessionStorage !== "undefined" && sessionStorage.getItem('test') === 'true') {
+            sessionStorage.setItem('test', 'true');
+            if (typeof window.sessionStorage !== 'undefined' && sessionStorage.getItem('test') === 'true') {
                 support = true;
             }
             sessionStorage.removeItem('test');
-          }
-          catch( e ) {
+        } catch (e) {
             support = false;
-            console.log( e );
+            console.log(e);
         }
 
         if (support) {
-            sessionStorage.setItem('cookiePolicy','accepted');
+            sessionStorage.setItem('cookiePolicy', 'accepted');
             this.cookiePolicyAccepted = true;
         } else {
             this.cookiePolicyAccepted = true;
