a1ba8ebb06108fee149a4b240915afa0829ff380
atani <atani@csc.fi>
2023-06-28 12:57:57 +0300

    Enumeration file added for categories, activities and intervals

---
+++ src/app/admin/model/enumeration/Categories.ts
@@ -0,0 +1,18 @@
+export enum Categories {
+    EDUCATIONAL_LEVEL = 'educationalLevels',
+    EDUCATIONAL_SUBJECT = 'educationalSubjects',
+    ORGANIZATION = 'organizations',
+}
+
+export enum Activities {
+    SEARCH = 'Searched',
+    VIEW = 'Viewed',
+    EDIT = 'Edited',
+    DOWNLOAD = 'Downloaded',
+}
+
+export enum Intervals {
+    DAY = 'day',
+    WEEK = 'week',
+    MONTH = 'month',
+}
