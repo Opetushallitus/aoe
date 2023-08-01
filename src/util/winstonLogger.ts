c976f1c246d2076d5363dd110a2813d99fcb7b0c
Mika Ropponen <mroppone@csc.fi>
2023-08-01 08:04:34 +0300

    Removed unused logging functions

--- src/util/winstonLogger.ts
+++ src/util/winstonLogger.ts
@@ -3,6 +3,7 @@ import { ConsoleTransportOptions } from "winston/lib/winston/transports";
 
 // Custom logging levels
 const loggingLevels = {
+<<<<<<< Updated upstream
     levels: {
         digi: 0,
         info: 1,
@@ -11,6 +12,15 @@ const loggingLevels = {
         warn: 4,
         debug: 5,
     }
+=======
+  levels: {
+    info: 0,
+    http: 1,
+    error: 2,
+    warn: 3,
+    debug: 4,
+  },
+>>>>>>> Stashed changes
 };
 
 // Options for console logging
@@ -25,6 +35,7 @@ const consoleOptions: ConsoleTransportOptions = {
 
 // Configuration for logging format and transports
 export const winstonLogger = winston.createLogger({
+<<<<<<< Updated upstream
     exitOnError: false,
     format: format.combine(
         format.splat(), // Use also printf format with argument specifiers %d %s %o etc.
@@ -40,6 +51,23 @@ export const winstonLogger = winston.createLogger({
         }),
         new winston.transports.Console(consoleOptions),
     ],
+=======
+  exitOnError: false,
+  format: format.combine(
+    format.splat(), // Use also printf format with argument specifiers %d %s %o etc.
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf((log: Logform.TransformableInfo) => `[${log.level.toUpperCase()}] ${log.timestamp} ${log.message}`),
+  ),
+  levels: loggingLevels.levels,
+  transports: [
+    // new winston.transports.File({
+    //   filename: 'digivisio/digivisio.log',
+    //   handleExceptions: true,
+    //   level: 'digi',
+    // }),
+    new winston.transports.Console(consoleOptions),
+  ],
+>>>>>>> Stashed changes
 });
 
 export default {
