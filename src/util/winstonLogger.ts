13eb81af7bd9cdc1ed781564f2cab612e6624054
Mika Ropponen <mroppone@csc.fi>
2023-10-21 08:42:35 +0300

    Explicitly disabled TLS in the DinD configuration

--- src/util/winstonLogger.ts
+++ src/util/winstonLogger.ts
@@ -4,7 +4,6 @@ import { ConsoleTransportOptions } from 'winston/lib/winston/transports';
 // Custom logging levels
 const loggingLevels = {
   levels: {
-    // digi: 0,
     info: 0,
     http: 1,
     error: 2,
@@ -20,9 +19,6 @@ const consoleOptions: ConsoleTransportOptions = {
   handleExceptions: true,
 };
 
-// Dedicated file logging and rotation for the Digivisio HTTP requests.
-// const digivisioLogPath: string = process.env.DIGIVISIO_LOG_PATH as string;
-
 // Configuration for logging format and transports
 export const winstonLogger = winston.createLogger({
   exitOnError: false,
@@ -32,14 +28,7 @@ export const winstonLogger = winston.createLogger({
     format.printf((log: Logform.TransformableInfo) => `[${log.level.toUpperCase()}] ${log.timestamp} ${log.message}`),
   ),
   levels: loggingLevels.levels,
-  transports: [
-    // new winston.transports.File({
-    //   filename: 'digivisio/digivisio.log',
-    //   handleExceptions: true,
-    //   level: 'digi',
-    // }),
-    new winston.transports.Console(consoleOptions),
-  ],
+  transports: [new winston.transports.Console(consoleOptions)],
 });
 
 export default winstonLogger;
