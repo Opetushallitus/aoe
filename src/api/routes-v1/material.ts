b01af40c2f40380eb91d013946e494e8da79a7fb
Mikael Saipori <mroppone@csc.fi>
2024-01-31 09:54:46 +0000

    Some file and metadata management endpoints refactored and moved to API v2

--- src/api/routes-v1/material.ts
+++ src/api/routes-v1/material.ts
@@ -1,11 +1,6 @@
 import { NextFunction, Request, Response, Router } from 'express';
 import { updateEducationalMaterialMetadata } from '../../controllers/educationalMaterial';
-import {
-  addLinkToMaterial,
-  getEducationalMaterialMetadata,
-  setEducationalMaterialObsoleted,
-} from '../../queries/apiQueries';
-import { downloadMaterialFile } from '../../queries/fileHandling';
+import { getEducationalMaterialMetadata, setEducationalMaterialObsoleted } from '../../queries/apiQueries';
 import { checkAuthenticated, hasAccessToPublicatication } from '../../services/authService';
 import { runMessageQueueThread } from '../../services/threadService';
 import { winstonLogger } from '../../util/winstonLogger';
@@ -39,27 +34,19 @@ export default (router: Router): void => {
   // :publishedat format 'YYYY-MM-DDTHH:mm:ss.SSSZ' (ISODate) - regex path validation in API v2.0.
   // :edumaterialid defined as a number between 1 to 6 digits to prevent similar endpoints collision.
   router.get(
-    '/material/:edumaterialid([0-9]{1,6})/:publishedat?',
+    '/material/:edumaterialid([0-9]{1,6})/:publishedat([0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{3}Z)?',
     (req: Request, res: Response, next: NextFunction) => {
-      getEducationalMaterialMetadata(req, res, next, false).catch(() => {
+      getEducationalMaterialMetadata(req, res, next, false).catch((): void => {
         winstonLogger.error('Metadata request failed for a single file download.');
       });
     },
     (req: Request, res: Response) => {
       if (['view', 'edit'].includes(req.query.interaction as string)) {
-        runMessageQueueThread(req, res).then((result) => {
+        runMessageQueueThread(req, res).then((result): void => {
           if (result) winstonLogger.debug('THREAD: Message queue publishing completed for %o', result);
         });
       }
       res.end();
     },
   );
-
-  // Download all files related to an educational material and stream as a single zip file from the cloud object storage.
-  // :publishedat format: 'YYYY-MM-DDTHH:mm:ss.SSSZ' (ISODate) - regex path validation in API v2.0.
-  // :edumaterialid defined as a number between 1 to 6 digits to prevent similar endpoints collision.
-  router.get('/material/file/:edumaterialid([0-9]{1,6})/:publishedat?', downloadMaterialFile);
-
-  // Save a link type material to an educational material.
-  router.post('/material/link/:edumaterialid', checkAuthenticated, hasAccessToPublicatication, addLinkToMaterial);
 };
