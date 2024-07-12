07bef8e3c9c93657a82cb11268d6772e11ce136b
Mikael Saipori <saipori@pm.me>
2024-07-12 14:46:08 +0300

    Removed user identifier from the new notification result

--- src/api/routes-v2/process.ts
+++ src/api/routes-v2/process.ts
@@ -11,12 +11,13 @@ import { NextFunction, Request, Response, Router } from 'express';
 
 /**
  * API version 2.0 for requesting application system processes.
- * This module is a collection of endpoints starting with /process/.
+ * This module is a collection of endpoints starting with /api/v2/process/.
  * @param router express.Router
  */
 export default (router: Router): void => {
   const moduleRoot = '/process';
 
+  // Get currently active notifications.
   router.get(
     `${moduleRoot}/notifications`,
     (req: Request, res: Response, next: NextFunction): void => {
@@ -33,6 +34,7 @@ export default (router: Router): void => {
     },
   );
 
+  // Get currently active and upcoming notifications.
   router.get(
     `${moduleRoot}/notifications/all`,
     hasAccessToAOE,
@@ -50,6 +52,7 @@ export default (router: Router): void => {
     },
   );
 
+  // Save a new scheduled notification.
   router.post(
     `${moduleRoot}/notifications`,
     hasAccessToAOE,
@@ -69,6 +72,7 @@ export default (router: Router): void => {
     },
   );
 
+  // Update a notification to disabled.
   router.patch(
     `${moduleRoot}/notifications/:id`,
     hasAccessToAOE,
