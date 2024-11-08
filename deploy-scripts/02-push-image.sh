a9a0cff8a2fac35fbdfd17ceda6d64b6a6a106b9
Mikko Kauhanen <mikko.kauhanen@reaktor.com>
2024-11-08 15:14:37 +0200

    Add rest of the services for build

--- deploy-scripts/02-push-image.sh
+++ deploy-scripts/02-push-image.sh
@@ -5,7 +5,17 @@ set -o errexit -o nounset -o pipefail
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 readonly CURRENT_DIR
 
-# Run semantic-api buildscript
+# Run semantic-api push image script
 "${CURRENT_DIR}/../aoe-semantic-apis/deploy-scripts/02-push-image.sh"
 
+# Run streaming app push image script
 "${CURRENT_DIR}/../aoe-streaming-app/deploy-scripts/02-push-image.sh"
+
+# Run web backend push image script
+"${CURRENT_DIR}/../aoe-web-backend/deploy-scripts/02-push-image.sh"
+
+# Run data analytics push image script
+"${CURRENT_DIR}/../aoe-data-analytics/deploy-scripts/02-push-image.sh"
+
+# Run data analytics push image script
+"${CURRENT_DIR}/../aoe-data-services/deploy-scripts/02-push-image.sh"
