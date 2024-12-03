8ad51e2f0d493b030e5ada815ebf35e5128323c6
Harri Salokorpi <harri.salokorpi@reaktor.com>
2024-12-03 15:29:36 +0200

    Setup frontend build in Github

--- aoe-infra/infra/lib/front-end-bucket-stack.ts
+++ aoe-infra/infra/lib/front-end-bucket-stack.ts
@@ -1,35 +1,46 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib/core'
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { BucketAccessControl, ObjectOwnership } from 'aws-cdk-lib/aws-s3'
+import { RemovalPolicy } from 'aws-cdk-lib/core'
 
 
 interface FrontendBucketStackProps extends StackProps {
-    //    domain: string
-        environment: string,
-        cloudFrontDistribution: cloudfront.Distribution,
-    }
-    
+    environment: string,
+    cloudFrontDistribution: cloudfront.Distribution,
+}
+
 export class FrontendBucketStack extends Stack {
     readonly bucket: s3.Bucket;
     constructor(scope: Construct, id: string, props: FrontendBucketStackProps) {
-    super(scope, id, props);
+        super(scope, id, props);
 
-// FrontEnd S3 bucket - OAI does not support KMS - encryption
-    this.bucket = new s3.Bucket(this, 'FrontEndBucket', {
-        bucketName: `aoe-static-content-${props.environment}`,
-        enforceSSL: true,
-        // encryption: s3.BucketEncryption.KMS,
-        // encryptionKey: props.s3KmsKey,
-    });
+        // FrontEnd S3 bucket - OAI does not support KMS - encryption
+        this.bucket = new s3.Bucket(this, 'FrontEndBucket', {
+            bucketName: `aoe-static-content-${props.environment}`,
+            enforceSSL: true,
+            accessControl: BucketAccessControl.PRIVATE,
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            versioned: true, // Required for taking backups
+            objectOwnership: ObjectOwnership.BUCKET_OWNER_PREFERRED, // Required for restoring backups
+            lifecycleRules: [
+                {
+                    id: 'ExpireOldVersions',
+                    noncurrentVersionExpiration: cdk.Duration.days(30), // Retain old versions for 30 days
+                }
+            ],
+            removalPolicy: RemovalPolicy.RETAIN
+        });
 
-// CloudFront OAI, Origin & behaviour
-    const s3oai = new cloudfront.OriginAccessIdentity(this, 'OAI');
-    const s3origin = new origins.S3Origin(this.bucket, { originAccessIdentity: s3oai });
-    
-    props.cloudFrontDistribution.addBehavior('/static/*', s3origin, { 
-        viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS 
-    });
+        // CloudFront OAI, Origin & behaviour
+        const s3oai = new cloudfront.OriginAccessIdentity(this, 'OAI');
+        const s3origin = new origins.S3Origin(this.bucket, { originAccessIdentity: s3oai });
+
+        props.cloudFrontDistribution.addBehavior('/static/*', s3origin, {
+            viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS
+        });
     }
 } 
