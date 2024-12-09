584b724eed3e281f6820bd47d85b16b202f4f16f
Mikko Kauhanen <mikko.kauhanen@reaktor.com>
2024-12-09 08:47:25 +0200

    Add aws-msk-iam-sasl-signer-js for kafka connection

--- aoe-web-backend/src/resource/kafkaClient.ts
+++ aoe-web-backend/src/resource/kafkaClient.ts
@@ -1,10 +1,30 @@
 import config from '@/config';
 import { Kafka, Partitioners, Producer } from 'kafkajs';
+import { generateAuthToken } from 'aws-msk-iam-sasl-signer-js';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const kafka: Kafka = new Kafka({
   clientId: config.MESSAGE_QUEUE_OPTIONS.clientId as string,
   brokers: config.MESSAGE_QUEUE_OPTIONS.brokerServers.split(',') as string[],
+  ...(isProd
+    ? {
+        ssl: true,
+        sasl: {
+          mechanism: 'oauthbearer',
+          oauthBearerProvider: () => oauthBearerTokenProvider(config.MESSAGE_QUEUE_OPTIONS.region),
+        },
+      }
+    : {}),
 });
+
+async function oauthBearerTokenProvider(region) {
+  // Uses AWS Default Credentials Provider Chain to fetch credentials
+  const authTokenResponse = await generateAuthToken({ region });
+  return {
+    value: authTokenResponse.token,
+  };
+}
 
 export const kafkaProducer: Producer = kafka.producer({
   allowAutoTopicCreation: true,
