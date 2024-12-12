5c27082c44cda628594f24f3bdcabd75e714eeb3
Harri Salokorpi <harri.salokorpi@reaktor.com>
2024-12-12 16:52:59 +0200

    Refactor frontend build to run build steps in separate shells

---
+++ aoe-web-frontend/deploy-scripts/scripts/build-configuration.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# shellcheck source=../scripts/common-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../scripts/common-functions.sh"
+
+# shellcheck source=./deploy-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../deploy-scripts/deploy-functions.sh"
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../../../scripts/build-functions.sh"
+
+function main {
+  local aoe_service_name="aoe-web-frontend"
+  local service_image_tag="AOE_WEB_FRONTEND_TAG"
+
+  configuration=${1:-none}
+  case $configuration in
+    "dev"|"qa"|"prod")
+      echo "Building for configuration ${configuration}"
+      ;;
+    *)
+      echo "FRONTEND_CONFIGURATION not set to correct value (got $configuration)"
+      exit 1
+      ;;
+  esac
+
+  cd "$repo"
+
+  buildService "$aoe_service_name-${configuration}" "$service_image_tag"
+}
+
+main $1
+
+
