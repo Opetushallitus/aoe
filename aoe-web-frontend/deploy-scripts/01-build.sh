31213c0f4994235e2f2b2ec174aeabe36d39fde7
Harri Salokorpi <harri.salokorpi@reaktor.com>
2024-12-12 15:59:12 +0200

    Change reserved name ENVIRONMENT to FRONTEND_CONFIGURATION and streamline passing

--- aoe-web-frontend/deploy-scripts/01-build.sh
+++ aoe-web-frontend/deploy-scripts/01-build.sh
@@ -14,9 +14,9 @@ function main {
 
   cd "$repo"
 
-  FRONTEND_ENVIRONMENT=dev buildService "$aoe_service_name-dev" "$service_image_tag"
-  FRONTEND_ENVIRONMENT=qa buildService "$aoe_service_name-qa" "$service_image_tag"
-  FRONTEND_ENVIRONMENT=prod buildService "$aoe_service_name-prod" "$service_image_tag"
+  FRONTEND_CONFIGURATION=dev buildService "$aoe_service_name-dev" "$service_image_tag"
+  FRONTEND_CONFIGURATION=qa buildService "$aoe_service_name-qa" "$service_image_tag"
+  FRONTEND_CONFIGURATION=prod buildService "$aoe_service_name-prod" "$service_image_tag"
 }
 
 main
