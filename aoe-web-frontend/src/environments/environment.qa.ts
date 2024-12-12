39712aa022e07c811091e76a90bb2bc8f5df4afa
Harri Salokorpi <harri.salokorpi@reaktor.com>
2024-12-12 15:49:25 +0200

    Introduce dev and qa environments to Angular configuration

---
+++ aoe-web-frontend/src/environments/environment.qa.ts
@@ -0,0 +1,28 @@
+/**
+ * @ignore
+ */
+export const environment = {
+  production: true,
+  loginUrl: 'https://qa.aoe.fi/api',
+  backendUrl: 'https://qa.aoe.fi/api/v1',
+  backendUrlV2: 'https://qa.aoe.fi/api/v2',
+  embedBackendUrl: 'https://lessons.qa.aoe.fi/embed',
+  statisticsBackendUrl: 'https://qa.aoe.fi/api/v2/statistics/test',
+  frontendUrl: 'https://qa.aoe.fi',
+  newERLSKey: 'aoe.new-educational-resource',
+  koodistoUrl: 'https://qa.aoe.fi/ref/api/v1',
+  sessionCookie: 'connect.sid',
+  userdataKey: 'aoe.userdata',
+  cookieSettingsCookie: 'aoe.cookies',
+  searchParams: 'aoe.searchParams',
+  searchResults: 'aoe.searchResults',
+  usedFilters: 'aoe.usedFilters',
+  editMaterial: 'aoe.editMaterial',
+  title: '- Avointen oppimateriaalien kirjasto (qa.aoe.fi)',
+  collection: 'aoe.collection',
+  collectionSearchParams: 'aoe.collectionSearchParams',
+  collectionSearchResults: 'aoe.collectionSearchResults',
+  sessionMaxAge: 60 * 60 * 8 * 1000, // 8 hours
+  disableForms: 'aoe.disableForms',
+  disableLogin: 'aoe.disableLogin',
+};
