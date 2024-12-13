1f7a0a9acb3f73f2c6d2555cfcca79f0a6a4584b
Esa Hakala <esa.hakala@gofore.com>
2024-12-13 17:19:16 +0200

    Added domain and cert

--- aoe-infra/infra/lib/cloudfront-stack.ts
+++ aoe-infra/infra/lib/cloudfront-stack.ts
@@ -4,30 +4,37 @@ import { Construct } from 'constructs';
 import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-// import * as route53 from 'aws-cdk-lib/aws-route53';
-// import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
 
 
 interface CloudfrontStackProps extends StackProps {
-//    domain: string
+    domain: string
+    publicHostedZone: route53.IHostedZone
 //    environment: string,
     alb: elbv2.ILoadBalancerV2
+    certificate: acm.ICertificate
     // bucket: s3.Bucket,
 }
 
 export class CloudfrontStack extends Stack {
   readonly distribution: cloudfront.Distribution;
+  readonly certificate: acm.Certificate;
   constructor(scope: Construct, id: string, props: CloudfrontStackProps) {
     super(scope, id, props);
 
+// route53 alias record for cloudfront
+    new route53.ARecord(this, 'AliasRecord', {
+      zone: props.publicHostedZone,
+      recordName: props.domain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(this.distribution)),
+      });
 
-// // new certificate
-    // const certificate = new acm.Certificate(this, 'Certificate', {
-    // domainName: props.domain,
-    // validation: acm.CertificateValidation.fromDns(),
-    // });
 
     this.distribution = new cloudfront.Distribution(this, 'Distribution', {
+      domainNames: [props.domain],
+      certificate: props.certificate,
       defaultBehavior: {
         origin: new origins.LoadBalancerV2Origin(props.alb),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
@@ -37,11 +44,6 @@ export class CloudfrontStack extends Stack {
 
     // this.distribution.addBehavior('/static/*', s3origin, { viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS});
 
-    // route53 alias record for cloudfront
-    // new route53.ARecord(this, 'AliasRecord', {
-    //     zone: route53.HostedZone.fromLookup(this, 'Domain', { domainName: props.domain }),
-    //     target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(distribution)),
-    //     });
 
     // Add s3 bucket as a new origin for the CloudFront distribution
     // this.distribution.addBehavior('/static/*', new origins.S3Origin(props.bucket ),{
