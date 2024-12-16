0564416d664f722ad15abbabfd219c00d1acb107
Mikko Kauhanen <mikko.kauhanen@reaktor.com>
2024-12-16 10:18:25 +0200

    Fix aoe frontend cdk ecs stack

--- aoe-infra/infra/bin/infra.ts
+++ aoe-infra/infra/bin/infra.ts
@@ -322,7 +322,7 @@ if (environmentName == 'dev' || environmentName == 'qa' || environmentName == 'p
     healthCheckGracePeriod: 180,
     healthCheckInterval: 5,
     healthCheckTimeout: 2,
-    albPriority: 104,
+    albPriority: 140,
     privateDnsNamespace: namespace.privateDnsNamespace,
     iAmPolicyStatements: [kafkaClusterIamPolicy, kafkaTopicIamPolicy, kafkaGroupIamPolicy]
   })
@@ -353,7 +353,7 @@ if (environmentName == 'dev' || environmentName == 'qa' || environmentName == 'p
     healthCheckGracePeriod: 180,
     healthCheckInterval: 5,
     healthCheckTimeout: 2,
-    albPriority: 101,
+    albPriority: 110,
     iAmPolicyStatements: [s3PolicyStatement],
     privateDnsNamespace: namespace.privateDnsNamespace
   })
@@ -384,7 +384,7 @@ if (environmentName == 'dev' || environmentName == 'qa' || environmentName == 'p
     healthCheckGracePeriod: 180,
     healthCheckInterval: 5,
     healthCheckTimeout: 2,
-    albPriority: 103,
+    albPriority: 130,
     privateDnsNamespace: namespace.privateDnsNamespace
   })
 
@@ -458,7 +458,7 @@ if (environmentName == 'dev' || environmentName == 'qa' || environmentName == 'p
     healthCheckGracePeriod: 180,
     healthCheckInterval: 5,
     healthCheckTimeout: 2,
-    albPriority: 102,
+    albPriority: 120,
     iAmPolicyStatements: [aossPolicyStatement, s3PolicyStatement,
       efsPolicyStatement, kafkaClusterIamPolicy, kafkaTopicIamPolicy
     ],
@@ -498,20 +498,18 @@ if (environmentName == 'dev' || environmentName == 'qa' || environmentName == 'p
     minimumCount: environmentConfig.services.web_frontend.min_count,
     maximumCount: environmentConfig.services.web_frontend.max_count,
     cpuArchitecture: CpuArchitecture.X86_64,
-    env_vars: {
-      ...environmentConfig.services.web_frontend.env_vars
-    },
+    env_vars: {},
     parameter_store_secrets: [],
     secrets_manager_secrets: [],
     utilityAccountId: utilityAccountId,
     alb: Alb.alb,
     listener: Alb.albListener,
-    listenerPathPatterns: ["/static/*"],
-    healthCheckPath: "/",
+    listenerPathPatterns: ["/*"],
+    healthCheckPath: "/health",
     healthCheckGracePeriod: 180,
     healthCheckInterval: 5,
     healthCheckTimeout: 2,
-    albPriority: 102,
+    albPriority: 49000,
     iAmPolicyStatements: [],
     privateDnsNamespace: namespace.privateDnsNamespace,
   })
