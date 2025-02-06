65c0f123af96ec1f9ba82b135787c8d7e3b9ae41
Harri Salokorpi <harri.salokorpi@reaktor.com>
2025-02-06 20:09:02 +0200

    Fix linter errors

--- aoe-infra/lib/monitor-stack.ts
+++ aoe-infra/lib/monitor-stack.ts
@@ -4,33 +4,33 @@ import * as sns from 'aws-cdk-lib/aws-sns';
 import * as chatbot from 'aws-cdk-lib/aws-chatbot';
 import * as ssm from 'aws-cdk-lib/aws-ssm';
 import * as logs from 'aws-cdk-lib/aws-logs';
-import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 
 export interface MonitorStackProps extends cdk.StackProps {
- slackChannelName: string,
- environment: string
+  slackChannelName: string,
+  environment: string
 }
 
 
 export class MonitorStack extends cdk.Stack {
- public topic: cdk.aws_sns.Topic;
- constructor(scope: Construct, id: string, props: MonitorStackProps) {
-   super(scope, id, props);
+  public topic: cdk.aws_sns.Topic;
+  public slackChannel: chatbot.SlackChannelConfiguration
+  constructor(scope: Construct, id: string, props: MonitorStackProps) {
+    super(scope, id, props);
 
     const slackChannelId = ssm.StringParameter.valueFromLookup(this, '/monitor/slack_channel_id');
     const slackWorkspaceId = ssm.StringParameter.valueFromLookup(this, '/monitor/slack_workspace_id');
 
     this.topic = new sns.Topic(this, `${props.environment}-cloudwatch-slack`);
-   
-    const slackChannel = new chatbot.SlackChannelConfiguration(this, 'SlackChannel', {
-        slackChannelConfigurationName: `${props.slackChannelName}`,
-        slackChannelId: slackChannelId,
-        slackWorkspaceId: slackWorkspaceId,
-        notificationTopics: [this.topic],
-        loggingLevel: chatbot.LoggingLevel.INFO,
-        logRetention: logs.RetentionDays.THREE_MONTHS,
+
+    this.slackChannel = new chatbot.SlackChannelConfiguration(this, 'SlackChannel', {
+      slackChannelConfigurationName: `${props.slackChannelName}`,
+      slackChannelId: slackChannelId,
+      slackWorkspaceId: slackWorkspaceId,
+      notificationTopics: [this.topic],
+      loggingLevel: chatbot.LoggingLevel.INFO,
+      logRetention: logs.RetentionDays.THREE_MONTHS,
     });
 
- }
+  }
 }
