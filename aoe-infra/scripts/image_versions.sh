b70603b180e31dbb0b7a5ff9fbbd16d56b31e46d
Harri Salokorpi <harri.salokorpi@reaktor.com>
2025-02-10 15:43:06 +0200

    Add utility script for parsing all image versions from config using jq

---
+++ aoe-infra/scripts/image_versions.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+# Figure out where we are currently
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly CURRENT_DIR
+
+image_versions() {
+  echo "Environment: $1"
+  echo "---"
+  echo "Data analytics:   $(jq -r '.services .data_analytics .image_tag' "$1")"
+  echo "Data services:    $(jq -r '.services .data_services .image_tag' "$1")"
+  echo "Semantic APIs:    $(jq -r '.services .semantic_apis .image_tag' "$1")"
+  echo "Streaming:        $(jq -r '.services .streaming .image_tag' "$1")"
+  echo "Web backend:      $(jq -r '.services .web_backend .image_tag' "$1")"
+  echo "Web frontend:     $(jq -r '.services .web_frontend .image_tag' "$1")"
+  echo ""
+}
+
+image_versions "${CURRENT_DIR}/../environments/dev.json"
+image_versions "${CURRENT_DIR}/../environments/qa.json"
+image_versions "${CURRENT_DIR}/../environments/prod.json"
