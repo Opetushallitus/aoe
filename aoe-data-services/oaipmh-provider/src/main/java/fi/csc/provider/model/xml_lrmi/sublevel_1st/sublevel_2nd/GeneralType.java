e00a77ef7c59bfe70654594dfaa05faaa6153eed
Harri Salokorpi <harri.salokorpi@reaktor.com>
2025-02-14 19:48:57 +0200

    Strip out all control characters in GeneralType XML output having values between 0x00-0x08
    These control characters are not allowed in XML at all.

--- aoe-data-services/oaipmh-provider/src/main/java/fi/csc/provider/model/xml_lrmi/sublevel_1st/sublevel_2nd/GeneralType.java
+++ aoe-data-services/oaipmh-provider/src/main/java/fi/csc/provider/model/xml_lrmi/sublevel_1st/sublevel_2nd/GeneralType.java
@@ -3,11 +3,9 @@ package fi.csc.provider.model.xml_lrmi.sublevel_1st.sublevel_2nd;
 import jakarta.xml.bind.annotation.XmlAccessType;
 import jakarta.xml.bind.annotation.XmlAccessorType;
 import jakarta.xml.bind.annotation.XmlValue;
-import lombok.Getter;
 import lombok.Setter;
 
 @SuppressWarnings("unused")
-@Getter
 @Setter
 @XmlAccessorType(XmlAccessType.NONE)
 public class GeneralType {
@@ -15,4 +13,9 @@ public class GeneralType {
     @XmlValue
     private String value;
 
+    public String getValue() {
+        // Remove all ASCII low-end control characters from value on get, since
+        // they are not allowed in XML
+        return value.replaceAll("[\\x00-\\x08]", "");
+    }
 }
