a83701c08a8b0d622ebf4a9edfcc1495d1cfc621
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-02-25 19:13:26 +0200

    Make start-local-env run on santeri's machine

--- scripts/fetch_secrets.sh
+++ scripts/fetch_secrets.sh
@@ -1,13 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 set -euo pipefail
 
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
 
+function aws {
+  docker_run_with_aws_env \
+    --volume "$repo:/aws" \
+    --rm -i "amazon/aws-cli:$aws_cli_version" "$@"
+}
+
 require_dev_aws_session
 
 echo "Fetching secret from AWS Secrets Manager..."
-aws s3 cp s3://aoe-local-dev/semantic-api/.env "$(dirname "$0")/../aoe-semantic-apis/.env" --profile aoe-dev
-aws s3 cp s3://aoe-local-dev/data-services/.env "$(dirname "$0")/../aoe-data-services/.env" --profile aoe-dev
-aws s3 cp s3://aoe-local-dev/data-analytics/.env "$(dirname "$0")/../aoe-data-analytics/.env" --profile aoe-dev
-aws s3 cp s3://aoe-local-dev/web-backend/.env "$(dirname "$0")/../aoe-web-backend/.env" --profile aoe-dev
-aws s3 cp s3://aoe-local-dev/streaming-app/.env "$(dirname "$0")/../aoe-streaming-app/.env" --profile aoe-dev
+aws s3 cp s3://aoe-local-dev/semantic-api/.env /aws/aoe-semantic-apis/.env --profile aoe-dev
+aws s3 cp s3://aoe-local-dev/data-services/.env /aws/aoe-data-services/.env --profile aoe-dev
+aws s3 cp s3://aoe-local-dev/data-analytics/.env /aws/aoe-data-analytics/.env --profile aoe-dev
+aws s3 cp s3://aoe-local-dev/web-backend/.env /aws/aoe-web-backend/.env --profile aoe-dev
+aws s3 cp s3://aoe-local-dev/streaming-app/.env /aws/aoe-streaming-app/.env --profile aoe-dev
