87ab457db9f90c6761b6ebd43d272d5a37a471a2
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-03-05 17:40:07 +0200

    Add initial renovate config

---
+++ scripts/renovate/run-renovate-locally.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../common-functions.sh"
+
+TOKEN="redacted"
+
+function main() {
+
+  pushd ${repo}
+  docker run \
+    -e RENOVATE_TOKEN=${TOKEN} \
+    -e LOG_LEVEL=debug \
+    -e RENOVATE_AUTODISCOVER=true \
+    -e RENOVATE_REPOSITORIES="Opetushallitus/aoe" \
+    -e RENOVATE_AUTODISCOVER_FILTER="Opetushallitus/aoe" \
+    renovate/renovate --dry-run=lookup
+  popd
+}
+
+main
