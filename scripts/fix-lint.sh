d8bd86c899950eb3ec95af6d09c1766795ab95f0
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-03-26 14:22:32 +0200

    Add script to fix linting errors (except frontend)

---
+++ scripts/fix-lint.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -o errexit -o nounset -o pipefail
+
+# shellcheck source=../scripts/common-functions.sh
+source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/common-functions.sh"
+
+function main {
+    pushd "$repo"
+    use_correct_node_version
+
+    fixLint "aoe-semantic-apis"
+    # fixLint "aoe-web-frontend"
+    fixLint "aoe-web-backend"
+    fixLint "aoe-streaming-app"
+    fixLint "aoe-infra"
+    popd
+}
+
+function fixLint {
+    local repository=$1
+    pushd "$repository"
+    npm_ci_if_package_lock_has_changed
+    npm run fix-lint
+    popd
+}
+
+main "$@"
