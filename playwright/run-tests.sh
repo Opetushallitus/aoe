bb668539e456e85ffa901235b396f81dae274791
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-04-11 13:19:37 +0300

    Fix run-tests to setup node

--- playwright/run-tests.sh
+++ playwright/run-tests.sh
@@ -2,8 +2,14 @@
 set -o errexit -o nounset -o pipefail
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../scripts/common-functions.sh"
 
-npm_ci_if_package_lock_has_changed
+function main {
+  use_correct_node_version
 
-npm run prettier-check
-npx --no playwright install --with-deps chromium
-npx --no playwright test "$@"
+  npm_ci_if_package_lock_has_changed
+
+  npm run prettier-check
+  npx --no playwright install --with-deps chromium
+  npx --no playwright test "$@"
+}
+
+main "$@"
