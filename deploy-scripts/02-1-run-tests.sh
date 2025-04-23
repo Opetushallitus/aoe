c7e25a7fe591063e150d5dfa93086aeae52e1d79
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2025-04-23 16:02:35 +0300

    export tag env variables

--- deploy-scripts/02-1-run-tests.sh
+++ deploy-scripts/02-1-run-tests.sh
@@ -22,13 +22,20 @@ function clean {
 function start_services {
 
   if running_on_gh_actions; then
-    AOE_WEB_BACKEND_TAG="${github_registry}aoe-web-backend:${revision}"
-    AOE_DATA_SERVICES_TAG="${github_registry}aoe-data-services:${revision}"
-    AOE_WEB_FRONTEND_TAG="${github_registry}aoe-web-frontend-ci:${revision}"
-    AOE_STREAMING_APP_TAG="${github_registry}aoe-streaming-app:${revision}"
-    AOE_SEMANTIC_APIS_TAG="${github_registry}aoe-semantic-apis:${revision}"
-    AOE_DATA_ANALYTICS_TAG="${github_registry}aoe-data-analytics:${revision}"
+    export AOE_WEB_BACKEND_TAG="${github_registry}aoe-web-backend:${revision}"
+    export AOE_DATA_SERVICES_TAG="${github_registry}aoe-data-services:${revision}"
+    export AOE_WEB_FRONTEND_TAG="${github_registry}aoe-web-frontend-ci:${revision}"
+    export AOE_STREAMING_APP_TAG="${github_registry}aoe-streaming-app:${revision}"
+    export AOE_SEMANTIC_APIS_TAG="${github_registry}aoe-semantic-apis:${revision}"
+    export AOE_DATA_ANALYTICS_TAG="${github_registry}aoe-data-analytics:${revision}"
+
   fi
+  echo $AOE_WEB_BACKEND_TAG
+  echo $AOE_DATA_SERVICES_TAG
+  echo $AOE_WEB_FRONTEND_TAG
+  echo $AOE_STREAMING_APP_TAG
+  echo $AOE_SEMANTIC_APIS_TAG
+  echo $AOE_DATA_ANALYTICS_TAG
   $compose --profile aoe up --no-build -d
 }
 
