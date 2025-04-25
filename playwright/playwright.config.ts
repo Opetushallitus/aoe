0ee5e6efcc88edecee35f6a3d2f1ef8ae02c11e7
Simo-Pekka Kerkelä <simo-pekka.kerkela@reaktor.com>
2025-04-25 10:24:53 +0300

    AOE-10 use github reporter for tests, upload test results

--- playwright/playwright.config.ts
+++ playwright/playwright.config.ts
@@ -18,11 +18,12 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 4 : 0,
+  retries: process.env.CI ? 3 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
+    [process.env.CI ? 'github' : 'null'],
     ['list'],
     [
       'junit',
