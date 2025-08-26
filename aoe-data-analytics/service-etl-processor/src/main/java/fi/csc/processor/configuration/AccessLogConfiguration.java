7d88a8d4bc3db4b24c0e4a1cc630622c446aca6d
Riku Honkanen <riku.honkanen@reaktor.com>
2025-08-26 14:17:56 +0300

    AOE-27: Add access logs to satistics endpoint

---
+++ aoe-data-analytics/service-etl-processor/src/main/java/fi/csc/processor/configuration/AccessLogConfiguration.java
@@ -0,0 +1,28 @@
+package fi.csc.processor.configuration;
+
+import ch.qos.logback.access.tomcat.LogbackValve;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.server.WebServerFactory;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AccessLogConfiguration {
+
+    @Bean
+    WebServerFactoryCustomizer<WebServerFactory> containerCustomizer() {
+        return container -> {
+            if (container instanceof TomcatServletWebServerFactory) {
+                ((TomcatServletWebServerFactory) container).addContextCustomizers((TomcatContextCustomizer) context -> {
+                    LogbackValve logbackValve = new LogbackValve();
+                    logbackValve.setFilename("logback-access.xml");
+                    logbackValve.setAsyncSupported(true);
+                    context.getPipeline().addValve(logbackValve);
+                });
+            }
+        };
+
+    }
+}
