671b20b68bb513d049f234cfc7d3a8659b2f7f2e
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2025-09-17 12:47:52 +0300

    AOE-29: remove useless angular unit tests that only test component
    creation. These tests are not run and do not not even work. They only
    add dependencies and make it harder to patch vulnerabilities.

--- aoe-web-frontend/src/app/pipes/clean-filename.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CleanFilenamePipe } from './clean-filename.pipe';
-
-describe('CleanFilenamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new CleanFilenamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
