99e6a17fcaa4310365e6c917d0c072f0f29e35de
Riku Honkanen <riku.honkanen@reaktor.com>
2025-10-20 15:48:23 +0300

    AOE-39: Format

--- playwright/tests/bryssel.spec.ts
+++ playwright/tests/bryssel.spec.ts
@@ -1,12 +1,31 @@
 import { expect, test } from '@playwright/test'
 import { BrysselEtusivu } from './pages/BrysselEtusivu'
 
-test('käyttäjä voi hakea analytiikka sivulta oppimateriaaalien käyttömääriä', async ({ page }) => {
+test('käyttäjä voi hakea analytiikka sivulta oppimateriaaalien käyttömääriä', async ({
+  page,
+}) => {
   const etusivu = BrysselEtusivu(page)
   await etusivu.goto()
 
   const analytiikka = await etusivu.clickTopic('analytiikka')
-  await analytiikka.taytaJaHaeOppimateriaalienKayttomaarat(['Haku', 'Katselu'], 'Kuukausi')
+  await analytiikka.taytaJaHaeOppimateriaalienKayttomaarat(
+    ['Haku', 'Katselu'],
+    'Kuukausi'
+  )
+
+  await expect(analytiikka.kayttomaaraChart).toBeVisible()
+})
+test('käyttäjä voi hakea analytiikka sivulta oppimateriaaalien käyttömääriä', async ({
+  page,
+}) => {
+  const etusivu = BrysselEtusivu(page)
+  await etusivu.goto()
+
+  const analytiikka = await etusivu.clickTopic('analytiikka')
+  await analytiikka.taytaJaHaeOppimateriaalienKayttomaarat(
+    ['Haku', 'Katselu'],
+    'Kuukausi'
+  )
 
   await expect(analytiikka.kayttomaaraChart).toBeVisible()
 })
--- playwright/tests/kirjautumaton-kayttaja.spec.ts
+++ playwright/tests/kirjautumaton-kayttaja.spec.ts
@@ -1,28 +1,9 @@
 import { expect, test } from '@playwright/test'
 import { Etusivu } from './pages/Etusivu'
 
-test('kirjautumaton käyttäjä voi ladata oppimateriaalia', async ({ page, browser }) => {
-  const etusivu = Etusivu(page)
-  await etusivu.goto()
-  const omatMateriaalit = await etusivu.header.clickOmatMateriaalit()
-  const uusiMateriaali = await omatMateriaalit.luoUusiMateriaali()
-  const materiaaliNimi = uusiMateriaali.randomMateriaaliNimi()
-  await uusiMateriaali.taytaJaTallennaUusiMateriaali(materiaaliNimi)
-
-  const newContext = await browser.newContext({ storageState: undefined })
-  const kirjautumatonEtusivu = Etusivu(await newContext.newPage())
-  await kirjautumatonEtusivu.goto()
-  await kirjautumatonEtusivu.header.fi.click()
-  await expect(kirjautumatonEtusivu.header.kirjaudu).toBeVisible()
-  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(materiaaliNimi)
-  await kirjautumatonMateriaali.lataaDropdown.click()
-  const tiedostoBlob = await kirjautumatonMateriaali.lataaTiedosto('blank')
-  expect(await tiedostoBlob.text()).toContain('Blank PDF Document')
-})
-
-test('kirjautumaton käyttäjä voi katsella materiaalin tiedostoja esikatselunäkymässä, ja nähdä materiaalin sisällön.', async ({
+test('kirjautumaton käyttäjä voi ladata oppimateriaalia', async ({
   page,
-  browser
+  browser,
 }) => {
   const etusivu = Etusivu(page)
   await etusivu.goto()
@@ -36,14 +17,40 @@ test('kirjautumaton käyttäjä voi katsella materiaalin tiedostoja esikatselun�
   await kirjautumatonEtusivu.goto()
   await kirjautumatonEtusivu.header.fi.click()
   await expect(kirjautumatonEtusivu.header.kirjaudu).toBeVisible()
-  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(materiaaliNimi)
+  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(
+    materiaaliNimi
+  )
+  await kirjautumatonMateriaali.lataaDropdown.click()
+  const tiedostoBlob = await kirjautumatonMateriaali.lataaTiedosto('blank')
+  expect(await tiedostoBlob.text()).toContain('Blank PDF Document')
+})
+
+test('kirjautumaton käyttäjä voi katsella materiaalin tiedostoja esikatselunäkymässä, ja nähdä materiaalin sisällön.', async ({
+  page,
+  browser,
+}) => {
+  const etusivu = Etusivu(page)
+  await etusivu.goto()
+  const omatMateriaalit = await etusivu.header.clickOmatMateriaalit()
+  const uusiMateriaali = await omatMateriaalit.luoUusiMateriaali()
+  const materiaaliNimi = uusiMateriaali.randomMateriaaliNimi()
+  await uusiMateriaali.taytaJaTallennaUusiMateriaali(materiaaliNimi)
+
+  const newContext = await browser.newContext({ storageState: undefined })
+  const kirjautumatonEtusivu = Etusivu(await newContext.newPage())
+  await kirjautumatonEtusivu.goto()
+  await kirjautumatonEtusivu.header.fi.click()
+  await expect(kirjautumatonEtusivu.header.kirjaudu).toBeVisible()
+  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(
+    materiaaliNimi
+  )
   const preview = await kirjautumatonMateriaali.preview('blank.pdf')
   await expect(preview).toBeVisible()
 })
 
 test('kirjautumaton käyttämä voi ilman kirjautumista siirtyä materiaalin sisältöön, kun se on linkki ulkoiseen verkko-osoitteeseen.', async ({
   page,
-  browser
+  browser,
 }) => {
   const etusivu = Etusivu(page)
   await etusivu.goto()
@@ -60,7 +67,9 @@ test('kirjautumaton käyttämä voi ilman kirjautumista siirtyä materiaalin sis
   await kirjautumatonEtusivu.goto()
   await kirjautumatonEtusivu.header.fi.click()
   await expect(kirjautumatonEtusivu.header.kirjaudu).toBeVisible()
-  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(materiaaliNimi)
+  const kirjautumatonMateriaali = await kirjautumatonEtusivu.clickMateriaali(
+    materiaaliNimi
+  )
   const pagePromise = newContext.waitForEvent('page')
   await kirjautumatonMateriaali.clickVerkkosivu()
   const newPage = await pagePromise
