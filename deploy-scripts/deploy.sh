68679608f5844cdf04efb5aef3d7c27237d4f145
Tuomas Alanen <tuomas.alanen@reaktor.com>
2026-04-30 11:58:41 +0300

    AOE-90-1: Give approval for changes

--- deploy-scripts/deploy.sh
+++ deploy-scripts/deploy.sh
@@ -27,7 +27,7 @@ function deploy {
   PAGERDUTY_EVENT_URL=$( get_secret "/pagerduty/event_url")
   export PAGERDUTY_EVENT_URL
 
-  ./cdk.sh deploy --all --concurrency 10 "$@"
+  ./cdk.sh deploy --all --require-approval never --concurrency 10 "$@"
   popd
 }
 
