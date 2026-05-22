86fe1823764d959b4fa7a528adcab1a35dedd741
Tuomas Alanen <tuomas.alanen@reaktor.com>
2026-05-22 12:46:12 +0300

    AOE-97-6: Remove init-mongo.js init script
    - MongoDB no longer used locally after DocumentDB decommission

--- docker/init-scripts/init-mongo.js
+++
@@ -1,8 +0,0 @@
-db = db.getSiblingDB('aoe');
-
-// Create a new user with readWrite access on the appdb database
-db.createUser({
-    user: 'aoeuser',
-    pwd: 'aoepassword',  // Replace with your desired password
-    roles: [{ role: 'readWrite', db: 'aoe' }]
-});
